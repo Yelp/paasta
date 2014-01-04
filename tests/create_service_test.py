52ba4cf78ebecd8945fe7b55e68f324f1d41f39e
Tyler Roscoe <troscoe@yelp.com>
2014-01-03 20:56:29 -0800

    Actual tests

--- tests/create_service_test.py
+++ tests/create_service_test.py
@@ -1,9 +1,26 @@
+from mock import Mock
 import testify as T
 
+import create_service
 
-class ValidateArgsTestCase(T.TestCase):
-    def test(self):
-        pass
+
+class ValidateOptionsTestCase(T.TestCase):
+    def test_enable_puppet_requires_puppet_root(self):
+        parser = Mock()
+        options = Mock()
+        options.enable_puppet = True
+        options.puppet_root = None
+        with T.assert_raises(SystemExit):
+            create_service.validate_options(parser, options)
+
+    def test_enable_nagios_requires_nagios_root(self):
+        parser = Mock()
+        options = Mock()
+        options.enable_nagios = True
+        options.nagios_root = None
+        with T.assert_raises(SystemExit):
+            create_service.validate_options(parser, options)
+
 
 if __name__ == "__main__":
     T.run()
