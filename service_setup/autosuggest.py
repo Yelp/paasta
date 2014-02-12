224726fc6f02d408632a46ee7b4e39d97cae5315
Tyler Roscoe <troscoe@yelp.com>
2014-02-11 22:02:14 -0800

    Rename

--- service_setup/autosuggest.py
+++ service_setup/autosuggest.py
@@ -18,10 +18,10 @@ def suggest_vip():
     least_vip = min(vip_counts.items(), key=operator.itemgetter(1))
     return least_vip[0]
 
-def _get_port_from_port_file(root, portfile):
-    """Given a root and portfile (as from os.walk), attempt to return a port
-    number (int) from that portfile. Returns 0 if file is empty."""
-    with open(os.path.join(root, portfile)) as f:
+def _get_port_from_file(root, file):
+    """Given a root and file (as from os.walk), attempt to return a port
+    number (int) from that file. Returns 0 if file is empty."""
+    with open(os.path.join(root, file)) as f:
         port = f.read().strip()
         port = int(port) if port else 0
     return port
@@ -33,7 +33,7 @@ def suggest_port():
             os.path.join(config.PUPPET_ROOT, paths.SERVICE_FILES)):
         for f in files:
             if f.endswith("port"):
-                port = _get_port_from_port_file(root, f)
+                port = _get_port_from_file(root, f)
                 if not 14000 > port > 13000:
                     port = 0
                 max_port = max(port, max_port)
