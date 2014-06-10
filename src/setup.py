16dba4d185f5ba1eb603d8f90c66023aa5a7bf63
Kyle Anderson <kwa@yelp.com>
2014-06-10 15:22:26 -0700

    Added initial requirements.txT

---
+++ src/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name           = 'service_deployment_tools',
+    version        = '0.0.1',
+    provides       = ["service_deployment_tools"],
+    author         = 'Kyle Anderson',
+    author_email   = 'kwa@yelp.com',
+    description    = 'Tools for Yelps SOA infrastructure',
+    packages       = find_packages(exclude=["tests"]),
+    setup_requires = ['setuptools'],
+    include_package_data=True,
+    install_requires = [
+    ],
+    scripts = [
+    ]
+)
+
