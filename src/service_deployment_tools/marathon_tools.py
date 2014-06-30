f2b6211661583cb1ea734512b531683a560166c6
James McGuinness <jrm@yelp.com>
2014-06-30 14:59:30 -0700

    cleaned up logging, moved bounces to tools

--- src/service_deployment_tools/marathon_tools.py
+++ src/service_deployment_tools/marathon_tools.py
@@ -1,3 +1,19 @@
+import logging
+
+log = logging.getLogger("__main__")
+
+def brutal_bounce(old_ids, new_config, client):
+    """Brutally bounce the service by killing all old instances first.
+
+    Kills all old_ids then spawns a new app with the new_config via a
+    Marathon client."""
+    log.debug("Brutally bouncing...")
+    for app in old_ids:
+        log.info("Killing %s", app)
+        client.delete_app(app)
+    log.info("Creating %s", new_config['id'])
+    client.create_app(**new_config)
+
 def get_config():
     # Required keys (need defaults for some):
     #   docker_registry
--- src/service_deployment_tools/setup_marathon_job.py
+++ src/service_deployment_tools/setup_marathon_job.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import sys
+import logging
 import argparse
 import datetime
 import service_configuration_lib
@@ -11,14 +12,14 @@ from marathon import MarathonClient
 # https://github.com/mesosphere/marathon/blob/master/REST.md#post-v2apps
 
 ID_TKN = '__'
+log = logging.getLogger(__name__)
 
 def parse_args():
-    """Parse arguments for the script."""
     parser = argparse.ArgumentParser(description='Creates marathon jobs.')
     parser.add_argument('service_name',
                         help="The service name to create or update")
     parser.add_argument('instance_name',
-                        help="The service instance to create or update")
+                        help="The marathon instance of the service to create or update")
     parser.add_argument('-d', '--soa-dir', dest="soa_dir", metavar="SOA_DIR",
                         default=service_configuration_lib.DEFAULT_SOA_DIR,
                         help="define a different soa config directory")
@@ -28,109 +29,87 @@ def parse_args():
     return args
 
 def get_main_marathon_config():
-    """Get the main marathon configuration via marathon_tools.get_config()."""
-    if verbose:
-        print "Reading general marathon configuration"
+    log.debug("Reading marathon configuration")
     marathon_config = marathon_tools.get_config()
-    if verbose:
-        print "Marathon config is:"
-        print marathon_config
-        print ""
+    log.info("Marathon config is: %s", marathon_config)
     return marathon_config
 
-def get_docker_url(marathon_config, service_config):
+def get_docker_url(registry_uri, docker_image):
     """Compose the docker url.
 
-    Uses the docker_registry value from marathon_config and the
-    docker_image value from service_config.
+    Uses the registry_uri (docker_registry) value from marathon_config
+    and the docker_image value from a service config to make a Docker URL.
     The URL is prepended with docker:/// per the deimos docs, at
     https://github.com/mesosphere/deimos"""
-    if verbose:
-        print "Docker URL: " + str('docker:///' + \
-            marathon_config['docker_registry'] + '/' + service_config['docker_image'])
-    return 'docker:///' + marathon_config['docker_registry'] + '/' + service_config['docker_image']
+    docker_url = str('docker:///' + registry_uri + '/' + docker_image)
+    log.info("Docker URL: %s", docker_url)
+    return docker_url
 
-def get_time_tag():
-    """Gets the current time in Marathon's expected format for versioning."""
-    now = datetime.datetime.now()
-    return str(now.date()) + "T" + str(now.time()) + "Z"
+def get_ports(service_config):
+    """Gets the number of ports required from the service's marathon configuration.
 
-def get_ports(general_config):
-    """Gets the port from the general service configuration.
-
-    Defaults to no ports at all if unspecified.
+    Defaults to one port if unspecified.
+    Ports are randomly assigned by Mesos.
     This must return an array, as the Marathon REST API takes an
     array of ports, not a single value."""
-    if 'port' in general_config:
-        return [general_config['port']]
+    num_ports = service_config.get('num_ports')
+    if num_ports:
+        return [0 for i in range(int(num_ports))]
     else:
-        if verbose:
-            print "WARNING: 'port' not specified in config. No port will be used"
-        return []
+        log.warning("'num_ports' not specified in config. One port will be used.")
+        return [0]
 
 def get_mem(service_config):
     """Gets the memory required from the service's marathon configuration.
 
     Defaults to 100 if no value specified in the config."""
-    if 'mem' in service_config:
-        return service_config['mem']
-    else:
-        if verbose:
-            print "WARNING: 'mem' not specified in config. Using default: 100"
-        return '100'
+    mem = service_config.get('mem')
+    if not mem:
+        log.warning("'mem' not specified in config. Using default: 100")
+    return int(mem) if mem else '100'
 
 def get_cpus(service_config):
     """Gets the number of cpus required from the service's marathon configuration.
 
     Defaults to 1 if no value specified in the config."""
-    if 'cpus' in service_config:
-        return service_config['cpus']
-    else:
-        if verbose:
-            print "WARNING: 'cpus' not specified in config. Using default: 1"
-        return 1
+    cpus = service_config.get('cpus')
+    if not cpus:
+        log.warning("'cpus' not specified in config. Using default: 1")
+    return int(cpus) if cpus else 1
 
 def get_constraints(service_config):
     """Gets the constraints specified in the service's marathon configuration.
 
-    Defaults to no constraints if none specified."""
-    if 'constraints' in service_config:
-        return service_config['constraints']
-    else:
-        return []
+    Defaults to no constraints if none given."""
+    return service_config.get('constraints')
 
 def get_instances(service_config):
     """Get the number of instances specified in the service's marathon configuration.
 
     Defaults to 1 if not specified in the config."""
-    if 'instances' in service_config:
-        return service_config['instances']
-    else:
-        if verbose:
-            print "WARNING: 'instances' not specified in config. Using default: 1"
-        return 1
+    instances = service_config.get('instances')
+    if not instances:
+        log.warning("'instances' not specified in config. Using default: 1")
+    return int(instances) if instances else 1
 
 def get_bounce_method(service_config):
     """Get the bounce method specified in the service's marathon configuration.
 
     Defaults to brutal if no method specified in the config."""
-    if 'bounce_method' in service_config:
-        return service_config['bounce_method']
-    else:
-        if verbose:
-            print "WARNING: 'bounce_method' not specified in config. Using default: brutal"
-        return 'brutal'
+    bounce_method = service_config.get('bounce_method')
+    if not bounce_method:
+        log.warning("'bounce_method' not specified in config. Using default: brutal")
+    return bounce_method if bounce_method else 'brutal'
 
-def get_marathon_client(marathon_config):
+def get_marathon_client(url, user, passwd):
     """Get a new marathon client connection in the form of a MarathonClient object.
 
     Connects to the Marathon server at 'url' with login specified
-    by 'user' and 'pass', all from the general marathon_config."""
-    if verbose:
-        print "Connecting to Marathon server at: " + str(marathon_config['url'])
-    return MarathonClient(marathon_config['url'], marathon_config['user'], marathon_config['pass'])
+    by 'user' and 'pass', all from the marathon config."""
+    log.info("Connecting to Marathon server at: %s", url)
+    return MarathonClient(url, user, passwd)
 
-def create_complete_config(name, url, marathon_config, service_general_config, service_marathon_config):
+def create_complete_config(name, url, docker_options, executor, service_marathon_config):
     """Create the configuration that will be passed to the Marathon REST API.
 
     Currently compiles the following keys into one nice dict:
@@ -146,11 +125,11 @@ def create_complete_config(name, url, marathon_config, service_general_config, s
       constraints: the constraints on the Marathon job.
       instances: the number of instances required."""
     complete_config = {'id': name,
-                       'cmd': url,
-                       'container': {'image': url, 'options': marathon_config['docker_options']},
-                       'executor': marathon_config['executor'],
+                       'cmd': '/bin/true',
+                       'container': {'image': url, 'options': docker_options},
+                       'executor': executor,
                        'uris': [] }
-    complete_config['ports'] = get_ports(service_general_config)
+    complete_config['ports'] = get_ports(service_marathon_config)
     complete_config['mem'] = get_mem(service_marathon_config)
     complete_config['cpus'] = get_cpus(service_marathon_config)
     complete_config['constraints'] = get_constraints(service_marathon_config)
@@ -159,74 +138,48 @@ def create_complete_config(name, url, marathon_config, service_general_config, s
     for key in complete_config:
         if complete_config[key]:
             app_kwargs[key] = complete_config[key]
-    if verbose:
-        print "Complete configuration for instance is:"
-        print app_kwargs
-        print ""
+    log.info("Complete configuration for instance is: %s", app_kwargs)
     return app_kwargs
 
-def brutal_bounce(old_ids, new_config, client):
-    """Brutally bounce the service by killing all old instances first.
-
-    Kills all old_ids then spawns a new app with the new_config via a
-    Marathon client."""
-    if verbose:
-        print "Brutally bouncing..."
-    for app in old_ids:
-        if verbose:
-            print "Killing " + app
-        client.delete_app(app)
-    if verbose:
-        print "Creating " + new_config['id']
-    client.create_app(**new_config)
-
 def deploy_service(name, config, client, bounce_method):
-    """Deploy the service with the given name, config, and bounce_method.
-
-    Currently, the only bounce method recognized is brutal."""
-    if verbose:
-        print "Deploying service instance " + name + " with bounce_method " + bounce_method
-        print "Searching for old service iterations"
+    """Deploy the service with the given name, config, and bounce_method."""
+    log.info("Deploying service instance %s with bounce_method %s", name, bounce_method)
+    log.debug("Searching for old service instance iterations")
     filter_name = name.split(ID_TKN)[0] + ID_TKN + name.split(ID_TKN)[1]
     app_list = client.list_apps()
-    matching = [app.id for app in filter(lambda app: filter_name in app.id, app_list)]
-    if matching: # there's a previous iteration; bounce
-        if verbose:
-            print "Matching instances found: " + str(matching)
+    old_app_ids = [app.id for app in app_list if filter_name in app.id]
+    if old_app_ids: # there's a previous iteration; bounce
+        log.info("Old service instance iterations found: %s", old_app_ids)
         if bounce_method == "brutal":
-            brutal_bounce(matching, config, client)
+            marathon_tools.brutal_bounce(old_app_ids, config, client)
         else:
-            print "ERROR: bounce_method not recognized: " + bounce_method + ". Exiting"
+            log.error("bounce_method not recognized: %s. Exiting", bounce_method)
             return False
     else: # there wasn't actually a previous iteration; just deploy it
-        if verbose:
-            print "No old instances found. Deploying instance " + name
+        log.info("No old instances found. Deploying instance %s", name)
         client.create_app(**config)
-    if verbose:
-        print name + " deployed. Exiting"
+    log.info("%s deployed. Exiting", name)
     return True
 
 def setup_service(service_name, instance_name, client, marathon_config,
-                  service_general_config, service_marathon_config):
+                  service_marathon_config):
     """Setup the service instance given and attempt to deploy it, if possible.
 
     The full id of the service instance is service_name__instance_name__iteration.
     Doesn't do anything if the full id is already in Marathon.
     If it's not, attempt to find old instances of the service and bounce them."""
     full_id = service_name + ID_TKN + instance_name + ID_TKN + service_marathon_config['iteration']
-    if verbose:
-        print "Setting up service instance for: " + service_name + ID_TKN + instance_name
-        print "Desired Marathon instance id: " + full_id
-    docker_url = get_docker_url(marathon_config, service_marathon_config)
-    complete_config = create_complete_config(full_id, docker_url, marathon_config,
-                                             service_general_config, service_marathon_config)
+    log.info("Setting up service instance for: %s", service_name + ID_TKN + instance_name)
+    log.info("Desired Marathon instance id: %s", full_id)
+    docker_url = get_docker_url(marathon_config['docker_registry'],
+                                service_marathon_config['docker_image'])
+    complete_config = create_complete_config(full_id, docker_url, marathon_config['docker_options'],
+                                             marathon_config['executor'], service_marathon_config)
     try:
-        if verbose:
-            print "Checking if instance with iteration " + \
-                service_marathon_config['iteration'] + " already exists"
+        log.info("Checking if instance with iteration %s already exists",
+                 service_marathon_config['iteration'])
         client.get_app(full_id)
-        if verbose:
-            print "Iteration already exists. Exiting"
+        log.warning("App id %s already exists. Skipping configuration and exiting.", full_id)
         # I wanted to compare the configs with a call to get_app, but Marathon doesn't
         # return the 'container' key via its REST API, which makes comparing not
         # really viable (especially since the container keys extremely likely to
@@ -244,34 +197,39 @@ def main():
       -d, --soa-dir: A soa config directory to read config files from, otherwise uses
                      service_configuration_lib.DEFAULT_SOA_DIR
       -v, --verbose: Verbose output"""
-    global verbose
     args = parse_args()
     service_name = args.service_name
     instance_name = args.instance_name
     soa_dir = args.soa_dir
-    verbose = args.verbose
-    #time_tag = get_time_tag()
+    if args.verbose:
+        log.setLevel(logging.INFO)
+    else:
+        log.setLevel(logging.WARNING)
 
     marathon_config = get_main_marathon_config()
-    client = get_marathon_client(marathon_config)
+    client = get_marathon_client(marathon_config['url'], marathon_config['user'],
+                                 marathon_config['pass'])
 
-    if verbose:
-        print "Reading service configuration dir " + service_name + "/ in " + soa_dir
-    service_general_config = service_configuration_lib.read_service_configuration(service_name,
-                                                                                  soa_dir=soa_dir)
+    log.info("Reading service configuration files from dir %s/ in %s", service_name, soa_dir)
+    log.info("Reading general service configuration file: service.yaml")
+    service_general_config = service_configuration_lib.read_extra_service_information(
+                                    service_name,
+                                    "service.yaml",
+                                    soa_dir=soa_dir)
+    marathon_conf_file = "marathon-" + marathon_config['cluster']
+    log.info("Reading marathon configuration file: %s.yaml", marathon_conf_file)
     service_instance_configs = service_configuration_lib.read_extra_service_information(
                                     service_name,
-                                    "marathon-" + marathon_config['cluster'],
+                                    marathon_conf_file,
                                     soa_dir=soa_dir)
     if instance_name in service_instance_configs:
         if setup_service(service_name, instance_name, client, marathon_config,
-                         service_general_config, service_instance_configs[instance_name]):
+                         service_instance_configs[instance_name]):
             sys.exit(0)
         else:
             sys.exit(1)
     else:
-        print "ERROR: " + instance_name + " not found in config file marathon-" + \
-                marathon_config['cluster'] + ".yaml. Exiting"
+        log.error("%s not found in config file %s.yaml. Exiting", instance_name, marathon_conf_file)
         sys.exit(1)
 
 
