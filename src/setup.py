3356bc9026ab6102dbaa732ee0dd0739699ee516
James McGuinness <jrm@yelp.com>
2014-07-28 13:35:00 -0700

    bumping version

--- src/setup.py
+++ src/setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name           = 'service-deployment-tools',
-    version        = '0.1.19',
+    version        = '0.1.20',
     provides       = ["service_deployment_tools"],
     author         = 'Kyle Anderson',
     author_email   = 'kwa@yelp.com',
