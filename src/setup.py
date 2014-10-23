93cbe1f5935db5a320b612961626fe0eb5bad700
Tyler Roscoe <troscoe@yelp.com>
2014-10-23 16:37:57 -0700

    0.6.2 setup.py

--- src/setup.py
+++ src/setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name           = 'service-deployment-tools',
-    version        = '0.6.1',
+    version        = '0.6.2',
     provides       = ["service_deployment_tools"],
     author         = 'Kyle Anderson',
     author_email   = 'kwa@yelp.com',
