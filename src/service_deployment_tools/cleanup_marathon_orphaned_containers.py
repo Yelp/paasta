633b00165460419ce3f887bda7ba8078a7f958cf
Tyler Roscoe <troscoe@yelp.com>
2014-10-24 23:27:08 -0700

    get_running_images and get_running_mesos_images

--- src/service_deployment_tools/cleanup_marathon_orphaned_containers.py
+++ src/service_deployment_tools/cleanup_marathon_orphaned_containers.py
@@ -11,6 +11,18 @@ import docker
 log = logging.getLogger('__main__')
 
 
+def get_running_images(client):
+    return client.containers()
+
+
+def get_running_mesos_images(running_images):
+    running_mesos_images = []
+    for image in running_images:
+        if any([name for name in image.get('Names', '') if name.startswith('/mesos-')]):
+            running_mesos_images.append(image)
+    return running_mesos_images
+
+
 def parse_args():
     parser = argparse.ArgumentParser(
         description='Stop Docker images spawned by Mesos which are no longer supposed to be running')
@@ -25,10 +37,17 @@ def parse_args():
 
 
 def main():
-    # args = parse_args()
+    args = parse_args()
+    if args.verbose:
+        log.setLevel(logging.INFO)
+    else:
+        log.setLevel(logging.WARNING)
+
     client = docker.Client()
-    print client
-    # import pdb; pdb.set_trace()
+    running_images = get_running_images(client)
+    running_mesos_images = get_running_mesos_images(running_images)
+    from pprint import pprint
+    pprint(running_mesos_images)
 
 
 if __name__ == "__main__":
--- src/tests/test_cleanup_marathon_orphaned_containers.py
+++ src/tests/test_cleanup_marathon_orphaned_containers.py
@@ -1,9 +1,48 @@
 #!/usr/bin/env python
 
+import mock
+import pytest
+
 import cleanup_marathon_orphaned_images
 
 
-class TestCleanupMarathonOrphanedImages:
-    def test(self):
-        assert True
-        cleanup_marathon_orphaned_images.main()
+# These should be left running
+mesos_deployed_old = {
+    'Names': ['/mesos-deployed-old', ],
+}
+mesos_undeployed_young = {
+    'Names': ['/mesos-undeployed-young', ],
+}
+nonmesos_undeployed_old = {
+    'Names': ['/nonmesos-undeployed-old', ],
+}
+# These should be cleaned up
+mesos_undeployed_old = {
+    'Names': ['/mesos-undeployed-old', ],
+}
+
+
+@pytest.yield_fixture
+def mock_get_running_images():
+    return_value = [
+        mesos_deployed_old,
+        nonmesos_undeployed_old,
+        mesos_undeployed_young,
+        mesos_undeployed_old,
+    ]
+    with mock.patch(
+        'cleanup_marathon_orphaned_images.get_running_images',
+        autospec=True,
+        return_value=return_value,
+    ) as (
+        mock_get_running_images
+    ):
+        yield mock_get_running_images
+
+
+def test_get_running_mesos_images(mock_get_running_images):
+    client = 'unused fake client'
+    # this is ridiculous but just in case i actually want to leave this mocked
+    running_images = cleanup_marathon_orphaned_images.get_running_images(client)
+    actual = cleanup_marathon_orphaned_images.get_running_mesos_images(running_images)
+    assert nonmesos_undeployed_old not in actual
