a9cb709848335f562fe05bd36a6bde7b24ceb288
Karthig Balendran <karthig@yelp.com>
2014-11-21 10:44:39 -0800

    Fix broken paasta list tests

--- src/tests/paasta_cli/test_cmds_list.py
+++ src/tests/paasta_cli/test_cmds_list.py
@@ -1,14 +1,20 @@
-from mock import patch
+from mock import patch, MagicMock
 from StringIO import StringIO
 
 from service_deployment_tools.paasta_cli.cmds.list import paasta_list
+
 
 @patch('service_deployment_tools.paasta_cli.cmds.list.read_services_configuration')
 @patch('sys.stdout', new_callable=StringIO)
 def test_list_paasta_list(mock_stdout, mock_read_services):
     # paasta_list print each service returned by get_services
 
-    mock_read_services.keys.return_value = ['service_1', 'service_2']
+    attrs = {'keys.return_value': ['service_1', 'service_2']}
+
+    mock_function = MagicMock()
+    mock_function.configure_mock(**attrs)
+    mock_read_services.return_value = mock_function
+
     args = ['./paasta_cli', 'list']
     paasta_list(args)
     output = mock_stdout.getvalue()
--- src/tests/paasta_cli/test_paasta_cli.py
+++ src/tests/paasta_cli/test_paasta_cli.py
@@ -1,18 +1,25 @@
 import sys
 from StringIO import StringIO
 
-from mock import patch
+from mock import patch, MagicMock
 
 from service_deployment_tools.paasta_cli import paasta_cli
 
+
 @patch('sys.stdout', new_callable=StringIO)
-@patch('service_deployment_tools.paasta_cli.cmds.list.get_services')
+@patch('service_deployment_tools.paasta_cli.cmds.list.read_services_configuration')
 @patch('service_deployment_tools.paasta_cli.paasta_cli.paasta_commands')
-def test_paasta_list(mock_paasta_commands, mock_get_services, mock_stdout):
+@patch('service_deployment_tools.paasta_cli.paasta_cli.load_method')
+def test_paasta_list(mock_paasta_commands, mock_read_services, mock_stdout):
     # 'paasta list' with no args prints list of services in list.get_services
 
     mock_paasta_commands.return_value = ['list']
-    mock_get_services.return_value = ['service_1', 'service_2']
+
+    attrs = {'keys.return_value': ['service_1', 'service_2']}
+    mock_function = MagicMock()
+    mock_function.configure_mock(**attrs)
+    mock_read_services.return_value = mock_function
+
     sys.argv = ['./paasta_cli', 'list']
     paasta_cli.main()
     output = mock_stdout.getvalue()
