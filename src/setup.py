e1db5a970a3aef9ae63db6b792b6899de7dd78db
Karthig Balendran <karthig@yelp.com>
2014-12-04 15:45:06 -0800

    Update version to 0.7.7

--- src/setup.py
+++ src/setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name           = 'service-deployment-tools',
-    version        = '0.7.6',
+    version        = '0.7.7',
     provides       = ["service_deployment_tools"],
     author         = 'Kyle Anderson',
     author_email   = 'kwa@yelp.com',
