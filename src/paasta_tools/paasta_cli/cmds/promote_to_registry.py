93ab7261d06d6ae6493470a3d50be04d7b3661f7
Kyle Anderson <kwa@yelp.com>
2014-12-30 15:10:26 -0800

    boilerplate for promote_to_registry

---
+++ src/paasta_tools/paasta_cli/cmds/promote_to_registry.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+"""Contains methods used by the paasta client to upload a docker
+image to a registry."""
+
+import subprocess
+import sys
+
+def add_subparser(subparsers):
+    list_parser = subparsers.add_parser(
+        'promote-to-registry',
+        description='Uploads a docker image to a registry',
+        help='Uploads a docker image to a registry')
+
+    list_parser.add_argument('-s', '--service',
+                             help='Name of service for which you wish to '
+                                  'generate a Jenkins pipeline',
+                             )
+    list_parser.add_argument('--sha',
+                             help='Git sha to name the remote image',
+                             )
+
+    list_parser.set_defaults(command=promote_to_registry)
+
+
+def build_promote_command():
+#	docker push docker-paasta.yelpcorp.com:443/$upstream_job_name:paasta-$upstream_git_commit
+    return ""
+
+
+def promote_to_registry(args):
+    """Upload a docker image to a registry"""
+    service_name = args.service
+    try:
+        validate_service_name(service_name)
+    except NoSuchService as service_not_found:
+        print service_not_found
+        sys.exit(1)
+
+    try:
+        cmd = build_promote_command()
+
+        print "INFO: Executing fab_repo %s" % cmd
+        subprocess.check_call([cmd])
+
+    except subprocess.CalledProcessError as subprocess_error:
+        print "%s\nFailed to promote image" % subprocess_error
+        sys.exit(1)
+
---
+++ src/tests/paasta_cli/test_cmds_promote_to_registry.py
@@ -0,0 +1,108 @@
+import sys
+from StringIO import StringIO
+from subprocess import CalledProcessError
+
+from mock import patch
+from pytest import raises
+
+from paasta_tools.paasta_cli.cmds.generate_pipeline \
+    import paasta_generate_pipeline
+from paasta_tools.paasta_cli.paasta_cli import parse_args
+from paasta_tools.paasta_cli.utils import NoSuchService
+
+
+@patch('paasta_tools.paasta_cli.cmds.generate_pipeline.validate_service_name')
+@patch('paasta_tools.paasta_cli.cmds.generate_pipeline.guess_service_name')
+@patch('sys.stdout', new_callable=StringIO)
+def test_generate_pipeline_service_not_found(
+        mock_stdout, mock_guess_service_name, mock_validate_service_name):
+    # paasta generate cannot guess service name and none is provided
+
+    mock_guess_service_name.return_value = 'not_a_service'
+    mock_validate_service_name.side_effect = NoSuchService(None)
+
+    sys.argv = ['./paasta_cli', 'generate-pipeline']
+    parsed_args = parse_args()
+    expected_output = "%s\n" % NoSuchService.GUESS_ERROR_MSG
+
+    # Fail if exit(1) does not get called
+    with raises(SystemExit) as sys_exit:
+        paasta_generate_pipeline(parsed_args)
+
+    output = mock_stdout.getvalue()
+    assert sys_exit.value.code == 1
+    assert output == expected_output
+
+
+@patch('paasta_tools.paasta_cli.cmds.generate_pipeline.validate_service_name')
+@patch('paasta_tools.paasta_cli.cmds.generate_pipeline.guess_service_name')
+@patch('paasta_tools.paasta_cli.cmds.generate_pipeline.subprocess')
+def test_generate_pipeline_subprocess1_fail_no_opt_args(
+        mock_subprocess, mock_guess_service_name, mock_validate_service_name):
+    # paasta generate fails on the first subprocess call
+    # service name has to be guessed
+
+    mock_guess_service_name.return_value = 'fake_service'
+    mock_validate_service_name.return_value = None
+    mock_subprocess.check_call.side_effect = [
+        CalledProcessError(1, 'jenkins cmd 1'), 0]
+    sys.argv = ['./paasta_cli', 'generate-pipeline']
+    parsed_args = parse_args()
+
+    with raises(CalledProcessError):
+        paasta_generate_pipeline(parsed_args)
+
+
+@patch('paasta_tools.paasta_cli.cmds.generate_pipeline.validate_service_name')
+@patch('paasta_tools.paasta_cli.cmds.generate_pipeline.guess_service_name')
+@patch('paasta_tools.paasta_cli.cmds.generate_pipeline.subprocess')
+def test_generate_pipeline_subprocess2_fail_with_opt_args(
+        mock_subprocess, mock_guess_service_name, mock_validate_service_name):
+    # paasta generate fails on the second subprocess call
+    # service name provided as arg
+
+    mock_guess_service_name.return_value = 'fake_service'
+    mock_validate_service_name.return_value = None
+    mock_subprocess.check_call.side_effect = [
+        0, CalledProcessError(1, 'jenkins cmd 2')]
+
+    sys.argv = [
+        './paasta_cli', 'generate-pipeline', '--service', 'fake_service']
+    parsed_args = parse_args()
+
+    with raises(CalledProcessError):
+        paasta_generate_pipeline(parsed_args)
+
+
+@patch('paasta_tools.paasta_cli.cmds.generate_pipeline.validate_service_name')
+@patch('paasta_tools.paasta_cli.cmds.generate_pipeline.guess_service_name')
+@patch('paasta_tools.paasta_cli.cmds.generate_pipeline.subprocess')
+def test_generate_pipeline_success_no_opts(
+        mock_subprocess, mock_guess_service_name, mock_validate_service_name):
+    # paasta generate succeeds when service name must be guessed
+
+    mock_guess_service_name.return_value = 'fake_service'
+    mock_validate_service_name.return_value = None
+    mock_subprocess.check_call.side_effect = [
+        0, 0]
+
+    sys.argv = [
+        './paasta_cli', 'generate-pipeline']
+    parsed_args = parse_args()
+    assert paasta_generate_pipeline(parsed_args) is None
+
+
+@patch('paasta_tools.paasta_cli.cmds.generate_pipeline.validate_service_name')
+@patch('paasta_tools.paasta_cli.cmds.generate_pipeline.subprocess')
+def test_generate_pipeline_success_with_opts(
+        mock_subprocess, mock_validate_service_name):
+    # paasta generate succeeds when service name provided as arg
+
+    mock_validate_service_name.return_value = None
+    mock_subprocess.check_call.side_effect = [
+        0, 0]
+
+    sys.argv = [
+        './paasta_cli', 'generate-pipeline', '--service', 'fake_service']
+    parsed_args = parse_args()
+    assert paasta_generate_pipeline(parsed_args) is None
