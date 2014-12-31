4a84f480ce80bfafb59b15ecb8a805c08477ce1f
Tyler Roscoe <troscoe@yelp.com>
2014-12-30 16:13:37 -0800

    Delete more unneeded boilerplate

--- src/paasta_tools/paasta_cli/cmds/promote_to_registry.py
+++ src/paasta_tools/paasta_cli/cmds/promote_to_registry.py
@@ -6,7 +6,6 @@ image to a registry.
 import subprocess
 import sys
 
-from paasta_tools.paasta_cli.utils import NoSuchService
 from paasta_tools.paasta_cli.utils import validate_service_name
 
 
@@ -19,32 +18,32 @@ def add_subparser(subparsers):
     list_parser.add_argument('-s', '--service',
                              help='Name of service for which you wish to '
                                   'generate a Jenkins pipeline',
+                             required=True,
                              )
-    list_parser.add_argument('--sha',
+    list_parser.add_argument('-c', '--commit',
                              help='Git sha to name the remote image',
+                             required=True,
                              )
 
     list_parser.set_defaults(command=paasta_promote_to_registry)
 
 
-def build_promote_command():
-    # docker push docker-paasta.yelpcorp.com:443/$upstream_job_name:paasta-$upstream_git_commit
-    return ""
+def build_promote_command(upstream_job_name, upstream_git_commit):
+    return "docker push docker-paasta.yelpcorp.com:443/%s:paasta-%s" % (
+        upstream_job_name,
+        upstream_git_commit,
+    )
 
 
 def paasta_promote_to_registry(args):
     """Upload a docker image to a registry"""
     service_name = args.service
-    try:
-        validate_service_name(service_name)
-    except NoSuchService as service_not_found:
-        print service_not_found
-        sys.exit(1)
+    validate_service_name(service_name)
 
-    cmd = build_promote_command()
+    cmd = build_promote_command(service_name, args.commit)
     try:
-        print "INFO: Executing fab_repo %s" % cmd
+        print "INFO: Executing promote command '%s'" % cmd
         subprocess.check_call([cmd])
     except subprocess.CalledProcessError as subprocess_error:
-        print "%s\nFailed to promote image" % subprocess_error
+        print "ERROR: Failed to promote image:\n%s" % subprocess_error
         sys.exit(1)
--- src/tests/paasta_cli/test_cmds_promote_to_registry.py
+++ src/tests/paasta_cli/test_cmds_promote_to_registry.py
@@ -1,33 +1,23 @@
 import sys
-from StringIO import StringIO
 from subprocess import CalledProcessError
 
 from mock import patch
 from pytest import raises
 
+from paasta_tools.paasta_cli.cmds.promote_to_registry import build_promote_command
 from paasta_tools.paasta_cli.cmds.promote_to_registry import paasta_promote_to_registry
 from paasta_tools.paasta_cli.paasta_cli import parse_args
-from paasta_tools.paasta_cli.utils import NoSuchService
 
 
-@patch('paasta_tools.paasta_cli.cmds.promote_to_registry.validate_service_name', autospec=True)
-@patch('sys.stdout', new_callable=StringIO)
-def test_promote_to_registry_service_not_found(
-    mock_stdout,
-    mock_validate_service_name,
-):
-    mock_validate_service_name.side_effect = NoSuchService(None)
-
-    sys.argv = ['./paasta_cli', 'promote-to-registry']
-    parsed_args = parse_args()
-    expected_output = "%s\n" % NoSuchService.GUESS_ERROR_MSG
-
-    with raises(SystemExit) as sys_exit:
-        paasta_promote_to_registry(parsed_args)
-
-    output = mock_stdout.getvalue()
-    assert sys_exit.value.code == 1
-    assert output == expected_output
+def test_build_promote_command():
+    upstream_job_name = "fake_upstream_job_name"
+    upstream_git_commit = "fake_upstream_git_commit"
+    expected = "docker push docker-paasta.yelpcorp.com:443/%s:paasta-%s" % (
+        upstream_job_name,
+        upstream_git_commit,
+    )
+    actual = build_promote_command(upstream_job_name, upstream_git_commit)
+    assert actual == expected
 
 
 @patch('paasta_tools.paasta_cli.cmds.promote_to_registry.validate_service_name', autospec=True)
@@ -39,7 +29,7 @@ def test_promote_to_registry_subprocess_fail(
     mock_subprocess.check_call.side_effect = [
         CalledProcessError(1, 'jenkins cmd 1'), 0]
     sys.argv = [
-        './paasta_cli', 'promote-to-registry', '--service', 'unused', '--sha', 'unused',
+        './paasta_cli', 'promote-to-registry', '--service', 'unused', '--commit', 'unused',
     ]
     parsed_args = parse_args()
 
@@ -56,7 +46,7 @@ def test_promote_to_registry_success(
     mock_subprocess.check_call.return_value = 0
 
     sys.argv = [
-        './paasta_cli', 'promote-to-registry', '--service', 'unused', '--sha', 'unused',
+        './paasta_cli', 'promote-to-registry', '--service', 'unused', '--commit', 'unused',
     ]
     parsed_args = parse_args()
     assert paasta_promote_to_registry(parsed_args) is None
@@ -71,7 +61,7 @@ def test_promote_to_registry_success_with_opts(
     mock_subprocess.check_call.return_value = 0
 
     sys.argv = [
-        './paasta_cli', 'promote-to-registry', '--service', 'fake_service', '--sha', 'deadbeef',
+        './paasta_cli', 'promote-to-registry', '--service', 'fake_service', '--commit', 'deadbeef',
     ]
     parsed_args = parse_args()
     assert paasta_promote_to_registry(parsed_args) is None
