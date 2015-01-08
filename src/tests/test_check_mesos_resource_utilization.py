fc4c0a5c75bb998fe0f1f371d4ed698665a07440
Kyle Anderson <kwa@yelp.com>
2015-01-07 18:17:13 -0800

    Added mesos cluster utilization sensu check for PAASTA-124

---
+++ src/tests/test_check_mesos_resource_utilization.py
@@ -0,0 +1,48 @@
+import mock
+import contextlib
+from paasta_tools import check_mesos_resource_utilization
+
+
+def test_check_thresholds_mem_over():
+    with contextlib.nested(
+        mock.patch('paasta_tools.check_mesos_resource_utilization.fetch_mesos_stats'),
+        mock.patch('paasta_tools.check_mesos_resource_utilization.send_event'),
+    ) as (
+        mock_fetch_mesos_stats,
+        mock_send_event,
+    ):
+        mock_fetch_mesos_stats.return_value = {'mem_percent': 100, 'cpus_percent': 0}
+        actual = check_mesos_resource_utilization.check_thresholds('90')
+        assert 'CRITICAL: Memory' in actual
+        assert 'OK: CPU' in actual
+        mock_send_event.assert_called_once_with(2, mock.ANY)
+
+
+def test_check_thresholds_cpu_over():
+    with contextlib.nested(
+        mock.patch('paasta_tools.check_mesos_resource_utilization.fetch_mesos_stats'),
+        mock.patch('paasta_tools.check_mesos_resource_utilization.send_event'),
+    ) as (
+        mock_fetch_mesos_stats,
+        mock_send_event,
+    ):
+        mock_fetch_mesos_stats.return_value = {'mem_percent': 10, 'cpus_percent': 100}
+        actual = check_mesos_resource_utilization.check_thresholds('90')
+        assert 'CRITICAL: CPU' in actual
+        assert 'OK: Memory' in actual
+        mock_send_event.assert_called_once_with(2, mock.ANY)
+
+
+def test_check_thresholds_ok():
+    with contextlib.nested(
+        mock.patch('paasta_tools.check_mesos_resource_utilization.fetch_mesos_stats'),
+        mock.patch('paasta_tools.check_mesos_resource_utilization.send_event'),
+    ) as (
+        mock_fetch_mesos_stats,
+        mock_send_event,
+    ):
+        mock_fetch_mesos_stats.return_value = {'mem_percent': 10, 'cpus_percent': 10}
+        actual = check_mesos_resource_utilization.check_thresholds('90')
+        assert 'OK: CPU' in actual
+        assert 'OK: Memory' in actual
+        mock_send_event.assert_called_once_with(0, mock.ANY)
