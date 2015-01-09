70a241eeebffb5354be589bf01b8261550e58d27
Kyle Anderson <kwa@yelp.com>
2015-01-09 13:51:00 -0800

    Released 0.8.6-yelp1 via make release

--- src/setup.py
+++ src/setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name           = 'paasta-tools',
-    version        = '0.8.5',
+    version        = '0.8.6',
     provides       = ["paasta_tools"],
     author         = 'Kyle Anderson',
     author_email   = 'kwa@yelp.com',
