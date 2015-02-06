6bbeeb650c5ece56235ceb439a890b395966f04f
Evan Krall <krall@yelp.com>
2015-02-06 00:58:49 -0800

    Extend replication_utils to be able to give me a list of (ip, port) for a given service / namespace.

--- paasta_tools/monitoring/replication_utils.py
+++ paasta_tools/monitoring/replication_utils.py
@@ -12,14 +12,42 @@ def get_replication_for_services(synapse_host_port, service_names):
     :param service_names: A list of strings that are the service names
                           that should be checked for replication.
 
-    :returns available_instances: A dictionary mapping the service names
+    :returns available_instance_counts: A dictionary mapping the service names
                                   to an integer number of available
                                   replicas
     :returns None: If it cannot connect to the specified synapse_host_port
     """
+    available_instances = get_all_registered_ip_ports_for_services(
+        synapse_host_port,
+        service_names
+    )
+
+    return dict([
+        (service_name, len(instances))
+        for (service_name, instances)
+        in available_instances.iteritems()
+    ])
+
+
+def get_all_registered_ip_ports_for_services(synapse_host_port, service_names):
+    """Returns the ips and ports of all registered instances for the
+    provided services.
+
+    This check is intended to be used with an haproxy load balancer, and
+    relies on the implementation details of that choice.
+
+    :param synapse_host_port: A string in host:port format that this check
+                              should contact for replication information.
+    :param service_names: A list of strings that are the service names
+                          that should be checked for replication.
+
+    :returns available_instances: A dictionary mapping the service names
+                                  to a list of (ip, port) tuples.
+    :returns None: If it cannot connect to the specified synapse_host_port
+    """
     reader = retrieve_haproxy_csv(synapse_host_port)
 
-    available_instances = dict([(service_name, 0) for
+    available_instances = dict([(service_name, []) for
                                 service_name in service_names])
     for line in reader:
         # clean up two irregularities of the CSV output, relative to
@@ -35,6 +63,8 @@ def get_replication_for_services(synapse_host_port, service_names):
         if (service in service_names and
                 slave not in ('FRONTEND', 'BACKEND') and
                 str(line['status']).startswith('UP')):
-            available_instances[service] += 1
+            ip, port_hostname = slave.split(':', 1)
+            port, _ = port_hostname.split('_', 1)
+            available_instances[service].append((ip, int(port)))
 
     return available_instances
--- tests/monitoring/test_replication_utils.py
+++ tests/monitoring/test_replication_utils.py
@@ -3,7 +3,10 @@ import os
 import mock
 import requests
 
-from paasta_tools.monitoring.replication_utils import get_replication_for_services
+from paasta_tools.monitoring.replication_utils import (
+    get_replication_for_services,
+    get_all_registered_ip_ports_for_services,
+)
 
 
 def test_get_replication_for_service():
@@ -28,3 +31,27 @@ def test_get_replication_for_service():
             'service4': 3
         }
         assert expected == replication_result
+
+
+def test_get_all_registered_ip_ports_for_services():
+    testdir = os.path.dirname(os.path.realpath(__file__))
+    testdata = os.path.join(testdir, 'haproxy_snapshot.txt')
+    with open(testdata, 'r') as fd:
+        mock_haproxy_data = fd.read()
+
+    mock_response = mock.Mock()
+    mock_response.text = mock_haproxy_data
+    mock_get = mock.Mock(return_value=(mock_response))
+
+    with mock.patch.object(requests.Session, 'get', mock_get):
+        replication_result = get_all_registered_ip_ports_for_services(
+            'foo',
+            ['service1', 'service2', 'service3', 'service4']
+        )
+
+        expected = [
+            ('10.46.2.32', 14902),
+            ('10.46.1.32', 14902),
+            ('10.46.3.32', 14902),
+        ]
+        assert set(replication_result['service4']) == set(expected)
