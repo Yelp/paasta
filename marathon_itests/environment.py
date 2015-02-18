057fd8ef77a27dae43bf4ded240f8d802f0643bc
Evan Krall <krall@yelp.com>
2015-02-17 18:13:37 -0800

    Document why the code in after_scenario exists.

--- marathon_itests/environment.py
+++ marathon_itests/environment.py
@@ -1,11 +1,14 @@
 import time
 
+
 def before_all(context):
     # TODO: Maybe be smarter and unblock when the marathon port is open?
     print "Sleeping 10 seconds to allow marathon to start up."
     time.sleep(10)
 
+
 def after_scenario(context, scenario):
+    """If a marathon client object exists in our context, delete any apps in Marathon and wait until they die."""
     if context.client:
         while True:
             apps = context.client.list_apps()
