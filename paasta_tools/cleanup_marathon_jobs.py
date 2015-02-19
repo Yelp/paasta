6046e9b7f14e3f26766b349bdafd89449130e4c6
Evan Krall <krall@yelp.com>
2015-02-18 21:44:29 -0800

    Change cleanup_marathon_jobs to make sense in the async bounce world.

--- paasta_tools/cleanup_marathon_jobs.py
+++ paasta_tools/cleanup_marathon_jobs.py
@@ -2,15 +2,12 @@
 """
 Usage: ./cleanup_marathon_jobs.py [options]
 
-Clean up marathon jobs/apps that aren't current (i.e. some part of the
-final marathon job configuration has changed) by deleting them.
+Clean up marathon apps that aren't supposed to run on this cluster by deleting them.
 
-Gets the current job/app list from marathon, and then a 'valid_app_list'
-via marathon_tools.get_marathon_services_for_cluster and then
-compiling the complete marathon job configuration for that service and
-calculating the MD5 hash that would've been appended to that service.
+Gets the current app list from marathon, and then a 'valid_app_list'
+via marathon_tools.get_marathon_services_for_cluster
 
-If a job/app in the marathon app list isn't in the valid_app_list, it's
+If an app in the marathon app list isn't in the valid_app_list, it's
 deleted.
 
 Command line options:
@@ -43,26 +40,6 @@ def parse_args():
     return args
 
 
-def get_valid_app_list(marathon_config, soa_dir):
-    """Get the 'valid_app_list' by calling marathon_tools.get_marathon_services_for_cluster
-    and then calculating each service's current configuration via its soa_dir configs in
-    order to get the current MD5 hash that should be on an app's id.
-
-    :param marathon_config: A dict of the marathon_configuration
-    :param soa_dir: The SOA config directory to read from
-    :returns: A list of valid app ids"""
-    log.info("Getting desired app list from configs")
-    cluster_app_list = marathon_tools.get_marathon_services_for_cluster(soa_dir=soa_dir)
-    valid_app_list = []
-    for name, instance in cluster_app_list:
-        try:
-            app_id = marathon_tools.get_app_id(name, instance, marathon_config, soa_dir=soa_dir)
-            valid_app_list.append(app_id)
-        except marathon_tools.NoDockerImageError:
-            pass
-    return valid_app_list
-
-
 def cleanup_apps(soa_dir):
     """Clean up old or invalid jobs/apps from marathon. Retrieves
     both a list of apps currently in marathon and a list of valid
@@ -74,18 +51,22 @@ def cleanup_apps(soa_dir):
     log.info("Connecting to marathon")
     client = marathon_tools.get_marathon_client(marathon_config['url'], marathon_config['user'],
                                                 marathon_config['pass'])
-    valid_app_list = get_valid_app_list(marathon_config, soa_dir)
-    app_ids = marathon_tools.list_all_marathon_app_ids(client)
-    for app_id in app_ids:
-        log.info("Checking app id %s", app_id)
-        if not any([app_id == deployed_id for deployed_id in valid_app_list]):
+
+    valid_services = marathon_tools.get_marathon_services_for_cluster(soa_dir=soa_dir)
+    valid_short_app_ids = [marathon_tools.compose_job_id(name, instance) for (name, instance) in valid_services]
+    running_app_ids = marathon_tools.list_all_marathon_app_ids(client)
+
+    for app_id in running_app_ids:
+        log.debug("Checking app id %s", app_id)
+        short_app_id = marathon_tools.remove_tag_from_job_id(app_id)
+        if short_app_id not in valid_short_app_ids:
             try:
                 log.warn("%s appears to be old; attempting to delete", app_id)
-                srv_instance = marathon_tools.remove_tag_from_job_id(app_id).replace('--', '_')
+                srv_instance = short_app_id.replace('--', '_')
                 with bounce_lib.bounce_lock_zookeeper(srv_instance):
                     bounce_lib.delete_marathon_app(app_id, client)
             except IOError:
-                log.info("%s is being bounced, skipping", app_id)
+                log.debug("%s is being bounced, skipping", app_id)
                 continue  # It's being bounced, don't touch it!
 
 
