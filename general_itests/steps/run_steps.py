81205c82829fb5a5cc3653370740f7821ad740c0
Zsolt Tripolszky <tripy@yelp.com>
2015-02-23 17:25:51 -0800

    Fix review issues

--- general_itests/steps/run_steps.py
+++ general_itests/steps/run_steps.py
@@ -3,9 +3,9 @@ from paasta_tools.utils import _run
 
 @when(u'we run a trivial command with timeout {timeout} seconds')
 def run_command(context, timeout):
-    fake_cmd = 'sleep 10'
+    fake_cmd = 'sleep 1'
     context.rc, context.output = _run(fake_cmd, timeout=float(timeout))
 
 @then(u'the command is killed with signal {killsignal}')
 def check_exit_code(context, killsignal):
-    assert context.rc == -getattr(signal, killsignal)
+    assert context.rc == -1 * getattr(signal, killsignal)
