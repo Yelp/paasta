d55e116042d39fb91b7a2322c2595448e8fb5cad
Zsolt Tripolszky <tripy@yelp.com>
2015-03-10 05:51:05 -0700

    Print out the duplicate images

--- paasta_tools/utils.py
+++ paasta_tools/utils.py
@@ -378,7 +378,7 @@ def check_docker_image(service_name, tag):
     image_name = build_docker_image_name(service_name)
     docker_tag = build_docker_tag(service_name, tag)
     images = docker_client.images(name=image_name)
-    result = len([image for image in images if docker_tag in image['RepoTags']])
-    if result > 1:
-        raise ValueError('More than one docker image found with tag %s' % docker_tag)
-    return result == 1
+    result = [image for image in images if docker_tag in image['RepoTags']]
+    if len(result) > 1:
+        raise ValueError('More than one docker image found with tag %s\n%s' % docker_tag, result)
+    return len(result) == 1
