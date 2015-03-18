7e2d06bdd1fcbeecaeb89a2b3a83876d71ad57ca
Zsolt Tripolszky <tripy@yelp.com>
2015-03-18 11:15:22 -0700

    Fix tests

--- tests/paasta_cli/test_cmds_mark_for_deployment.py
+++ tests/paasta_cli/test_cmds_mark_for_deployment.py
@@ -1,7 +1,13 @@
-from mock import MagicMock
 from mock import patch
 
 from paasta_tools.paasta_cli.cmds.mark_for_deployment import paasta_mark_for_deployment
+
+
+class args:
+    clusterinstance = 'cluster.instance'
+    service = 'test_service'
+    git_url = 'git://false.repo/services/test_services'
+    commit = 'fake-hash'
 
 
 @patch('paasta_tools.paasta_cli.cmds.mark_for_deployment._run', autospec=True)
@@ -15,9 +21,6 @@ def test_mark_for_deployment_run_fail(
     mock_run,
 ):
     mock_run.return_value = (1, 'Exterminate!')
-    args = MagicMock()
-    args.clusterinstance = 'cluster.instance'
-    args.service = 'test_service'
     paasta_mark_for_deployment(args)
     mock_exit.assert_called_once_with(1)
 
@@ -33,7 +36,4 @@ def test_mark_for_deployment_success(
     mock_run,
 ):
     mock_run.return_value = (0, 'Interminate!')
-    args = MagicMock()
-    args.clusterinstance = 'cluster.instance'
-    args.service = 'test_service'
     assert paasta_mark_for_deployment(args) is None
