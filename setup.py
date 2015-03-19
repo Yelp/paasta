e90a2f3992228f063c773792bb8e66b5ca6a7ac1
Evan Krall <krall@yelp.com>
2015-03-19 14:03:44 -0700

    Released 0.9.29-yelp1 via make release

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name           = 'paasta-tools',
-    version        = '0.9.28',
+    version        = '0.9.29',
     provides       = ["paasta_tools"],
     author         = 'Kyle Anderson',
     author_email   = 'kwa@yelp.com',
