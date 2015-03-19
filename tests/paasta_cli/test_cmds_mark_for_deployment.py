165c3bda7fe825a143d052157952088e55cdd96b
Zsolt Tripolszky <tripy@yelp.com>
2015-03-19 05:26:17 -0700

    Fix variablename

--- tests/paasta_cli/test_cmds_mark_for_deployment.py
+++ tests/paasta_cli/test_cmds_mark_for_deployment.py
@@ -3,7 +3,7 @@ from mock import patch
 from paasta_tools.paasta_cli.cmds.mark_for_deployment import paasta_mark_for_deployment
 
 
-class args:
+class fake_args:
     clusterinstance = 'cluster.instance'
     service = 'test_service'
     git_url = 'git://false.repo/services/test_services'
@@ -21,7 +21,7 @@ def test_mark_for_deployment_run_fail(
     mock_run,
 ):
     mock_run.return_value = (1, 'Exterminate!')
-    paasta_mark_for_deployment(args)
+    paasta_mark_for_deployment(fake_args)
     mock_exit.assert_called_once_with(1)
 
 
@@ -36,4 +36,4 @@ def test_mark_for_deployment_success(
     mock_run,
 ):
     mock_run.return_value = (0, 'Interminate!')
-    assert paasta_mark_for_deployment(args) is None
+    assert paasta_mark_for_deployment(fake_args) is None
