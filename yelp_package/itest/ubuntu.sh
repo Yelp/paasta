c0c3f706b8acd45fdb5a88f069e0b52d8093e028
Zsolt Tripolszky <tripy@yelp.com>
2015-04-09 03:06:55 -0700

    Add paasta_metastatus to package itest

--- yelp_package/itest/ubuntu.sh
+++ yelp_package/itest/ubuntu.sh
@@ -12,7 +12,8 @@ deploy_marathon_services
 generate_deployments_json
 check_marathon_services_replication
 generate_services_yaml
-paasta_serviceinit"
+paasta_serviceinit
+paasta_metastatus"
 
 MARATHON_SERVICES="fake_service_uno.main
 fake_service_dos.niam"
