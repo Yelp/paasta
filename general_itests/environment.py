7b2710f326d6b84d164721c0bf7a66eaa90698f8
Tyler Roscoe <troscoe@yelp.com>
2015-04-30 23:17:46 -0700

    getattr ftw

--- general_itests/environment.py
+++ general_itests/environment.py
@@ -1,12 +1,6 @@
 import shutil
 
 
-def before_scenario(context, scenario):
-    # I can't do context.get("tmpdir") so I'll set it here to ensure it exists.
-    # If a test sets it to something (because it creates a tmpdir), we'll clean
-    # it up in after_scenario().
-    context.tmpdir = None
-
 def after_scenario(context, scenario):
-    if context.tmpdir:
+    if getattr(context, "tmpdir"):
         shutil.rmtree(context.tmpdir)
