2597a8ecddccb47d1a85560b1bbd606e80bfe91e
Zsolt Tripolszky <tripy@yelp.com>
2015-05-21 13:08:30 -0700

    Released 0.9.73-yelp1 via make release

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name           = 'paasta-tools',
-    version        = '0.9.72',
+    version        = '0.9.73',
     provides       = ["paasta_tools"],
     author         = 'Kyle Anderson',
     author_email   = 'kwa@yelp.com',
