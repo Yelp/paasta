0cf2607a4f3fa2a28ab11e3562bf170c94d5eea0
Keshav Varma <keshav@yelp.com>
2015-05-26 16:40:59 -0700

    Released 0.10.3-yelp1 via make release

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name           = 'paasta-tools',
-    version        = '0.10.2',
+    version        = '0.10.3',
     provides       = ["paasta_tools"],
     author         = 'Kyle Anderson',
     author_email   = 'kwa@yelp.com',
