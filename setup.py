11f511b718492450a4a739022f70a54f3587ed46
Evan Krall <krall@yelp.com>
2015-05-28 19:11:56 -0700

    Released 0.11.0-yelp1 via make release

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name           = 'paasta-tools',
-    version        = '0.10.4',
+    version        = '0.11.0',
     provides       = ["paasta_tools"],
     author         = 'Kyle Anderson',
     author_email   = 'kwa@yelp.com',
