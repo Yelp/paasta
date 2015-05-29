43c5869c9b415e1fc7c76cf7da921594258bf86a
Keshav Varma <keshav@yelp.com>
2015-05-29 16:32:34 -0700

    fix test

--- tests/test_cleanup_marathon_jobs.py
+++ tests/test_cleanup_marathon_jobs.py
@@ -147,7 +147,7 @@ class TestCleanupMarathonJobs:
                 mock_log.assert_called_once_with(
                     instance='main',
                     service_name='example_service',
-                    level='event',
+                    level='debug',
                     component='deploy',
                     cluster='fake_cluster',
                     line='Exception raised: IOError("foo")'
