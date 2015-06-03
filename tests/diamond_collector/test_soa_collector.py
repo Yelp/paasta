520df192752a5d54229cce618ddc6fa1da750297
Semir Patel <spatel@yelp.com>
2015-06-02 21:14:47 -0700

    Fix for 5s delay introduced in test_soa_collector.py

--- tests/diamond_collector/test_soa_collector.py
+++ tests/diamond_collector/test_soa_collector.py
@@ -23,31 +23,37 @@ def test_collect():
         }
     }
 
-    collector = soa_collector.SOACollector()
+    # This patch is specific to the SOACollector constructor hence the need for
+    # two 'with' levels - ick!
+    with mock.patch.object(soa_collector, 'is_ephemeral_aws_service_instance',
+                           autospec=True,
+                           return_value=False):
 
-    with contextlib.nested(
-        mock.patch.object(marathon_tools, 'get_services_running_here_for_nerve',
-                          autospec=True,
-                          return_value=fake_services),
-        mock.patch.object(collector, 'publish', autospec=True),
-        mock.patch.object(soa_collector, 'get_json_metrics',
-                          autospec=True,
-                          return_value=fake_metrics_data)
-    ) as (
-        get_services_patch,
-        publish_patch,
-        get_json_metrics_patch,
-    ):
-        collector.collect()
+        collector = soa_collector.SOACollector()
 
-        assert publish_patch.call_count == 5
-        publish_patch.assert_any_call('myservice.meters.my_meter.count', 1,
-                                      metric_type='COUNTER', raw_value=1, precision=4)
-        publish_patch.assert_any_call('myservice.meters.my_meter.mean_rate', 2,
-                                      metric_type='GAUGE', raw_value=2, precision=4)
-        publish_patch.assert_any_call('myservice.meters.my_meter.m1_rate', 3,
-                                      metric_type='GAUGE', raw_value=3, precision=4)
-        publish_patch.assert_any_call('myservice.meters.my_meter.m5_rate', 4,
-                                      metric_type='GAUGE', raw_value=4, precision=4)
-        publish_patch.assert_any_call('myservice.meters.my_meter.m15_rate', 5,
-                                      metric_type='GAUGE', raw_value=5, precision=4)
+        with contextlib.nested(
+            mock.patch.object(marathon_tools, 'get_services_running_here_for_nerve',
+                              autospec=True,
+                              return_value=fake_services),
+            mock.patch.object(collector, 'publish', autospec=True),
+            mock.patch.object(soa_collector, 'get_json_metrics',
+                              autospec=True,
+                              return_value=fake_metrics_data),
+        ) as (
+            get_services_patch,
+            publish_patch,
+            get_json_metrics_patch,
+        ):
+            collector.collect()
+
+            assert publish_patch.call_count == 5
+            publish_patch.assert_any_call('myservice.meters.my_meter.count', 1,
+                                          metric_type='COUNTER', raw_value=1, precision=4)
+            publish_patch.assert_any_call('myservice.meters.my_meter.mean_rate', 2,
+                                          metric_type='GAUGE', raw_value=2, precision=4)
+            publish_patch.assert_any_call('myservice.meters.my_meter.m1_rate', 3,
+                                          metric_type='GAUGE', raw_value=3, precision=4)
+            publish_patch.assert_any_call('myservice.meters.my_meter.m5_rate', 4,
+                                          metric_type='GAUGE', raw_value=4, precision=4)
+            publish_patch.assert_any_call('myservice.meters.my_meter.m15_rate', 5,
+                                          metric_type='GAUGE', raw_value=5, precision=4)
