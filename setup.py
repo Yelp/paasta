ccec41aaa5f60a6311b3fc70f9a026d899a4611d
Semir Patel <spatel@yelp.com>
2015-06-02 20:19:00 -0700

    Bump to 0.11.6

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name           = 'paasta-tools',
-    version        = '0.11.5',
+    version        = '0.11.6',
     provides       = ["paasta_tools"],
     author         = 'Kyle Anderson',
     author_email   = 'kwa@yelp.com',
