599a0a848c583757014309fda80101111b2a5768
Zsolt Tripolszky <tripy@yelp.com>
2015-06-03 16:59:13 -0700

    Fix or review

--- marathon_itests/environment.py
+++ marathon_itests/environment.py
@@ -1,39 +1,10 @@
-import sys
-import threading
 import time
 
-import requests
-from fig.cli import command
-
-
-def no_marathon():
-    print 'Failed to connect to marathon, canceling integration tests'
-    sys.exit(1)
+from itest_utils import wait_for_marathon
 
 
 def before_all(context):
-    """Waits for marathon to start. Maximum 30 seconds"""
-    cmd = command.Command()
-    project = cmd.get_project(cmd.get_config_path())
-    service  = project.get_service('marathon')
-    #get_local_port returns '0.0.0.0:<portnumber>'
-    marathon_port = service.get_container().get_local_port(8080).split(':')[1]
-    reqtimer = threading.Timer(30, no_marathon)
-    reqtimer.start()
-    while True:
-        try:
-            print 'Connecting on 127.0.0.1:%s' % marathon_port
-            response = requests.get('http://127.0.0.1:%s/ping' % marathon_port, timeout=5)
-        except (
-            requests.exceptions.ConnectionError,
-            requests.exceptions.Timeout
-        ):
-            time.sleep(5)
-            continue
-        if response.status_code == 200:
-            print "Marathon is up and running!"
-            reqtimer.cancel()
-            break
+    wait_for_marathon()
 
 
 def after_scenario(context, scenario):
---
+++ marathon_itests/itest_utils.py
@@ -0,0 +1,58 @@
+import threading
+import sys
+import time
+
+import requests
+from fig.cli import command
+
+
+def get_fig_service(service_name):
+    """Returns a fig object for the service"""
+    cmd = command.Command()
+    project = cmd.get_project(cmd.get_config_path())
+    return project.get_service(service_name)
+
+
+def get_service_connection_string(service_name):
+    """Given a container name this function returns
+    the host and ephemeral port that you need to use to connect to. For example
+    if you are spinning up a 'web' container that inside listens on 80, this
+    function would return 0.0.0.0:23493 or whatever ephemeral forwarded port
+    it has from fig"""
+    service_port = get_service_internal_port(service_name)
+    return get_fig_service(service_name).get_container().get_local_port(service_port)
+
+
+def get_service_internal_port(service_name):
+    """Gets the exposed port for service_name from fig.yml. If there are
+    multiple ports. It returns the first one."""
+    return get_fig_service(service_name).options['ports'][0]
+
+
+def no_marathon():
+    """Helper function for wait_for_marathon timeout"""
+    print 'Failed to connect to marathon, canceling integration tests'
+    sys.exit(1)
+
+
+def wait_for_marathon():
+    """Waits for marathon to start. Maximum 30 seconds"""
+    # get_service_connection_string returns '0.0.0.0:<portnumber>'
+    marathon_port = get_service_connection_string('marathon').split(':')[1]
+    reqtimer = threading.Timer(30, no_marathon)
+    reqtimer.start()
+    while True:
+        print 'Connecting marathon on 127.0.0.1:%s' % marathon_port
+        try:
+            response = requests.get('http://127.0.0.1:%s/ping' % marathon_port, timeout=5)
+        except (
+            requests.exceptions.ConnectionError,
+            requests.exceptions.Timeout,
+        ):
+            time.sleep(5)
+            continue
+        if response.status_code == 200:
+            reqtimer.cancel()
+            print "Marathon is up and running!"
+            break
+
