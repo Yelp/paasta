6c864366530bf12ca91e9d3f7e99575af89dae95
Robert Johnson <robj@yelp.com>
2015-06-04 14:28:14 -0700

    improve wording of comments and tests

--- paasta_itests/paasta_metastatus.feature
+++ paasta_itests/paasta_metastatus.feature
@@ -1,5 +1,5 @@
 Feature: paasta_metastatus can describe health of cluster
 
-    Scenario: paasta_metastatus exits cleanly if it can't connect to mesos
+    Scenario: paasta_metastatus handles exceptional case of mesos not being available
         Given an unresponsive mesos instance
         Then mesos_tools.fetch_mesos_stats() should raise a MissingMasterException
--- paasta_itests/steps/mesos_steps.py
+++ paasta_itests/steps/mesos_steps.py
@@ -2,8 +2,7 @@ from behave import given
 
 @given('an unresponsive mesos instance')
 def unresponsive_mesos(context):
-    """ This is solely here for explicitness. 
-     We want to highlight that no setup is done as part of the test, 
-     meaning that the mesos cli cannot connect to an instance """
+    """ This is just symbolic and here to be explicit about what's
+    (not) happening """
     pass
 
