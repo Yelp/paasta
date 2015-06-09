e3a400e0fb842edc5d7033c1a0c4d861a855f4a6
Tyler Roscoe <troscoe@yelp.com>
2015-06-08 20:51:22 -0700

    Released 0.11.10-yelp1 via make release

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name           = 'paasta-tools',
-    version        = '0.11.9',
+    version        = '0.11.10',
     provides       = ["paasta_tools"],
     author         = 'Kyle Anderson',
     author_email   = 'kwa@yelp.com',
