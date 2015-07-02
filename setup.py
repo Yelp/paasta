fb5438ca8593bf2dc63e108fa466f6235ff6739f
Christopher Carroux <carroux@yelp.com>
2015-07-01 17:48:31 -0700

    Released 0.12.7-yelp1 via make release

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 setup(
     name           = 'paasta-tools',
     # Don't bump version manually. See `make release` docs in ./Makefile
-    version        = '0.12.6',
+    version        = '0.12.7',
     provides       = ["paasta_tools"],
     author         = 'Kyle Anderson',
     author_email   = 'kwa@yelp.com',
