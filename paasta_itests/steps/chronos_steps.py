63591e2dee02b734f3acfb5a7093764713f06d4f
Kyle Anderson <kwa@yelp.com>
2015-07-09 13:08:59 -0700

    minor adjustments for review feedback

--- paasta_itests/steps/chronos_steps.py
+++ paasta_itests/steps/chronos_steps.py
@@ -15,17 +15,22 @@ def working_chronos(context):
         print "Chronos connection already established"
 
 
+@when(u'we create a trivial chronos job')
+def create_trivial_chronos_job(context):
+    job = {
+        'async': False,
+        'command': 'echo 1',
+        'epsilon': 'PT15M',
+        'name': 'test_chronos_job',
+        'owner': '',
+        'disabled': True,
+        'schedule': 'R/2014-01-01T00:00:00Z/PT60M',
+    }
+    context.chronos_client.add(job)
+
+
 @then(u'we should be able to see it when we list jobs')
 def list_chronos_jobs_has_trivial_job(context):
     jobs = context.chronos_client.list()
     job_names = [ job['name'] for job in jobs ]
     assert 'test_chronos_job' in job_names
-
-
-@when(u'we create a trivial chronos job')
-def create_trivial_chronos_job(context):
-    job = {
-        'async': False, 'command': 'echo 1', 'epsilon': 'PT15M', 'name': 'test_chronos_job',
-        'owner': '', 'disabled': True, 'schedule': 'R/2014-01-01T00:00:00Z/PT60M'
-    }
-    context.chronos_client.add(job)
