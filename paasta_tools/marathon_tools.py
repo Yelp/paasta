f7789b22a8f7a2e584523a78ab22f08bc72f2b47
Robert Johnson <robj@yelp.com>
2015-07-16 09:50:15 -0700

    add wait_for_app_to_launch_tasks function
    this function uses the timeout wrapper to wait for
    tasks to launch belonging to a given app.

--- paasta_tools/marathon_tools.py
+++ paasta_tools/marathon_tools.py
@@ -24,6 +24,7 @@ from paasta_tools.utils import load_system_paasta_config
 from paasta_tools.utils import NoMarathonClusterFoundException
 from paasta_tools.utils import PaastaNotConfigured
 from paasta_tools.utils import PATH_TO_SYSTEM_PAASTA_CONFIG_DIR
+from paasta_tools.utils import timeout
 
 # DO NOT CHANGE ID_SPACER, UNLESS YOU'RE PREPARED TO CHANGE ALL INSTANCES
 # OF IT IN OTHER LIBRARIES (i.e. service_configuration_lib).
@@ -978,6 +979,26 @@ def app_has_tasks(client, app_id, expected_tasks):
         print("no app with id %s found")
         raise e
     return len(tasks) >= expected_tasks
+
+
+@timeout()
+def wait_for_app_to_launch_tasks(client, app_id, expected_tasks):
+    """ Wait for an app to have num_tasks tasks launched. If the app isn't found, then this will swallow the exception
+        and retry. Times out after 30 seconds.
+
+       :param client: The marathon client
+       :param app_id: The app id to which the tasks belong
+       :param num_tasks: The number of tasks to wait for
+    """
+    found = False
+    while not found:
+        try:
+            found = app_has_tasks(client, app_id, expected_tasks)
+        except NotFoundError:
+            print "App with id %s not found" % app_id
+            pass
+        if found:
+            return
 
 
 def create_complete_config(name, instance, marathon_config, soa_dir=DEFAULT_SOA_DIR):
