87d71a11dce5171f0268e4f4b691840c3ff81904
Robert Johnson <robj@yelp.com>
2015-07-16 09:51:10 -0700

    use a common function to wait for tasks to launch

--- paasta_itests/steps/paasta_metastatus_steps.py
+++ paasta_itests/steps/paasta_metastatus_steps.py
@@ -33,23 +33,7 @@ def run_paasta_metastatus_high_cpu(context, app_id):
 @when(u'a task belonging to the app with id "{app_id}" is in the task list')
 def task_is_ready(context, app_id):
     """ wait for a task with a matching task name to be ready. time out in 60 seconds """
-    count = 60
-    tries = 0
-    while tries < count:
-        try:
-            tasks = context.client.list_tasks(app_id=app_id)
-            if len(tasks) > 0:
-                print 'Found task belonging to app %s, started at %s' % (app_id, tasks[0].started_at)
-                return
-            else:
-                print 'Sleeping for 1 second...'
-                tries = tries + 1
-                sleep(1)
-        except NotFoundError:
-            print 'No app found. sleeping for 1 second...'
-            tries = tries + 1
-            sleep(1)
-    raise Exception("Timed out waiting for task with belonging to app with id %s to appear in the task list" % app_id)
+    marathon_tools.wait_for_app_to_launch_tasks(context.client, app_id, 1)
 
 
 @then(u'paasta_metastatus exits with return code "{expected_return_code}" and output "{expected_output}"')
