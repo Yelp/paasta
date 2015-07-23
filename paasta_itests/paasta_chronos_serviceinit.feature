39a160ac738c518ce7b29bb30bda50e3c4b044c9
Tyler Roscoe <troscoe@yelp.com>
2015-07-22 23:29:20 -0700

    Fill in a step

--- paasta_itests/paasta_chronos_serviceinit.feature
+++ paasta_itests/paasta_chronos_serviceinit.feature
@@ -1,6 +1,6 @@
 Feature: paasta_chronos_serviceinit
   Scenario: paasta_chronos_serviceinit can run status
     Given a working paasta cluster
-    When we run the chronos job test-service.main
+    When we run the chronos job test-service.job
     And we wait for it to be deployed
     Then paasta_chronos_serviceinit status should return "Healthy"
--- paasta_itests/steps/paasta_chronos_serviceinit_steps.py
+++ paasta_itests/steps/paasta_chronos_serviceinit_steps.py
@@ -1,11 +1,22 @@
+import sys
+
 from behave import when, then
 
+sys.path.append('../')
+from paasta_tools.utils import _run
 
-@when(u'we run the chronos job test-service.main')
+
+@when(u'we run the chronos job test-service.job')
 def run_chronos_test_job(context):
     pass
 
 
 @then(u'paasta_chronos_serviceinit status should return "Healthy"')
 def status_returns_healthy(context):
-    pass
+    cmd = '../paasta_tools/paasta_chronos_serviceinit.py test-service.job status'
+    print 'Running cmd %s' % cmd
+    (exit_code, output) = _run(cmd)
+    print 'Got exitcode %s with output:\n%s' % (exit_code, output)
+
+    assert exit_code == 0
+    assert "Healthy" in output
