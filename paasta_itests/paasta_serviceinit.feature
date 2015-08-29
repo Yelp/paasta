2557f43979b412c8c5d16b6ab5a39c46068f1ce9
Tyler Roscoe <troscoe@yelp.com>
2015-08-28 23:49:02 -0700

    Collapse paasta_chronos_serviceinit feature and steps into paasta_serviceinit

--- paasta_itests/paasta_serviceinit.feature
+++ paasta_itests/paasta_serviceinit.feature
@@ -1,4 +1,4 @@
-Feature: marathon_serviceinit can control marathon tasks
+Feature: marathon_serviceinit
 
   Scenario: marathon_serviceinit can run status
     Given a working paasta cluster
@@ -11,3 +11,15 @@ Feature: marathon_serviceinit can control marathon tasks
     When we run the marathon job test-service.main
     And we wait for it to be deployed
     Then marathon_serviceinit restart should get new task_ids
+
+
+Feature: paasta_chronos_serviceinit
+
+  Scenario: paasta_chronos_serviceinit can run status
+
+    Given a working paasta cluster
+    When we create a trivial chronos job
+    And the trivial chronos job appears in the job list
+    Then paasta_chronos_serviceinit status should return "Healthy"
+
+# vim: tabstop=2 expandtab shiftwidth=2 softtabstop=2
--- paasta_itests/steps/paasta_serviceinit_steps.py
+++ paasta_itests/steps/paasta_serviceinit_steps.py
@@ -7,6 +7,7 @@ import mock
 sys.path.append('../')
 import paasta_tools
 from paasta_tools import marathon_serviceinit
+from paasta_tools.utils import _run
 
 
 @when(u'we run the marathon job test-service.main')
@@ -43,7 +44,7 @@ def status_marathon_job_returns_healthy(context):
 
 
 @then(u'marathon_serviceinit restart should get new task_ids')
-def restart_gets_new_task_ids(context):
+def marathon_restart_gets_new_task_ids(context):
     normal_instance_count = 1
     cluster = context.system_paasta_config['cluster']
     app_id = 'test-service.main'
@@ -65,5 +66,27 @@ def restart_gets_new_task_ids(context):
     print "Tasks before the restart: %s" % old_tasks
     print "Tasks after  the restart: %s" % new_tasks
     assert old_tasks != new_tasks
+
+
+@then(u'paasta_chronos_serviceinit status should return "Healthy"')
+def chronos_status_returns_healthy(context):
+    # Another fun from using space as the separate is parsing the service+job
+    # string on the command line! For now, the simplest thing that can possibly
+    # work.
+    cmd = '../paasta_tools/paasta_chronos_serviceinit.py test-service\ job status'
+    print 'Running cmd %s' % cmd
+    (exit_code, output) = _run(cmd)
+    print 'Got exitcode %s with output:\n%s' % (exit_code, output)
+
+    assert exit_code == 0
+    # This doesn't work yet for a few reasons:
+    #
+    # * See note above about space vs dot vs SPACER. The resulting job can't
+    # please all of its masters so this test is doomed to fail no matter what.
+    # (As written at least it doesn't hang forever trying to kill the
+    # job-with-a-space that it can't kill.)
+    #
+    # * It's not supposed to as I did BDD to get here and I'm not done yet.
+    # assert "Healthy" in output
 
 # vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
