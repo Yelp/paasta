b7bc3980b04e3bd525397bd65bf25932f9d82b05
Robert Johnson <robj@yelp.com>
2015-09-10 06:40:34 -0700

    rename known/unknown to configured/unconfigured and remove quotes

--- paasta_itests/steps/cleanup_chronos_job_steps.py
+++ paasta_itests/steps/cleanup_chronos_job_steps.py
@@ -7,7 +7,7 @@ from paasta_tools.utils import _run
 from paasta_tools.chronos_tools import compose_job_id
 
 
-@when('I launch {num_jobs} "{state}" chronos jobs with service "{service}" with chronos instance "{job}" and differing tags')
+@when('I launch {num_jobs} {state} chronos jobs with service "{service}" with chronos instance "{job}" and differing tags')
 def launch_jobs(context, num_jobs, state, service, job):
     client = context.chronos_client
     jobs = [{
@@ -27,10 +27,10 @@ def launch_jobs(context, num_jobs, state, service, job):
             print 'Error creating test job: %s' % json.dumps(job)
             raise
 
-    if state == "known":
-        context.known_job_names = [job['name'] for job in jobs]
-    elif state == "unknown":
-        context.unknown_job_names = [job['name'] for job in jobs]
+    if state == "configured":
+        context.configured_job_names = [job['name'] for job in jobs]
+    elif state == "unconfigured":
+        context.unconfigured_job_names = [job['name'] for job in jobs]
 
 
 @when('I launch {num_jobs} non-paasta jobs')
@@ -59,13 +59,13 @@ def check_cleanup_chronos_jobs_output(context, expected_return_code):
     cmd = '../paasta_tools/cleanup_chronos_jobs.py --soa-dir %s' % context.soa_dir
     print cmd
     (exit_code, output) = _run(cmd)
-    print context.unknown_job_names
+    print context.unconfigured_job_names
     print 'Got exitcode %s with output:\n%s' % (exit_code, output)
 
     assert exit_code == int(expected_return_code)
     assert "Successfully Removed Tasks (if any were running) for:" in output
     assert "Successfully Removed Jobs:" in output
-    for job in context.unknown_job_names:
+    for job in context.unconfigured_job_names:
         assert '  %s' % job in output
 
 
@@ -76,23 +76,23 @@ def non_paasta_jobs(context):
     assert all([job_name in running_job_names for job_name in context.non_paasta_jobs])
 
 
-@then('the "{state}" chronos jobs are not in the job list')
+@then('the {state} chronos jobs are not in the job list')
 def not_in_job_list(context, state):
     jobs = context.chronos_client.list()
-    if state == "known":
-        state_list = context.known_job_names
-    elif state == "unknown":
-        state_list = context.unknown_job_names
+    if state == "configured":
+        state_list = context.configured_job_names
+    elif state == "unconfigured":
+        state_list = context.unconfigured_job_names
     assert_no_job_names_in_list(state_list, jobs)
 
 
-@then('the "{state}" chronos jobs are in the job list')
+@then('the {state} chronos jobs are in the job list')
 def all_in_job_list(context, state):
     jobs = context.chronos_client.list()
-    if state == "known":
-        state_list = context.known_job_names
-    elif state == "unknown":
-        state_list = context.unknown_job_names
+    if state == "configured":
+        state_list = context.configured_job_names
+    elif state == "unconfigured":
+        state_list = context.unconfigured_job_names
     assert_all_job_names_in_list(state_list, jobs)
 
 
