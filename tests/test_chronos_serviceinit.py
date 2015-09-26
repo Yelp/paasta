e5d7c92116b5fa3339d8c2a161d29eccd9716d30
Tyler Roscoe <troscoe@yelp.com>
2015-09-25 23:32:05 -0700

    whitespace

--- tests/test_chronos_serviceinit.py
+++ tests/test_chronos_serviceinit.py
@@ -181,8 +181,7 @@ def test_format_chronos_job_status_success_and_then_failure():
 
 
 def test_format_chronos_job_mesos_happy():
-    example_job = {
-    }
+    example_job = {}
     desired_state = ''
     actual = chronos_serviceinit.format_chronos_job_status(example_job, desired_state)
     print "##############################################################"
