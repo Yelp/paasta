adad65da6c4dbd8128d7ff2fcecf5ed7ff2f09a2
Kyle Anderson <kwa@yelp.com>
2015-12-03 14:17:51 -0800

    Fix version string on paasta manpage generation

--- build-manpages.sh
+++ build-manpages.sh
@@ -21,7 +21,7 @@ mkdir -p /nail/etc/services
 mkdir -p docs/man/
 . .tox/manpages/bin/activate
 
-VERSION=`./paasta_tools/paasta_cli/paasta_cli.py version`
+VERSION=`./paasta_tools/paasta_cli/paasta_cli.py --version`
 
 function build_man() {
     COMMAND=$1
