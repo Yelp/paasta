920174f672752e224d246e1d0c02aca505a0633c
Robert Johnson <robj@yelp.com>
2016-01-07 06:51:45 -0800

    fn for writing soa config for dependent/scheduled jobs

--- paasta_itests/steps/setup_steps.py
+++ paasta_itests/steps/setup_steps.py
@@ -136,17 +136,34 @@ def working_paasta_cluster(context):
     }, 'volumes.json')
 
 
-@given(u'I have yelpsoa-configs for the service "{service}" with {disabled} chronos instance "{instance}"')
+@given(u'we have yelpsoa-configs for the service "{service}" with {disabled} scheduled chronos instance "{instance}"')
 def write_soa_dir_chronos_instance(context, service, disabled, instance):
     soa_dir = mkdtemp()
     desired_disabled = (disabled == 'disabled')
     if not os.path.exists(os.path.join(soa_dir, service)):
         os.makedirs(os.path.join(soa_dir, service))
     with open(os.path.join(soa_dir, service, 'chronos-%s.yaml' % context.cluster), 'w') as f:
-        f.write(yaml.dump({
-            "%s" % instance: {
+        f.write(yaml.safe_dump({
+            instance: {
                 'schedule': 'R/2000-01-01T16:20:00Z/PT60S',
-                'command': 'echo "Taking a nap..." && sleep 1m && echo "Nap time over, back to work"',
+                'cmd': 'echo "Taking a nap..." && sleep 1m && echo "Nap time over, back to work"',
+                'monitoring': {'team': 'fake_team'},
+                'disabled': desired_disabled,
+            }
+        }))
+    context.soa_dir = soa_dir
+
+@given(u'we have yelpsoa-configs for the service "{service}" with {disabled} dependent chronos instance "{instance}" and parent "{parent}"')
+def write_soa_dir_dependent_chronos_instance(context, service, disabled, instance, parent):
+    soa_dir = mkdtemp()
+    desired_disabled = (disabled == 'disabled')
+    if not os.path.exists(os.path.join(soa_dir, service)):
+        os.makedirs(os.path.join(soa_dir, service))
+    with open(os.path.join(soa_dir, service, 'chronos-%s.yaml' % context.cluster), 'w') as f:
+        f.write(yaml.safe_dump({
+            instance: {
+                'parent':  parent,
+                'cmd': 'echo "Taking a nap..." && sleep 1m && echo "Nap time over, back to work"',
                 'monitoring': {'team': 'fake_team'},
                 'disabled': desired_disabled,
             }
