a5dcaf46caae21123bc28a28f01d8c52544044ab
Matthew Smith <matts@yelp.com>
2016-01-22 17:21:12 -0800

    added deprication warning for --clusterinstance to mark_for_deployment -h

--- paasta_tools/cli/cmds/mark_for_deployment.py
+++ paasta_tools/cli/cmds/mark_for_deployment.py
@@ -50,7 +50,8 @@ def add_subparser(subparsers):
     list_parser.add_argument(
         '-l', '--deploy_group', '--clusterinstance',
         help='Mark the service ready for deployment in this deploy group (e.g. '
-             'cluster1.canary, cluster2.main)',
+             'cluster1.canary, cluster2.main). --clusterinstance is depricated and '
+             'should be replaced with --deploy_group',
         required=True,
     )
     list_parser.add_argument(
