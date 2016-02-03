09ae832adaa7b4554318961bf41f291db6963435
Nathan Handler <nhandler@yelp.com>
2016-02-03 13:53:47 -0800

    Released 0.16.30-yelp1 via make release

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from setuptools import setup
 setup(
     name='paasta-tools',
     # Don't bump version manually. See `make release` docs in ./Makefile
-    version='0.16.29',
+    version='0.16.30',
     provides=["paasta_tools"],
     author='Kyle Anderson',
     author_email='kwa@yelp.com',
