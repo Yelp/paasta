ccc0be0461c36c1b58ca9334504075960613668f
Giuliano Cioffi <gcioffi@yelp.com>
2016-02-04 09:21:23 -0800

    remove ChronosJobConfig.__eq__ #149

--- paasta_tools/chronos_tools.py
+++ paasta_tools/chronos_tools.py
@@ -176,22 +176,6 @@ class ChronosJobConfig(InstanceConfig):
             config_dict=config_dict,
             branch_dict=branch_dict,
         )
-        self.__normalize()
-
-    def __eq__(self, other):
-        return (
-            (self.service == other.service) and
-            (self.get_job_name() == other.get_job_name()) and
-            (self.config_dict == other.config_dict) and
-            (self.branch_dict == other.branch_dict)
-        )
-
-    def __normalize(self):
-        """When the config supports alternative syntaxes, convert them
-        to a normal form."""
-        parents = self.get_parents()
-        if parents is not None and not isinstance(parents, list):
-            self.config_dict['parents'] = [parents]
 
     def get_service(self):
         return self.service
@@ -253,7 +237,11 @@ class ChronosJobConfig(InstanceConfig):
         return self.config_dict.get('schedule_time_zone')
 
     def get_parents(self):
-        return self.config_dict.get('parents', None)
+        parents = self.config_dict.get('parents', None)
+        if parents is not None and not isinstance(parents, list):
+            return [parents]
+        else:
+            return parents
 
     def get_shell(self):
         """Per https://mesos.github.io/chronos/docs/api.html, ``shell`` defaults
