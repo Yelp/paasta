22627c31759d126e01c83bcba1cba3b4c9b36d71
Kyle Anderson <kwa@yelp.com>
2016-02-16 15:53:27 -0800

    Make pick_random_port try to bind to 0.0.0.0 instead of localhost

--- paasta_tools/cli/cmds/local_run.py
+++ paasta_tools/cli/cmds/local_run.py
@@ -71,7 +71,7 @@ def pick_random_port():
     """
     s = socket.socket()
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s.bind(('127.0.0.1', 0))
+    s.bind(('0.0.0.0', 0))
     s.listen(0)
 
     sockname = s.getsockname()
