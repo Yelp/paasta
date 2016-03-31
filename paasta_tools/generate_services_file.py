3a8969714d2a35e8e94b17e4ea7c38ba5323bdc1
Kyle Anderson <kwa@yelp.com>
2016-03-31 16:57:51 -0700

    Fix up /etc/services file generation

--- paasta_tools/generate_services_file.py
+++ paasta_tools/generate_services_file.py
@@ -29,19 +29,21 @@ def get_service_lines_for_service(service):
     lines = []
     config = service_configuration_lib.read_service_configuration(service)
     port = config.get('port', None)
+    description = config.get('description', "No description")
+
     if port is not None:
-        lines.append("%s (%d/tcp)" % (service, port))
+        lines.append("%s\t%d/tcp\t# %s" % (service, port, description))
 
     for namespace, config in get_all_namespaces_for_service(service, full_name=False):
         proxy_port = config.get('proxy_port', None)
         if proxy_port is not None:
-            lines.append("%s (%d/tcp)" % (compose_job_id(service, namespace), proxy_port))
-    return lines
+            lines.append("%s\t%d/tcp\t# %s" % (compose_job_id(service, namespace), proxy_port, description))
+    return [line.encode('utf-8') for line in lines]
 
 
 def main():
     strings = []
-    for service in os.listdir(service_configuration_lib.DEFAULT_SOA_DIR):
+    for service in sorted(os.listdir(service_configuration_lib.DEFAULT_SOA_DIR)):
         strings.extend(get_service_lines_for_service(service))
     print "\n".join(strings)
     sys.exit(0)
