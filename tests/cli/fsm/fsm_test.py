9d93a7a5d474a8725125077ae888f2d586955489
Kyle Anderson <kwa@yelp.com>
2016-05-02 09:58:38 -0700

    Remove comments in fsm tests

--- tests/cli/fsm/fsm_test.py
+++ tests/cli/fsm/fsm_test.py
@@ -11,8 +11,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#import mock
-#from pytest import raises
-#
-#import paasta_tools.cli.cmds.fsm as fsm
-#from paasta_tools.utils import SystemPaastaConfig
