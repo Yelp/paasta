8e63173dbc412d3ab322bf64ef46832b29d3b72b
Kyle Anderson <kwa@yelp.com>
2016-05-02 13:01:46 -0700

    Added a cli entrypoint to autoscale_cluster

---
+++ paasta_tools/autoscale_cluster.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# Copyright 2015 Yelp Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import argparse
+import logging
+
+from paasta_tools.autoscaling_lib import autoscale_local_cluster
+
+
+log = logging.getLogger('__main__')
+logging.basicConfig()
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Autoscales the local PaaSTA cluster')
+    parser.add_argument('-v', '--verbose', action='store_true',
+                        dest="verbose", default=False)
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_args()
+    if args.verbose:
+        log.setLevel(logging.DEBUG)
+    else:
+        log.setLevel(logging.WARNING)
+
+    autoscale_local_cluster()
+
+
+if __name__ == '__main__':
+    main()
