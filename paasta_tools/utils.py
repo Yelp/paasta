c2908c2f7898b42c2b6a351a0d7ec60cb7885b7c
Evan Krall <krall@yelp.com>
2016-05-06 13:07:45 -0700

    Get log writer options from log_writer.options instead of log_writer

--- paasta_tools/utils.py
+++ paasta_tools/utils.py
@@ -523,11 +523,10 @@ def list_log_writers():
 
 def configure_log():
     """We will log to the yocalhost binded scribe."""
-    log_writer_options = load_system_paasta_config().get_log_writer()
+    log_writer_config = load_system_paasta_config().get_log_writer()
     global _log_writer
-    LogWriterClass = get_log_writer_class(log_writer_options['driver'])
-    del log_writer_options['driver']  # In case the log writer class doesn't take **kwargs or a 'driver' parameter.
-    _log_writer = LogWriterClass(**log_writer_options)
+    LogWriterClass = get_log_writer_class(log_writer_config['driver'])
+    _log_writer = LogWriterClass(**log_writer_config.get('options', {}))
 
 
 def _log(*args, **kwargs):
