5a056fbde8ba99c4c485aebdb594e488b680f236
Huadong Liu <hliu@yelp.com>
2016-05-11 17:07:50 -0700

    Released 0.19.3 via make release

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from setuptools import setup
 setup(
     name='paasta-tools',
     # Don't bump version manually. See `make release` docs in ./Makefile
-    version='0.19.2',
+    version='0.19.3',
     provides=["paasta_tools"],
     author='Kyle Anderson',
     author_email='kwa@yelp.com',
