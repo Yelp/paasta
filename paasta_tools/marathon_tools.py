c9b783bd933449dd2bf4660041f974a68fdbccf0
Evan Krall <krall@yelp.com>
2016-06-15 14:02:33 -0700

    Fix typo in function name

--- paasta_tools/marathon_tools.py
+++ paasta_tools/marathon_tools.py
@@ -524,7 +524,7 @@ class MarathonServiceConfig(InstanceConfig):
             default = {'check_haproxy': True}
         return self.config_dict.get('bounce_health_params', default)
 
-    def get_bounce_marging_factor(self):
+    def get_bounce_margin_factor(self):
         return self.config_dict.get('bounce_margin_factor', 1.0)
 
     def get_accepted_resource_roles(self):
