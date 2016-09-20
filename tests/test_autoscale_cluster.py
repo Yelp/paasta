33328c732c3d24e7b28a400f85ed63861f963821
Matthew Mead-Briggs <mmb@yelp.com>
2016-09-20 11:57:20 +0100

    Test main methods in autoscaling scripts
    Kind of a noddy test but would have picked up some ImportErrors that I
    introduced recently so better than 0 coverage.

---
+++ tests/test_autoscale_cluster.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# Copyright 2015-2016 Yelp Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import mock
+
+from paasta_tools.autoscale_cluster import main
+
+
+@mock.patch('paasta_tools.autoscale_cluster.logging', autospec=True)
+@mock.patch('paasta_tools.autoscale_cluster.autoscale_local_cluster', autospec=True)
+@mock.patch('paasta_tools.autoscale_cluster.parse_args', autospec=True)
+def test_main(mock_parse_args, mock_autoscale_local_cluster, logging):
+    mock_parse_args.return_value = mock.Mock(dry_run=True)
+    main()
+    mock_autoscale_local_cluster.assert_called_with(dry_run=True)
