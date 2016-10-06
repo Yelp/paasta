9d0c3f18e740d1b0417f7dc77147cb3b98776534
Matthew Mead-Briggs <mmb@yelp.com>
2016-10-06 14:21:05 +0100

    Make paasta-api restart more useful
    * Restart if there is a syntax error etc that causes the process to exit
    * Restart if swagger.json is written

--- paasta_tools/api/api.py
+++ paasta_tools/api/api.py
@@ -24,6 +24,7 @@ import requests_cache
 import service_configuration_lib
 from gevent.wsgi import WSGIServer
 from pyramid.config import Configurator
+from pyramid.scripts.pserve import watch_file
 
 from paasta_tools import marathon_tools
 from paasta_tools.api import settings
@@ -56,6 +57,7 @@ def parse_paasta_api_args():
 def make_app(global_config=None):
     paasta_api_path = os.path.dirname(sys.modules['paasta_tools.api'].__file__)
     setup_paasta_api()
+    watch_file(os.path.join(paasta_api_path, 'api_docs/swagger.json'))
 
     config = Configurator(settings={
         'service_name': 'paasta-api',
