6ee69b59ac4ec1c27d814682a51ca50b52846d33
Alexander Dudko <adudko@yelp.com>
2017-05-10 12:31:56 -0700

    Increase timeout in mesos_cpu_metrics_provider, and work properly when it times out

--- paasta_tools/autoscaling/autoscaling_service_lib.py
+++ paasta_tools/autoscaling/autoscaling_service_lib.py
@@ -449,14 +449,15 @@ def mesos_cpu_metrics_provider(marathon_service_config, marathon_tasks, mesos_ta
 
     monkey.patch_socket()
     jobs = [gevent.spawn(task.stats_callable) for task in mesos_tasks]
-    gevent.joinall(jobs, timeout=10)
+    gevent.joinall(jobs, timeout=60)
     mesos_tasks = dict(zip([task['id'] for task in mesos_tasks], [job.value for job in jobs]))
 
     current_time = int(datetime.now().strftime('%s'))
     time_delta = current_time - last_time
 
     mesos_cpu_data = {task_id: float(stats.get('cpus_system_time_secs', 0.0) + stats.get(
-        'cpus_user_time_secs', 0.0)) / (stats.get('cpus_limit', 0) - .1) for task_id, stats in mesos_tasks.items()}
+        'cpus_user_time_secs', 0.0)) / (stats.get('cpus_limit', 0) - .1)
+        for task_id, stats in mesos_tasks.items() if stats is not None}
 
     if not mesos_cpu_data:
         raise MetricsProviderNoDataError("Couldn't get any cpu data from Mesos")
