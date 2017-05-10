8c60db8f705983eca9b15af9c4f4cdc6c5814619
Maksym Melnychok <maksym@yelp.com>
2017-05-09 17:36:26 -0700

    handle all events, change async to accept list of tuples: [(event type, function), ...]

--- task_processing/runners/async.py
+++ task_processing/runners/async.py
@@ -12,14 +12,11 @@ class AsyncError(Exception):
 
 
 class Async(Runner):
-    def __init__(self, executor, on_success=None, on_failure=None, on_all=None):
-        if on_success is None and on_failure is None and on_all is None:
+    def __init__(self, executor, callbacks=None):
+        if not callbacks:
             raise AsyncError("must provide at least one callback")
 
-        self.on_success = on_success
-        self.on_failure = on_failure
-        self.on_all = on_all
-
+        self.callbacks = callbacks
         self.executor = executor
 
         callback_t = Thread(target=self._callback_loop)
@@ -33,9 +30,6 @@ class Async(Runner):
         event_queue = self.executor.get_event_queue()
         while True:
             event = event_queue.get()
-            if self.on_success and event.is_success():
-                self.on_success(event)
-            if self.on_failure and event.is_failure():
-                self.on_failure(event)
-            if self.on_all:
-                self.on_all(event)
+            for (cl, fn) in self.callbacks:
+                if isinstance(event, cl):
+                    fn(event)
