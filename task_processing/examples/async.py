cbfb4b9f91cd92b1f9fc192862d1dc0c4eb74a85
Maksym Melnychok <maksym@yelp.com>
2017-05-10 19:37:26 -0700

    fix async example

--- task_processing/examples/async.py
+++ task_processing/examples/async.py
@@ -5,13 +5,23 @@ from __future__ import unicode_literals
 
 import logging
 import os
+import time
 
-from task_processing.events.event import EventBase
+from task_processing.events.event import EventTerminal
 from task_processing.interfaces.task_executor import make_task_config
 from task_processing.plugins.mesos.mesos_executor import MesosExecutor
 from task_processing.runners.async import Async
 
 logging.basicConfig()
+
+
+class Counter(object):
+    def __init__(self):
+        self.terminated = 0
+
+    def process_event(self, event):
+        print('{} {}'.format(event.task_id, type(event)))
+        self.terminated += 1
 
 
 def main():
@@ -20,12 +30,22 @@ def main():
         credential_secret_file="/src/task_processing/examples/cluster/secret",
         mesos_address=mesos_address
     )
-    task_config = make_task_config(image="ubuntu:14.04", cmd="/bin/sleep 10")
-    runner = Async(executor, [(EventBase, lambda ev: print(ev))])
 
-    configs = [task_config] * 100
-    for config in configs:
-        runner.run(config)
+    counter = Counter()
+    runner = Async(executor, [(EventTerminal, counter.process_event)])
+
+    for _ in range(100):
+        task_config = make_task_config(image="ubuntu:14.04",
+                                       cmd="/bin/sleep 10")
+        runner.run(task_config)
+
+    while True:
+        print('terminated {} tasks'.format(counter.terminated))
+        if counter.terminated >= 100:
+            return
+        time.sleep(10)
+
+    runner.stop()
 
 
 if __name__ == "__main__":
