ef90059aed17ffdc4c1640eaa71b662b13525285
Joseph Lukasik <jgl@yelp.com>
2017-06-01 12:52:12 -0700

    Only call get_draining_hosts once per bounce, catch ReadTimeout exception

--- tests/test_setup_marathon_job.py
+++ tests/test_setup_marathon_job.py
@@ -1552,6 +1552,7 @@ class TestGetOldHappyUnhappyDrainingTasks(object):
                 bounce_health_params={},
                 system_paasta_config=fake_system_paasta_config,
                 log_deploy_error=None,
+                draining_hosts=[],
             )
         actual_live_happy_tasks, actual_live_unhappy_tasks, actual_draining_tasks, actual_at_risk_tasks = actual
         assert actual_live_happy_tasks == expected_live_happy_tasks
@@ -1615,6 +1616,7 @@ class TestGetOldHappyUnhappyDrainingTasks(object):
                 bounce_health_params={},
                 system_paasta_config=fake_system_paasta_config,
                 log_deploy_error=None,
+                draining_hosts=[],
             )
         actual_live_happy_tasks, actual_live_unhappy_tasks, actual_draining_tasks, actual_at_risk_tasks = actual
         assert actual_live_happy_tasks == expected_live_happy_tasks
