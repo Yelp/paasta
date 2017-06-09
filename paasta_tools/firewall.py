61c3b753525543dbf3730e28ffe3cb246ce5ec96
Benjamin Chess <bchess@yelp.com>
2017-06-09 08:39:56 -0700

    fix log_prefix access

--- paasta_tools/firewall.py
+++ paasta_tools/firewall.py
@@ -68,7 +68,7 @@ class ServiceGroup(collections.namedtuple('ServiceGroup', (
         if conf.get_dependencies() is None:
             return ()
 
-        rules = [_default_rule(conf)]
+        rules = [_default_rule(conf, self.log_prefix)]
         rules.extend(_well_known_rules(conf))
         rules.extend(_smartstack_rules(conf, soa_dir, synapse_service_dir))
         return tuple(rules)
@@ -84,7 +84,7 @@ class ServiceGroup(collections.namedtuple('ServiceGroup', (
         return 'paasta.{}'.format(self.service)[:28] + ' '
 
 
-def _default_rule(conf):
+def _default_rule(conf, log_prefix):
     policy = conf.get_outbound_firewall()
     if policy == 'block':
         return iptables.Rule(
@@ -104,7 +104,7 @@ def _default_rule(conf):
             dst='0.0.0.0/0.0.0.0',
             target='LOG',
             target_parameters=(
-                ('log-prefix', ((conf.log_prefix),)),
+                ('log-prefix', ((log_prefix),)),
             ),
             matches=(
                 ('limit', (('limit', '1/sec'),)),
