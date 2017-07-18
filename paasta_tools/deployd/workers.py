e9a2fc5261e4ac1fe2f53ce9c3e58d88a5d9a687
Matthew Mead-Briggs <mmb@yelp.com>
2017-07-18 17:49:07 +0100

    Refactor run method of worker
    This splits out the meat of the processing of service instances into a
    separate method which makes it easier to test and easier for me to add
    the error handling in the next commit.

--- paasta_tools/deployd/workers.py
+++ paasta_tools/deployd/workers.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 from __future__ import unicode_literals
 
 import time
+from collections import namedtuple
 
 from paasta_tools import marathon_tools
 from paasta_tools.deployd.common import BounceTimers
@@ -9,6 +10,8 @@ from paasta_tools.deployd.common import exponential_back_off
 from paasta_tools.deployd.common import PaastaThread
 from paasta_tools.deployd.common import ServiceInstance
 from paasta_tools.setup_marathon_job import deploy_marathon_service
+
+BounceResults = namedtuple('BounceResults', ['bounce_again_in_seconds', 'failures', 'bounce_timers'])
 
 
 class PaastaDeployWorker(PaastaThread):
@@ -66,59 +69,64 @@ class PaastaDeployWorker(PaastaThread):
         self.log.info("{} starting up".format(self.name))
         while True:
             service_instance = self.bounce_q.get()
-            failures = service_instance.failures
-            bounce_timers = self.setup_timers(service_instance)
-            self.log.info("{} processing {}.{}".format(self.name, service_instance.service, service_instance.instance))
-            marathon_apps = marathon_tools.get_all_marathon_apps(self.marathon_client, embed_tasks=True)
-            bounce_timers.setup_marathon.start()
-            try:
-                return_code, bounce_again_in_seconds = deploy_marathon_service(
-                    service=service_instance.service,
-                    instance=service_instance.instance,
-                    client=self.marathon_client,
-                    soa_dir=marathon_tools.DEFAULT_SOA_DIR,
-                    marathon_config=self.marathon_config,
-                    marathon_apps=marathon_apps,
-                )
-            except Exception as e:
-                self.log.warning("deploy_marathon_service caused exception: {}".format(e))
-                return_code = -2
-            if return_code != 0:
-                failures += 1
-                bounce_again_in_seconds = exponential_back_off(
-                    failures=failures,
-                    factor=self.config.get_deployd_worker_failure_backoff_factor(),
-                    base=2,
-                    max_time=6000,
-                )
-
-            bounce_timers.setup_marathon.stop()
-            self.log.info("setup marathon completed with exit code {} for {}.{}".format(
-                return_code,
-                service_instance.service,
-                service_instance.instance,
-            ))
-            if bounce_again_in_seconds:
-                bounce_timers.processed_by_worker.start()
-                self.log.info("{}.{} not in steady state so bouncing again in {} "
-                              "seconds".format(
-                                  service_instance.service,
-                                  service_instance.instance,
-                                  bounce_again_in_seconds,
-                              ))
+            bounce_results = self.process_service_instance(service_instance)
+            if bounce_results.bounce_again_in_seconds:
                 service_instance = ServiceInstance(
                     service=service_instance.service,
                     instance=service_instance.instance,
-                    bounce_by=int(time.time()) + bounce_again_in_seconds,
+                    bounce_by=int(time.time()) + bounce_results.bounce_again_in_seconds,
                     watcher=self.name,
-                    bounce_timers=bounce_timers,
-                    failures=failures,
+                    bounce_timers=bounce_results.bounce_timers,
+                    failures=bounce_results.failures,
                 )
                 self.inbox_q.put(service_instance)
-            else:
-                bounce_timers.bounce_length.stop()
-                self.log.info("{}.{} in steady state".format(
-                    service_instance.service,
-                    service_instance.instance,
-                ))
             time.sleep(0.1)
+
+    def process_service_instance(self, service_instance):
+        failures = service_instance.failures
+        bounce_timers = self.setup_timers(service_instance)
+        self.log.info("{} processing {}.{}".format(self.name, service_instance.service, service_instance.instance))
+        marathon_apps = marathon_tools.get_all_marathon_apps(self.marathon_client, embed_tasks=True)
+        bounce_timers.setup_marathon.start()
+        try:
+            return_code, bounce_again_in_seconds = deploy_marathon_service(
+                service=service_instance.service,
+                instance=service_instance.instance,
+                client=self.marathon_client,
+                soa_dir=marathon_tools.DEFAULT_SOA_DIR,
+                marathon_config=self.marathon_config,
+                marathon_apps=marathon_apps,
+            )
+        except Exception as e:
+            self.log.warning("deploy_marathon_service caused exception: {}".format(e))
+            return_code = -2
+        if return_code != 0:
+            failures += 1
+            bounce_again_in_seconds = exponential_back_off(
+                failures=failures,
+                factor=self.config.get_deployd_worker_failure_backoff_factor(),
+                base=2,
+                max_time=6000,
+            )
+
+        bounce_timers.setup_marathon.stop()
+        self.log.info("setup marathon completed with exit code {} for {}.{}".format(
+            return_code,
+            service_instance.service,
+            service_instance.instance,
+        ))
+        if bounce_again_in_seconds:
+            bounce_timers.processed_by_worker.start()
+            self.log.info("{}.{} not in steady state so bouncing again in {} "
+                          "seconds".format(
+                              service_instance.service,
+                              service_instance.instance,
+                              bounce_again_in_seconds,
+                          ))
+        else:
+            bounce_timers.bounce_length.stop()
+            self.log.info("{}.{} in steady state".format(
+                service_instance.service,
+                service_instance.instance,
+            ))
+        return BounceResults(bounce_again_in_seconds, failures, bounce_timers)
