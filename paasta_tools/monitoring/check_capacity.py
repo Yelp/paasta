361589d07a611ff96ab9bea03754f43ab513a52f
Matthew Bentley <bentley@yelp.com>
2017-07-25 13:43:21 +0100

    Re-work how capacity-check works to make its actions more obvious
    Now select a resource, and set a warning and a critical level

--- paasta_tools/monitoring/check_capacity.py
+++ paasta_tools/monitoring/check_capacity.py
@@ -18,6 +18,7 @@ from __future__ import unicode_literals
 import argparse
 import json
 import sys
+from collections import defaultdict
 
 from paasta_tools.api.client import get_paasta_api_client
 from paasta_tools.utils import load_system_paasta_config
@@ -25,19 +26,21 @@ from paasta_tools.utils import paasta_print
 
 
 def parse_capacity_check_options():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
 
     parser.add_argument(
-        '--cpus', dest='cpus_default', type=float,
-        help='Default maximum cpu capacity before critical.',
+        'type', choices=['cpus', 'mem', 'disk'], type=str,
+        help='The resource to check.',
     )
     parser.add_argument(
-        '--mem', dest='mem_default', type=float,
-        help='Default maximum memory capacity before critical.',
+        '--warn', '-w', dest='warn', type=float, default=90,
+        help='Level to emit a warning status.',
     )
     parser.add_argument(
-        '--disk', dest='disk_default', type=float,
-        help='Default maximum disk capacity before critical.',
+        '--crit', '-c', dest='crit', type=float, default=90,
+        help='Level to emit a critical status.',
     )
     parser.add_argument(
         '--overrides', dest='overrides', type=str,
@@ -49,10 +52,8 @@ def parse_capacity_check_options():
         help='Cluster to check. Defaults to looking for the current cluster.',
     )
     parser.add_argument(
-        '--attributes', dest='attributes', type=str,
-        help='Comma separated list of attributes to check. If not specified, '
-        'these will be infered from the contents of the overrides file. '
-        'If overrides is also not specified, just check the whole cluster.\n'
+        '--attributes', dest='attributes', type=str, default='pool',
+        help='Comma separated list of attributes to check.\n'
         'By default, only checks attributes individually; see --cross-product-check',
     )
     parser.add_argument(
@@ -74,18 +75,28 @@ def calc_percent_usage(resource_item, value_to_check):
     return 100 * (values['used'] / values['total'])
 
 
+def error_message(failures, level, cluster, value_to_check):
+    result = "%s cluster %s %s usage:\n" % (level, cluster, value_to_check)
+    results = []
+    for f in failures:
+        attrs = ", ".join(["%s: %s" % (e['attr'], e['value']) for e in f['attrs']])
+        results.append(
+            "    %s is at %s percent %s, maximum %s percent" % (
+                attrs, f['current'], value_to_check,
+                f['maximum'],
+            ),
+        )
+
+    result += "\n".join(results)
+
+    return result
+
+
 def run_capacity_check():
     options = parse_capacity_check_options()
     system_paasta_config = load_system_paasta_config()
     cluster = options.cluster if options.cluster is not None else system_paasta_config.get_cluster()
-    to_check = filter(
-        lambda x: x[0] is not None,
-        [(options.disk_default, 'disk'), (options.cpus_default, 'cpus'), (options.mem_default, 'mem')],
-    )
-    if len(to_check) != 1:
-        paasta_print('UNKNOWN exactly one of --cpus, --mem, or --disk must be specified to capacity check')
-        sys.exit(3)
-    value_to_check = to_check[0][1]
+    value_to_check = options.type
 
     client = get_paasta_api_client(cluster=options.cluster)
     if client is None:
@@ -95,55 +106,57 @@ def run_capacity_check():
     if options.overrides:
         with open(options.overrides, 'r') as f:
             overrides = json.loads(f.read())
-        attributes = overrides.keys()
     else:
-        attributes = []
         overrides = {}
 
-    if options.attributes:
-        attributes = options.attributes.split(',')
+    attributes = options.attributes.split(',')
 
-    if options.cpc or attributes == []:
-        resource_use = {'superregion': client.resources.resources(groupings=attributes + ['superregion']).result()}
+    if options.cpc:
+        resource_use = {'all': client.resources.resources(groupings=attributes).result()}
     else:
         resource_use = {a: client.resources.resources(groupings=[a]).result() for a in attributes}
 
     default_check = {
-        'cpus': options.cpus_default,
-        'mem': options.mem_default,
-        'disk': options.disk_default,
+        'warn': {
+            'cpus': options.warn,
+            'mem': options.warn,
+            'disk': options.warn,
+        },
+        'crit': {
+            'cpus': options.crit,
+            'mem': options.crit,
+            'disk': options.crit,
+        },
     }
 
-    failures = []
+    failures = defaultdict(list)
     for attribute, values in resource_use.items():
         for usage_value in values:
             attribute_value = usage_value['groupings'].get(attribute, 'unknown')
             check = overrides.get('attribute', {}).get(attribute_value, default_check)
             usage_percent = calc_percent_usage(usage_value, value_to_check)
-            if usage_percent > check[value_to_check]:
-                failures.append({
-                    'attrs': [{'attr': a, 'value': v} for a, v in usage_value['groupings'].items()],
-                    'maximum': check[value_to_check], 'current': usage_percent,
-                })
+            for c in ['crit', 'warn']:
+                if usage_percent > check[c][value_to_check]:
+                    failures[c].append({
+                        'attrs': [{'attr': a, 'value': v} for a, v in usage_value['groupings'].items()],
+                        'maximum': check[c][value_to_check], 'current': usage_percent,
+                    })
+                    break
 
-    if len(failures) > 0:
-        result = "CRITICAL cluster %s %s usage: " % (cluster, value_to_check)
-        results = []
-        for f in failures:
-            attrs = ", ".join(["%s: %s" % (e['attr'], e['value']) for e in f['attrs']])
-            results.append(
-                "%s is at %s percent %s, maximum %s percent" % (
-                    attrs, f['current'], value_to_check,
-                    f['maximum'],
-                ),
-            )
-
-        result += "; ".join(results)
+    return_value = [0]
+    if len(failures['crit']) > 0:
+        result = error_message(failures['crit'], 'CRITICAL', cluster, value_to_check)
         paasta_print(result)
-        sys.exit(2)
+        return_value.append(2)
+    if len(failures['warn']) > 0:
+        result = error_message(failures['warn'], 'WARNING', cluster, value_to_check)
+        paasta_print(result)
+        return_value.append(1)
 
-    paasta_print("OK cluster %s is below critical capacity in %s" % (cluster, value_to_check))
-    sys.exit(0)
+    if max(return_value) == 0:
+        paasta_print("OK cluster %s is below critical capacity in %s" % (cluster, value_to_check))
+
+    sys.exit(max(return_value))
 
 
 if __name__ == "__main__":
