1057f389da57644f9dbd0f960d874fc593a2d0f5
Chris Kuehl <ckuehl@yelp.com>
2017-08-03 12:43:35 -0700

    Add support for whitelisting CIDR/port combinations

--- paasta_tools/firewall.py
+++ paasta_tools/firewall.py
@@ -5,6 +5,7 @@ from __future__ import unicode_literals
 import collections
 import hashlib
 import io
+import ipaddress
 import itertools
 import json
 import logging
@@ -99,6 +100,7 @@ class ServiceGroup(collections.namedtuple(
         rules = list(_default_rules(conf, self.log_prefix))
         rules.extend(_well_known_rules(conf))
         rules.extend(_smartstack_rules(conf, soa_dir, synapse_service_dir))
+        rules.extend(_cidr_rules(conf))
         return tuple(rules)
 
     def update_rules(self, soa_dir, synapse_service_dir):
@@ -257,6 +259,72 @@ def _smartstack_rules(conf, soa_dir, synapse_service_dir):
         service_namespaces = get_all_namespaces_for_service(service, soa_dir=soa_dir)
         port = dict(service_namespaces)[namespace]['proxy_port']
         yield _yocalhost_rule(port, 'proxy_port ' + namespace)
+
+
+def _cidr_rules(conf):
+    for dep in conf.get_dependencies() or ():
+        cidr = dep.get('cidr')
+        port = dep.get('port')
+
+        if cidr is not None:
+            try:
+                network = ipaddress.IPv4Network(cidr)
+            except ipaddress.AddressValueError:
+                # TODO: surface this error to the user?
+                continue
+
+            if port is not None:
+                try:
+                    port = int(port)
+                except ValueError:
+                    # TODO: surface this error to the user?
+                    continue
+
+                if not 1 <= port <= 65535:
+                    # TODO: surface this error to the user?
+                    continue
+
+            # Set up an ip rule if no port, or a tcp/udp rule if there is a port
+            dst = f'{network.network_address.exploded}/{network.netmask}'
+            if port is None:
+                yield iptables.Rule(
+                    protocol='ip',
+                    src='0.0.0.0/0.0.0.0',
+                    dst=dst,
+                    target='ACCEPT',
+                    matches=(
+                        (
+                            'comment',
+                            (
+                                ('comment', (f'allow {network}:*',)),
+                            ),
+                        ),
+                    ),
+                    target_parameters=(),
+                )
+            else:
+                for proto in ('tcp', 'udp'):
+                    yield iptables.Rule(
+                        protocol=proto,
+                        src='0.0.0.0/0.0.0.0',
+                        dst=dst,
+                        target='ACCEPT',
+                        matches=(
+                            (
+                                'comment',
+                                (
+                                    ('comment', (f'allow {network}:{port}',)),
+                                ),
+                            ),
+                            (
+                                proto,
+                                (
+                                    ('dport', (str(port),)),
+                                ),
+                            ),
+                        ),
+                        target_parameters=(),
+                    )
 
 
 def services_running_here():
