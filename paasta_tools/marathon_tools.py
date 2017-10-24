2be3c430044121e5840779bdacc518a0b856aecd
Fatema Boxwala <fbox@yelp.com>
2017-10-23 17:32:30 -0700

    sets proportional as the default service autoscaling policy

--- paasta_tools/marathon_tools.py
+++ paasta_tools/marathon_tools.py
@@ -385,7 +385,7 @@ class MarathonServiceConfig(LongRunningServiceConfig):
     def get_autoscaling_params(self) -> AutoscalingParamsDict:
         default_params: AutoscalingParamsDict = {
             'metrics_provider': 'mesos_cpu',
-            'decision_policy': 'pid',
+            'decision_policy': 'proportional',
             'setpoint': 0.8,
         }
         return deep_merge_dictionaries(
