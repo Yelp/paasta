b8a3afbe18b20a6627bbf98896175b2e793ef410
Matthew Bentley <bentley@yelp.com>
2017-11-20 10:16:17 -0800

    Fix metrics names

--- paasta_tools/deployd/master.py
+++ paasta_tools/deployd/master.py
@@ -175,7 +175,7 @@ class DeployDaemon(PaastaThread):
     def startup(self):
         self.is_leader = True
         self.log.info("This node is elected as leader {}".format(socket.getfqdn()))
-        self.metrics = get_metrics_interface('paasta_deployd')
+        self.metrics = get_metrics_interface('paasta.deployd')
         QueueMetrics(self.inbox, self.bounce_q, self.config.get_cluster(), self.metrics).start()
         self.inbox.start()
         self.log.info("Starting all watcher threads")
