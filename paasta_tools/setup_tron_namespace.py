e58c74ea468ec38e0a618b5e3872b59d2b49f6e1
maksym <maksym@yelp.com>
2018-05-28 18:21:12 +0200

    add -a/--all argument to paasta_setup_tron_namespace

--- paasta_tools/setup_tron_namespace.py
+++ paasta_tools/setup_tron_namespace.py
@@ -28,26 +28,39 @@ from paasta_tools import tron_tools
 from paasta_tools.tron.client import TronRequestError
 from paasta_tools.tron_tools import InvalidTronConfig
 from paasta_tools.tron_tools import TronNotConfigured
+from paasta_tools.tron_tools import ConflictingNamespacesError
 from paasta_tools.utils import NoConfigurationForServiceError
 from paasta_tools.utils import PaastaNotConfiguredError
-
 
 log = logging.getLogger(__name__)
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='Update the Tron namespace configuration for a service.')
+    parser = argparse.ArgumentParser(
+        description='Update the Tron namespace configuration for a service.')
     parser.add_argument(
         'service',
+        nargs='*',
         help='The service to update.',
     )
     parser.add_argument(
-        '-d', '--soa-dir', dest="soa_dir", metavar="SOA_DIR",
+        '-a',
+        '--all',
+        dest='all_namepsaces',
+        action='store_true',
+        help='Update all available Tron namespaces.')
+    parser.add_argument(
+        '-d',
+        '--soa-dir',
+        dest="soa_dir",
+        metavar="SOA_DIR",
         default=tron_tools.DEFAULT_SOA_DIR,
         help="Use a different soa config directory",
     )
     parser.add_argument(
-        '-v', '--verbose', action='store_true',
+        '-v',
+        '--verbose',
+        action='store_true',
         default=False,
     )
     args = parser.parse_args()
@@ -62,22 +75,48 @@ def main():
     else:
         logging.basicConfig(level=logging.WARNING)
 
-    try:
-        new_config = tron_tools.create_complete_config(
-            service=args.service,
-            soa_dir=args.soa_dir,
-        )
+        if args.all_namepsaces:
+            tron_config = tron_tools.load_tron_config()
+            try:
+                services = tron_tools.get_tron_namespaces_for_cluster(
+                    cluster=tron_config.get_tron_cluster())
+            except ConflictingNamespacesError as e:
+                log.error('Failed to list tron namespaces: {error}'.format(
+                    error=str(e)))
+        else:
+            services = [args.service]
+
         client = tron_tools.get_tron_client()
-        client.update_namespace(args.service, new_config)
-    except (
-        InvalidTronConfig,
-        NoConfigurationForServiceError,
-        TronNotConfigured,
-        PaastaNotConfiguredError,
-        TronRequestError,
-    ) as e:
-        log.error('Update for {namespace} failed: {error}'.format(namespace=args.service, error=str(e)))
-        sys.exit(1)
+
+        updated = []
+        failed = []
+
+        for service in services:
+            try:
+                new_config = tron_tools.create_complete_config(
+                    service=args.service,
+                    soa_dir=args.soa_dir,
+                )
+                client.update_namespace(args.service, new_config)
+                updated.append(service)
+            except (
+                    InvalidTronConfig,
+                    NoConfigurationForServiceError,
+                    TronNotConfigured,
+                    PaastaNotConfiguredError,
+                    TronRequestError,
+            ) as e:
+                log.error('Update for {namespace} failed: {error}'.format(
+                    namespace=args.service, error=str(e)))
+                failed.append(service)
+
+        log.info(
+            'Updated following namespaces: {updated}, failed: {failed}'.format(
+                updated=updated,
+                failed=failed,
+            ))
+
+        sys.exit(1 if failed else 0)
 
 
 if __name__ == "__main__":
