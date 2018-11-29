caf4a062f1473e68facc844a74137169ffd8c394
Matthew Mead-Briggs <mmb@yelp.com>
2018-11-29 09:33:32 -0800

    Ignore failed docker restart in k8s example
    This is trying to restart before the service is fully configured I think
    and so it is failing. Just skipping for now.

--- example_cluster/dind-cluster-v1.11.sh
+++ example_cluster/dind-cluster-v1.11.sh
@@ -2289,7 +2289,7 @@ function dind::custom-docker-opts {
     local json=$(IFS="+"; echo "${jq[*]}")
     docker exec -i ${container_id} /bin/sh -c "mkdir -p /etc/docker && jq -n '${json}' > /etc/docker/daemon.json"
     docker exec ${container_id} systemctl daemon-reload
-    docker exec ${container_id} systemctl restart docker
+    docker exec ${container_id} systemctl restart docker || true
   fi
 }
 
