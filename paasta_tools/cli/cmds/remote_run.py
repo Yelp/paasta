3e03e9eba231d217cc6661a95d09d5791b110a1f
Ka Wai Wan <kawaiwan@yelp.com>
2018-12-19 15:00:32 -0800

    Add retries and email args to remote-run

--- paasta_tools/cli/cmds/remote_run.py
+++ paasta_tools/cli/cmds/remote_run.py
@@ -12,6 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import os
 import json
 import re
 from shlex import quote
@@ -43,6 +44,8 @@ ARG_DEFAULTS = dict(
         docker_image=None,
         dry_run=False,
         constraint=[],
+        notification_email=None,  # always append to remote-run command, so no default
+        retries=3,
     ),
     stop=dict(run_id=None, framework_id=None),
     list=dict(),
@@ -120,9 +123,13 @@ def add_start_parser(subparser):
         action='store_true',
         default=ARG_DEFAULTS['start']['detach'],
     )
+    default_staging_timeout = ARG_DEFAULTS['start']['staging_timeout']
     parser.add_argument(
         '-t', '--staging-timeout',
-        help='A timeout for the task to be launching before killed',
+        help=(
+            'A timeout in seconds for the task to be launching before killed. '
+            f'Default: {default_staging_timeout}s'
+        ),
         default=ARG_DEFAULTS['start']['staging_timeout'],
         type=float,
     )
@@ -163,6 +170,27 @@ def add_start_parser(subparser):
         action='append',
         default=ARG_DEFAULTS['start']['constraint'],
     )
+    default_email = os.environ.get('EMAIL', None)
+    parser.add_argument(
+        '-E', '--notification-email',
+        help=(
+            'Email address to send remote-run notifications to. '
+            'A notification will be sent when a task either succeeds or fails. '
+            'Required if environment variable $EMAIL not set. '
+        ) + (f'Default: {default_email}' if default_email else ''),
+        default=default_email,
+        required=default_email is None,
+    )
+    default_retries = ARG_DEFAULTS['start']['retries']
+    parser.add_argument(
+        '-r', '--retries',
+        help=(
+            'Number of times to retry if task fails to launch. '
+            f'Default: {default_retries}'
+        ),
+        type=int,
+        default=default_retries,
+    )
     return parser
 
 
@@ -184,6 +212,7 @@ def add_stop_parser(subparser):
             'ID of framework to stop. Must belong to remote-run of selected '
             'service instance.'
         ),
+        type=int,
         default=ARG_DEFAULTS['stop']['framework_id'],
     )
     return parser
