61a609003949e215cdbeb92ce69cb96776e8307b
Mingqi Zhu <mingqiz@yelp.com>
2019-02-11 14:13:41 -0800

    reordered improts

--- paasta_tools/cli/cmds/metastatus.py
+++ paasta_tools/cli/cmds/metastatus.py
@@ -23,14 +23,15 @@ from paasta_tools.api.client import get_paasta_api_client
 from paasta_tools.cli.utils import execute_paasta_metastatus_on_remote_master
 from paasta_tools.cli.utils import get_paasta_metastatus_cmd_args
 from paasta_tools.cli.utils import lazy_choices_completer
+from paasta_tools.mesos_tools import get_mesos_leader
 from paasta_tools.utils import DEFAULT_SOA_DIR
 from paasta_tools.utils import list_clusters
 from paasta_tools.utils import list_services
 from paasta_tools.utils import load_system_paasta_config
 from paasta_tools.utils import paasta_print
 from paasta_tools.utils import PaastaColors
+from paasta_tools.utils import PaastaNotConfiguredError
 from paasta_tools.utils import SystemPaastaConfig
-from paasta_tools.mesos_tools import get_mesos_leader
 
 
 def add_subparser(
@@ -172,7 +173,10 @@ def print_cluster_status(
     )
 
     paasta_print("Cluster: %s" % cluster)
-    paasta_print("Mesos leader: %s" % get_mesos_leader())
+    try:
+        paasta_print("Mesos leader: %s" % get_mesos_leader())
+    except PaastaNotConfiguredError:
+        pass
     paasta_print(get_cluster_dashboards(cluster))
     paasta_print(output)
     paasta_print()
