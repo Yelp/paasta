ddb7bcfc6d7ec5bd80afeeb36a15b4c83c0d8ee1
Kyle Anderson <kwa@yelp.com>
2019-02-20 10:13:40 -0800

    Delete iptables rules when duplicates are detected

--- paasta_tools/contrib/kill_bad_containers.py
+++ paasta_tools/contrib/kill_bad_containers.py
@@ -7,14 +7,6 @@ from paasta_tools import iptables
 from paasta_tools.utils import get_docker_client
 
 
-def list_docker_nat_rules():
-    chain_name = 'DOCKER'
-    table = iptc.Table(iptc.Table.NAT)
-    chain = iptc.Chain(table, chain_name)
-    for rule in chain.rules:
-        yield iptables.Rule.from_iptc(rule)
-
-
 def get_container_from_dport(dport, docker_client):
     for container in docker_client.containers():
         try:
@@ -37,11 +29,19 @@ def target_rule_to_dport(rule):
 
 
 def kill_containers_with_duplicate_iptables_rules(docker_client):
+    chain_name = 'DOCKER'
+    table = iptc.Table(iptc.Table.NAT)
+    chain = iptc.Chain(table, chain_name)
+
     targets_seen = {}
-    for rule in list_docker_nat_rules():
+    raw_rules_seen = {}
+
+    for iptables_rule in chain.rules:
+        rule = iptables.Rule.from_iptc(iptables_rule)
         target = rule.target_parameters
         if target not in targets_seen:
             targets_seen[target] = rule
+            raw_rules_seen[target] = iptables_rule
         else:
             print("This is the second time we've seen a rule with the same target_parameters!")
             print(rule)
@@ -49,19 +49,24 @@ def kill_containers_with_duplicate_iptables_rules(docker_client):
             container1 = get_container_from_dport(dport, docker_client)
             print("The other rule with that target is:")
             print(targets_seen[target])
-            dport = target_rule_to_dport(rule)
-            container2 = get_container_from_dport(dport, docker_client)
+            dport2 = target_rule_to_dport(targets_seen[target])
+            container2 = get_container_from_dport(dport2, docker_client)
             if container1["Id"] == container2["Id"]:
                 print("The same container is getting traffic for both ports!")
                 print(container1)
                 print("Killing the container")
                 docker_client.kill(container1["Id"])
+                print("Deleting both iptables rules")
+                chain.delete_rule(iptables_rule)
+                chain.delete_rule(raw_rules_seen[target])
             else:
                 print("These are two different containers, which means we have duplicate ips:")
                 print(container1)
                 print(container2)
                 print("Not sure which to kill, picking the first one")
                 docker_client.kill(container1["Id"])
+                print("Deleting the iptables rule for that container")
+                chain.delete_rule(iptables_rule)
 
 
 def main():
