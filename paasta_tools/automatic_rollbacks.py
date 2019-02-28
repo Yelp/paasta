4e40431a8835b7cd21bc2bfcce235abf53f0c1e1
Kyle Anderson <kwa@yelp.com>
2019-02-27 16:57:29 -0800

    Added a slack event tailer that acks button presses

--- paasta_tools/automatic_rollbacks.py
+++ paasta_tools/automatic_rollbacks.py
@@ -1,3 +1,18 @@
+import json
+import logging
+
+import requests
+
+try:
+    from scribereader import scribereader
+except ImportError:
+    scribereader = None
+
+SLACK_WEBHOOK_STREAM = 'stream_slack_incoming_webhook'
+SCRIBE_ENV = 'uswest1-prod'
+log = logging.getLogger(__name__)
+
+
 def get_slack_blocks_for_initial_deployment(message):
     blocks = [
         {
@@ -31,3 +46,67 @@ def get_slack_blocks_for_initial_deployment(message):
         },
     ]
     return blocks
+
+
+class ButtonPress():
+    def __init__(self, event):
+        self.event = event
+        self.username = event["user"]["username"]
+        self.response_url = event["response_url"]
+        # TODO: Handle multiple actions?
+        self.action = event["actions"][0]
+
+    def __repr__(self):
+        return self.event
+
+    def ack(self):
+        # Implements responding to button presses
+        # https://api.slack.com/messaging/interactivity/enabling#responding-to-interactions
+        # But isn't the api_call method per-se
+        # https://github.com/slackapi/python-slackclient/issues/270
+
+        # Acking a button means updating the original message.
+        # Currently this function just appends to the message with an ack.
+        blocks = self.event["message"]["blocks"]
+        blocks.append({"type": "divider"})
+        blocks.append(
+            {
+                "type": "section", "text": {
+                    "type": "mrkdwn",
+                    "text": f"Got it. You pressed '{self.action['value']}'. This isn't implemented yet.",
+                },
+            },
+        )
+        requests.post(self.response_url, json={"blocks": blocks})
+
+
+def event_to_buttonpress(event):
+    return ButtonPress(event=event)
+
+
+def parse_webhook_event_json(line):
+    event = json.loads(line)
+    log.debug(event)
+    return event
+
+
+def is_relevant_event(event):
+    # TODO: Implement filtering
+    return True
+
+
+def watch_for_slack_webhooks():
+    host_and_port = scribereader.get_env_scribe_host(SCRIBE_ENV, True)
+    host = host_and_port['host']
+    port = host_and_port['port']
+    tailer = scribereader.get_stream_tailer(SLACK_WEBHOOK_STREAM, host, port)
+    for line in tailer:
+        event = parse_webhook_event_json(line)
+        if is_relevant_event(event):
+            buttonpress = event_to_buttonpress(event)
+            buttonpress.ack()
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    watch_for_slack_webhooks()
