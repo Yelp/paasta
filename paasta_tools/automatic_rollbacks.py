9f6ab774faad95212a40087fb299547d9c28251c
Kyle Anderson <kwa@yelp.com>
2019-03-01 16:29:57 -0800

    Added fancier buttons and the ability to control the states

--- paasta_tools/automatic_rollbacks.py
+++ paasta_tools/automatic_rollbacks.py
@@ -3,6 +3,7 @@ import logging
 
 import requests
 
+from paasta_tools.slack import get_slack_client
 try:
     from scribereader import scribereader
 except ImportError:
@@ -13,7 +14,7 @@ SCRIBE_ENV = 'uswest1-prod'
 log = logging.getLogger(__name__)
 
 
-def get_slack_blocks_for_initial_deployment(message):
+def get_slack_blocks_for_initial_deployment(message, last_action=None, status=None, active_button=None):
     blocks = [
         {
             "type": "section",
@@ -22,30 +23,98 @@ def get_slack_blocks_for_initial_deployment(message):
                 "text": message,
             },
         },
+        {"type": "divider"},
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": f"Status: {status}\nLast action: {last_action}",
+            },
+        },
         {
             "type": "actions",
-            "block_id": "rollback_block1",
-            "elements": [
-                {
-                    "type": "button",
-                    "text": {
-                            "type": "plain_text",
-                            "text": "Roll Back (Not Implemented)",
-                    },
-                    "value": "rollback",
-                },
-                {
-                    "type": "button",
-                    "text": {
-                            "type": "plain_text",
-                            "text": "Continue (Not Implemented)",
-                    },
-                    "value": "continue",
-                },
-            ],
+            "block_id": "deployment_actions",
+            "elements": get_button_elements(["rollback", "forward"], active_button=active_button),
         },
     ]
     return blocks
+
+
+def get_button_elements(buttons, active_button=None):
+    elements = []
+    for button in buttons:
+        if button == "rollback" and active_button == "rollback":
+            elements.append(
+                {
+                    "type": "button",
+                    "text": {
+                        "type": "plain_text",
+                        "text": "Rolling Back :zombocom: (Not Implemented)",
+                        "emoji": True,
+                    },
+                    "value": "rollback",
+                },
+            )
+        elif button == "rollback" and active_button != "rollback":
+            elements.append(
+                {
+                    "type": "button",
+                    "text": {
+                        "type": "plain_text",
+                        "text": "Roll Back :arrow_backward: (Not Implemented)",
+                        "emoji": True,
+                    },
+                    "value": "rollback",
+                    "confirm": get_confirmation_object(button),
+                },
+            )
+        elif button == "forward" and active_button == "forward":
+            elements.append(
+                {
+                    "type": "button",
+                    "text": {
+                        "type": "plain_text",
+                        "text": "Rolling Forward :zombocom: (Not Implemented)",
+                        "emoji": True,
+                    },
+                    "value": "forward",
+                },
+            )
+        elif button == "forward" and active_button != "forward":
+            elements.append(
+                {
+                    "type": "button",
+                    "text": {
+                        "type": "plain_text",
+                        "text": "Continue Forward :arrow_forward: (Not Implemented)",
+                        "emoji": True,
+                    },
+                    "value": "forward",
+                    "confirm": get_confirmation_object(button),
+                },
+            )
+    return elements
+
+
+def get_confirmation_object(action):
+    return {
+        "title": {
+            "type": "plain_text",
+            "text": "Are you sure?",
+        },
+        "text": {
+            "type": "mrkdwn",
+            "text": f"Did you mean to press {action}?",
+        },
+        "confirm": {
+            "type": "plain_text",
+            "text": "Yes. Do it!",
+        },
+        "deny": {
+            "type": "plain_text",
+            "text": "Stop, I've changed my mind!",
+        },
+    }
 
 
 class ButtonPress():
@@ -54,29 +123,18 @@ class ButtonPress():
         self.username = event["user"]["username"]
         self.response_url = event["response_url"]
         # TODO: Handle multiple actions?
-        self.action = event["actions"][0]
+        self.action = event["actions"][0]["value"]
+        self.thread_ts = event["container"]["thread_ts"]
+        self.channel = event["channel"]["name"]
 
     def __repr__(self):
         return self.event
 
-    def ack(self):
+    def update(self, blocks):
         # Implements responding to button presses
         # https://api.slack.com/messaging/interactivity/enabling#responding-to-interactions
         # But isn't the api_call method per-se
         # https://github.com/slackapi/python-slackclient/issues/270
-
-        # Acking a button means updating the original message.
-        # Currently this function just appends to the message with an ack.
-        blocks = self.event["message"]["blocks"]
-        blocks.append({"type": "divider"})
-        blocks.append(
-            {
-                "type": "section", "text": {
-                    "type": "mrkdwn",
-                    "text": f"Got it. You pressed '{self.action['value']}'. This isn't implemented yet.",
-                },
-            },
-        )
         requests.post(self.response_url, json={"blocks": blocks})
 
 
@@ -95,7 +153,7 @@ def is_relevant_event(event):
     return True
 
 
-def watch_for_slack_webhooks():
+def watch_for_slack_webhooks(sc):
     host_and_port = scribereader.get_env_scribe_host(SCRIBE_ENV, True)
     host = host_and_port['host']
     port = host_and_port['port']
@@ -104,9 +162,17 @@ def watch_for_slack_webhooks():
         event = parse_webhook_event_json(line)
         if is_relevant_event(event):
             buttonpress = event_to_buttonpress(event)
-            buttonpress.ack()
+            followup_message = f"Got it. {buttonpress.username} pressed {buttonpress.action}"
+            sc.post(channels=[buttonpress.channel], message=followup_message, thread_ts=buttonpress.thread_ts)
+            action = buttonpress.action
+            blocks = get_slack_blocks_for_initial_deployment(
+                message="New Message", last_action=action, status=f"Taking action on the {action} button",
+                active_button=action,
+            )
+            buttonpress.update(blocks)
 
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
-    watch_for_slack_webhooks()
+    sc = get_slack_client()
+    watch_for_slack_webhooks(sc)
