f5797e3a61490cb24e1b4dafe800e8732a08a537
Mingqi Zhu <mingqiz@yelp.com>
2019-04-24 14:40:14 -0700

    fixed comment

--- paasta_tools/run-paasta-api-in-dev-mode.py
+++ paasta_tools/run-paasta-api-in-dev-mode.py
@@ -7,11 +7,11 @@ from paasta_tools.cli.utils import pick_random_port
 
 def main():
     print('-------------------------------------------------------')
-    print('Please run echo $$PAASTA_SYSTEM_CONFIG_DIR to continue')
+    print('Please run export PAASTA_SYSTEM_CONFIG_DIR=etc_paasta_for_development to continue')
     print("Please set environment variable PAASTA_TEST_CLUSTER to the cluster you want to use.")
     print("This is necessary for tron jobs")
     print('-------------------------------------------------------')
-    cluster = os.environ.get('PAASTA_TEST_CLUSTER', 'mesosstage')
+    cluster = os.environ.get('PAASTA_TEST_CLUSTER', 'norcal-devc')
     config_path = 'etc_paasta_for_development'
 
     copy_tree('/etc/paasta', os.path.join(os.getcwd(), config_path))
