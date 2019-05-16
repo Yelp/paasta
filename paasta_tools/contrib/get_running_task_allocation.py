58e6885a3571dd319025ec9437e54abf00aa2a54
Qui Nguyen <1322636+qui@users.noreply.github.com>
2019-05-16 14:01:28 -0700

    Add pool and timestamps to task info

--- paasta_tools/contrib/get_running_task_allocation.py
+++ paasta_tools/contrib/get_running_task_allocation.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python
+import time
+
 import a_sync
+import simplejson as json
 
 from paasta_tools import mesos_tools
 from paasta_tools.utils import load_system_paasta_config
+from paasta_tools.utils import paasta_print
 
 
-def get_mesos_container_id_from_task(task):
+def get_container_info_from_task(task):
     for status in task['statuses']:
+        if status['state'] != 'TASK_RUNNING':
+            continue
         container_id = status.get('container_status', {}).get('container_id', {}).get('value')
-        if container_id:
-            return container_id
+        time_start = status.get('timestamp')
+        return container_id, time_start
+    return None, None
 
 
 def get_paasta_service_instance_from_task(task):
@@ -25,25 +32,32 @@ def get_paasta_service_instance_from_task(task):
     return service, instance
 
 
+async def get_pool_from_task(task):
+    attributes = (await task.slave())['attributes']
+    return attributes.get('pool', 'default')
+
+
 @a_sync.to_blocking
 async def get_task_allocation_info():
     tasks = await mesos_tools.get_cached_list_of_running_tasks_from_frameworks()
     info_list = []
     for task in tasks:
         info = {'resources': task['resources']}
-        info['mesos_container_id'] = get_mesos_container_id_from_task(task)
-        service, instance = get_paasta_service_instance_from_task(task)
-        info['service'] = service
-        info['instance'] = instance
+        info['mesos_container_id'], info['start_time'] = get_container_info_from_task(task)
+        info['paasta_service'], info['paasta_instance'] = get_paasta_service_instance_from_task(task)
+        info['paasta_pool'] = await get_pool_from_task(task)
         info_list.append(info)
+    return info_list
 
 
 def main():
     cluster = load_system_paasta_config().get_cluster()
     info_list = get_task_allocation_info()
+    timestamp = time.time()
     for info in info_list:
         info['cluster'] = cluster
-        print(info)
+        info['timestamp'] = timestamp
+        paasta_print(json.dumps(info))
 
 
 if __name__ == '__main__':
