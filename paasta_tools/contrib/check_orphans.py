9f185e6e2c40d02cdd8c2fc5aa66ecaa34979fe2
Ben Plotnick <plotnick@yelp.com>
2019-06-07 11:26:22 -0700

    Change check_orphans.py into a check and minor fixes

--- paasta_tools/contrib/check_orphans.py
+++ paasta_tools/contrib/check_orphans.py
@@ -4,8 +4,9 @@ import asyncio
 import json
 import logging
 import os.path
-import pathlib
+import sys
 from collections import defaultdict
+from enum import Enum
 from typing import Any
 from typing import DefaultDict
 from typing import Dict
@@ -17,13 +18,21 @@ from typing import Tuple
 
 import yaml
 from kazoo.client import KazooClient
+from kazoo.exceptions import NoNodeError
+
+logger = logging.getLogger('check_orphans')
 
 PREFIX = '/smartstack/global/'
 CHUNK_SIZE = 50  # How many concurrent xinetd connections
 DEFAULT_ZK_DISCOVERY_PATH = '/nail/etc/zookeeper_discovery/infrastructure/local.yaml'
 DEFAULT_NERVE_XINETD_PORT = 8735
 DEFAULT_SERVICE_BLACKLIST_PATH = './orphan_service_blacklist.yaml'
-DEFAULT_OUTPUT_DIR = './output/'
+
+
+class ExitCode(Enum):
+    OK = 0
+    ORPHANS = 1
+    COLLISIONS = 2
 
 
 def get_zk_hosts(path: str) -> List[str]:
@@ -36,14 +45,14 @@ SmartstackData = Dict[str, Dict[str, Any]]
 
 
 def get_zk_data(blacklisted_services: Set[str]) -> SmartstackData:
-    logging.info(f'using {DEFAULT_ZK_DISCOVERY_PATH} for zookeeper')
+    logger.info(f'using {DEFAULT_ZK_DISCOVERY_PATH} for zookeeper')
     zk_hosts = get_zk_hosts(DEFAULT_ZK_DISCOVERY_PATH)
 
-    logging.debug(f'connecting to zk hosts {zk_hosts}')
+    logger.debug(f'connecting to zk hosts {zk_hosts}')
     zk = KazooClient(hosts=zk_hosts)
     zk.start()
 
-    logging.debug(f'pulling smartstack data from zookeeper')
+    logger.debug(f'pulling smartstack data from zookeeper')
     zk_data = {}
     services = zk.get_children(PREFIX)
     for service in services:
@@ -52,7 +61,10 @@ def get_zk_data(blacklisted_services: Set[str]) -> SmartstackData:
         service_instances = zk.get_children(os.path.join(PREFIX, service))
         instances_data = {}
         for instance in service_instances:
-            instance_node = zk.get(os.path.join(PREFIX, service, instance))
+            try:
+                instance_node = zk.get(os.path.join(PREFIX, service, instance))
+            except NoNodeError:
+                continue
             instances_data[instance] = json.loads(instance_node[0])
             zk_data[service] = instances_data
 
@@ -74,7 +86,7 @@ def read_from_zk_data(registrations: SmartstackData) -> Set[InstanceTuple]:
 
 
 async def transfer_one_file(host: str, port: int = DEFAULT_NERVE_XINETD_PORT) -> Tuple[str, Optional[str]]:
-    logging.debug(f'getting file from {host}')
+    logger.debug(f'getting file from {host}')
     try:
         reader, _ = await asyncio.wait_for(
             asyncio.open_connection(host=host, port=port, limit=2**32),
@@ -82,14 +94,14 @@ async def transfer_one_file(host: str, port: int = DEFAULT_NERVE_XINETD_PORT) ->
         )
         resp = await asyncio.wait_for(reader.read(), timeout=1.0)
     except (asyncio.TimeoutError, ConnectionRefusedError):
-        logging.error(f'error getting file from {host}')
+        logger.error(f'error getting file from {host}')
         return (host, None)
 
     return (host, resp.decode())
 
 
 async def gather_files(hosts: Set[str]) -> Dict[str, str]:
-    logging.info('gathering files from {} hosts'.format(len(hosts)))
+    logger.info('gathering files from {} hosts'.format(len(hosts)))
     tasks = [transfer_one_file(host) for host in hosts]
     responses = {}
     for idx in range(0, len(tasks), CHUNK_SIZE):
@@ -127,46 +139,30 @@ def get_instance_data(service_blacklist_path: str) -> Tuple[Set[InstanceTuple], 
     hosts = {x[0] for x in zk_instance_data}
 
     # Dump Nerve configs from each host via xinetd
-    coro = gather_files(hosts)
-    results = asyncio.get_event_loop().run_until_complete(coro)
+    results = asyncio.get_event_loop().run_until_complete(gather_files(hosts))
 
     nerve_instance_data, not_found_hosts = read_nerve_files(results)
 
     # Filter out anything that we couldn't get a nerve config for
     zk_instance_data_filtered = {x for x in zk_instance_data if x[0] not in not_found_hosts}
 
-    logging.info('zk_instance_data (unfiltered) len: {}'.format(len(zk_instance_data)))
-    logging.info('zk_instance_data (filtered) len: {}'.format(len(zk_instance_data_filtered)))
-    logging.info('nerve_instance_data len: {}'.format(len(nerve_instance_data)))
+    logger.info('zk_instance_data (unfiltered) len: {}'.format(len(zk_instance_data)))
+    logger.info('zk_instance_data (filtered) len: {}'.format(len(zk_instance_data_filtered)))
+    logger.info('nerve_instance_data len: {}'.format(len(nerve_instance_data)))
 
     return zk_instance_data_filtered, nerve_instance_data
 
 
-def write_output(
-    output_dir: str,
+def check_orphans(
     zk_instance_data: Set[InstanceTuple],
     nerve_instance_data: Set[InstanceTuple],
-) -> None:
+) -> ExitCode:
     orphans = zk_instance_data - nerve_instance_data
-    if orphans:
-        logging.warning('{} orphans found'.format(len(orphans)))
-
-    pathlib.Path(output_dir).mkdir(exist_ok=True)
 
     # groupby host
-
     orphans_by_host: DefaultDict[str, List[Tuple[int, str]]] = defaultdict(list)
     for orphan in orphans:
         orphans_by_host[orphan.host].append((orphan.port, orphan.service))
-    with open(os.path.join(output_dir, 'orphans_by_host.yaml'), 'w') as f:
-        yaml.dump(dict(orphans_by_host), f)
-
-    # groupby service
-    orphans_by_service: DefaultDict[str, List[Tuple[str, int]]] = defaultdict(list)
-    for orphan in orphans:
-        orphans_by_service[orphan.service].append((orphan.host, orphan.port))
-    with open(os.path.join(output_dir, 'orphans_by_service.yaml'), 'w') as f:
-        yaml.dump(dict(orphans_by_service), f)
 
     # collisions
     instance_by_addr: DefaultDict[Tuple[str, int], Set[str]] = defaultdict(set)
@@ -182,14 +178,21 @@ def write_output(
             )
 
     if collisions:
-        logging.warning('Collisions found! Traffic is being misrouted!')
-    with open(os.path.join(output_dir, 'collisions.yaml'), 'w') as f:
-        f.write('\n'.join(collisions))
+        logger.warning('Collisions found! Traffic is being misrouted!')
+        print('\n'.join(collisions))
+        return ExitCode.COLLISIONS
+    elif orphans:
+        logger.warning('{} orphans found'.format(len(orphans)))
+        print(dict(orphans_by_host))
+        return ExitCode.ORPHANS
+    else:
+        logger.info(
+            'No orphans found out of {} service registrations seen'.format(len(zk_instance_data)),
+        )
+        return ExitCode.OK
 
-    logging.info('Wrote out orphan data')
 
-
-def main():
+def main() -> ExitCode:
     logging.basicConfig(level=logging.INFO)
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -197,17 +200,12 @@ def main():
         default=DEFAULT_SERVICE_BLACKLIST_PATH,
         help="Path to a file that contains services to ignore",
     )
-    parser.add_argument(
-        '--output-dir',
-        default=DEFAULT_OUTPUT_DIR,
-        help="Where to write the output files",
-    )
     args = parser.parse_args()
 
     zk_instance_data, nerve_instance_data = get_instance_data(args.service_blacklist_path)
 
-    write_output(args.output_dir, zk_instance_data, nerve_instance_data)
+    return check_orphans(zk_instance_data, nerve_instance_data)
 
 
 if __name__ == '__main__':
-    main()
+    sys.exit(main().value)
