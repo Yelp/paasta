d6b340b43fa0c3f47b47c8ec67e10a6e27f3142d
Stuart Elston <selston@yelp.com>
2019-08-16 10:13:44 -0700

    Released 0.85.47 via make release

--- paasta_tools/__init__.py
+++ paasta_tools/__init__.py
@@ -17,4 +17,4 @@
 # setup phase, the dependencies may not exist on disk yet.
 #
 # Don't bump version manually. See `make release` docs in ./Makefile
-__version__ = "0.85.46"
+__version__ = "0.85.47"
