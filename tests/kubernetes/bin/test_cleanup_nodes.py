9d2678252ecdb50ef2e2031c1263040a5be0f793
Robert Johnson <rob@rob-johnson.me>
2019-10-16 14:39:59 +0100

    check the state of the instance before terminating

--- tests/kubernetes/bin/test_cleanup_nodes.py
+++ tests/kubernetes/bin/test_cleanup_nodes.py
@@ -1,17 +1,24 @@
 import mock
 import pytest
-from kubernetes.client.rest import ApiException
 from kubernetes.client import V1DeleteOptions
+from kubernetes.client.rest import ApiException
 
+from paasta_tools.kubernetes.bin.cleanup_nodes import does_instance_exist
 from paasta_tools.kubernetes.bin.cleanup_nodes import main
 from paasta_tools.kubernetes.bin.cleanup_nodes import nodes_for_cleanup
 from paasta_tools.kubernetes.bin.cleanup_nodes import terminate_nodes
 
+
 def test_nodes_for_cleanup():
-    with mock.patch('paasta_tools.kubernetes.bin.cleanup_nodes.is_node_ready', side_effect=[True, False, False]):
+    with mock.patch(
+        "paasta_tools.kubernetes.bin.cleanup_nodes.terminated_nodes", autospec=True
+    ) as mock_terminated_nodes:
         m1, m2, m3 = mock.Mock(), mock.Mock(), mock.Mock()
-        for_cleanup = nodes_for_cleanup([m1,m2,m3])
-        assert for_cleanup == [m2,m3]
+        mock_client = mock.Mock()
+        mock_terminated_nodes.return_value = [m2, m3]
+        for_cleanup = nodes_for_cleanup(mock_client, [m1, m2, m3])
+        assert for_cleanup == [m2, m3]
+
 
 def test_terminate_nodes():
 
@@ -19,12 +26,11 @@ def test_terminate_nodes():
     mock_client.core.delete_node.side_effect = [None, ApiException(404), None]
 
     m1, m2, m3 = mock.Mock(), mock.Mock(), mock.Mock()
-    success, errors = terminate_nodes(
-        client=mock_client, 
-        nodes=[m1, m2, m3], 
-    )
+    success, errors = terminate_nodes(client=mock_client, nodes=[m1, m2, m3])
     expected_calls = [
-        mock.call.core.delete_node(node, body=V1DeleteOptions(), propagation_policy='foreground') 
+        mock.call.core.delete_node(
+            node, body=V1DeleteOptions(), propagation_policy="foreground"
+        )
         for node in [m1, m2, m3]
     ]
 
@@ -33,22 +39,56 @@ def test_terminate_nodes():
     assert errors[0][0] == m2
     assert isinstance(errors[0][1], ApiException)
 
-
     mock_client.reset_mock()
 
     mock_client.core.delete_node.side_effect = [None, ApiException(404), None]
-    success, errors = terminate_nodes(
-        client=mock_client, 
-        nodes=[m1, m2, m3], 
-    )
+    success, errors = terminate_nodes(client=mock_client, nodes=[m1, m2, m3])
     expected_calls = [
-        mock.call.core.delete_node(node, body=V1DeleteOptions(), propagation_policy='foreground') 
-        for node in [m1,m2,m3]
+        mock.call.core.delete_node(
+            node, body=V1DeleteOptions(), propagation_policy="foreground"
+        )
+        for node in [m1, m2, m3]
     ]
     assert mock_client.mock_calls == expected_calls
     assert success == [m1, m3]
     assert errors[0][0] == m2
     assert isinstance(errors[0][1], ApiException)
+
+
+def test_does_instance_exist():
+
+    # if the node doesn't exist at all, then the client will raise an exception
+    with mock.patch(
+        "paasta_tools.kubernetes.bin.cleanup_nodes.ClientError", autospec=True
+    ) as mock_error:
+        mock_error.response = {"Error": {"Code": 404}}
+        mock_client = mock.MagicMock()
+        mock_client.side_effect = mock_error
+
+        assert does_instance_exist(mock_client, "i-12345") is False
+
+    statuses = [
+        "pending",
+        "running",
+        "shutting-down",
+        "terminated",
+        "stopping",
+        "stopped",
+    ]
+    running = [True, True, False, False, False, False]
+
+    for status, running in zip(statuses, running):
+        mock_client.reset_mock()
+        mock_client.describe_instance_status.return_value = {
+            "InstanceStatuses": [{"InstanceState": {"Name": status}}]
+        }
+        assert does_instance_exist(mock_client, "i-12345") is running
+
+    # finally, there have been instances where the client doesn't 404, but there
+    # isn't a status attached to the instance
+    mock_client.reset_mock()
+    mock_client.describe_instance_status.return_value = {"InstanceStatuses": []}
+    assert does_instance_exist(mock_client, "i-12345") is False
 
 
 def test_main():
@@ -57,45 +97,57 @@ def test_main():
     ) as mock_get_all_nodes, mock.patch(
         "paasta_tools.kubernetes.bin.cleanup_nodes.KubeClient", autospec=True
     ) as mock_kube_client, mock.patch(
-        'paasta_tools.kubernetes.bin.cleanup_nodes.is_node_ready', autospec=True
-    ) as mock_is_node_ready, mock.patch(
-        'paasta_tools.kubernetes.bin.cleanup_nodes.nodes_for_cleanup', autospec=True
+        "paasta_tools.kubernetes.bin.cleanup_nodes.nodes_for_cleanup", autospec=True
     ) as mock_nodes_for_cleanup, mock.patch(
-        'paasta_tools.kubernetes.bin.cleanup_nodes.terminate_nodes', autospec=True
+        "paasta_tools.kubernetes.bin.cleanup_nodes.terminate_nodes", autospec=True
     ) as mock_terminate_nodes, mock.patch(
-        'paasta_tools.kubernetes.bin.cleanup_nodes.parse_args', autospec=True
-    ) as mock_parse_args:
+        "paasta_tools.kubernetes.bin.cleanup_nodes.parse_args", autospec=True
+    ) as mock_parse_args, mock.patch(
+        "boto3.client", autospec=True
+    ):
         mock_args = mock.MagicMock()
         mock_args.dry_run = False
 
         mock_parse_args.return_value = mock_args
 
-        m1, m2, m3 = mock.MagicMock(), mock.MagicMock(), mock.MagicMock()
+        m1 = mock.MagicMock(metadata=mock.Mock())
+        m2 = mock.Mock(metadata=mock.Mock())
+        m3 = mock.Mock(metadata=mock.Mock())
+
+        for i, m in enumerate([m1, m2, m3]):
+            m.metadata.name = f"m{i+1}"
+            m.metadata.labels = {
+                "failure-domain.beta.kubernetes.io/region": "us-west-1"
+            }
+
         mock_get_all_nodes.return_value = [m1, m2, m3]
+        mock_nodes_for_cleanup.return_value = [m2, m3]
+        mock_terminate_nodes.return_value = (["m2"], [("m3", mock.MagicMock())])
 
-        mock_is_node_ready.side_effect = [True, False, False]
-
-        mock_terminate_nodes.return_value = ([], [('foo', mock.MagicMock())])
         with pytest.raises(SystemExit) as e:
             main()
-            mock_terminate_nodes.assert_called_once_with([m2, m3])
-            assert e.value.code == 1
+
+        mock_terminate_nodes.assert_called_once_with(mock_kube_client(), ["m2", "m3"])
+        assert e.value.code == 1
+
+        mock_terminate_nodes.reset_mock()
+        mock_terminate_nodes.return_value = (["m2", "m3"], [])
+        main()
+        mock_terminate_nodes.assert_called_once_with(mock_kube_client(), ["m2", "m3"])
 
 
 def test_main_dry_run():
     with mock.patch(
         "paasta_tools.kubernetes.bin.cleanup_nodes.get_all_nodes", autospec=True
     ) as mock_get_all_nodes, mock.patch(
-        "paasta_tools.kubernetes.bin.cleanup_nodes.KubeClient", autospec=True
-    ) as mock_kube_client, mock.patch(
-        'paasta_tools.kubernetes.bin.cleanup_nodes.is_node_ready', autospec=True
+        "paasta_tools.kubernetes.bin.cleanup_nodes.is_node_ready", autospec=True
     ) as mock_is_node_ready, mock.patch(
-        'paasta_tools.kubernetes.bin.cleanup_nodes.nodes_for_cleanup', autospec=True
-    ) as mock_nodes_for_cleanup, mock.patch(
-        'paasta_tools.kubernetes.bin.cleanup_nodes.terminate_nodes', 
+        "paasta_tools.kubernetes.bin.cleanup_nodes.terminate_nodes", autospec=True
     ) as mock_terminate_nodes, mock.patch(
-        'paasta_tools.kubernetes.bin.cleanup_nodes.parse_args', autospec=True
-    ) as mock_parse_args:
+        "paasta_tools.kubernetes.bin.cleanup_nodes.parse_args", autospec=True
+    ) as mock_parse_args, mock.patch(
+        "boto3.client", autospec=True
+    ):
         mock_args = mock.MagicMock()
         mock_args.dry_run = True
 
