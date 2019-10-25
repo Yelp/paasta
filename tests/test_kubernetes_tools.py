df8c7f89fcea9e195531937decc4ceb6f9892a1e
Bronek Kozicki <bronek@yelp.com>
2019-10-25 07:11:46 -0700

    Fix test
    This test needs a conditions=None (i.e. not found), but the default MagicMock is anything but. Without this change the test is false positive.

--- tests/test_kubernetes_tools.py
+++ tests/test_kubernetes_tools.py
@@ -1862,7 +1862,7 @@ def test_is_pod_ready():
     )
     assert not is_pod_ready(mock_pod)
 
-    mock_pod = mock.MagicMock(status=mock.MagicMock())
+    mock_pod = mock.MagicMock(status=mock.MagicMock(conditions=None))
     assert not is_pod_ready(mock_pod)
 
 
