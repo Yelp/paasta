99d1aeebd29da24dd4a685866d56c25cc0478765
Ashish Khatkar <akhatkar@yelp.com>
2020-01-23 17:28:40 -0800

    Showing pod status irrespective of state of flink cluster

--- paasta_tools/cli/cmds/status.py
+++ paasta_tools/cli/cmds/status.py
@@ -866,6 +866,17 @@ def should_job_info_be_shown(cluster_state):
     )
 
 
+def append_pod_status(pod_status, output: List[str]):
+    output.append(f"    Pods:")
+    rows: List[Union[str, Tuple[str, str, str]]] = [("Pod Name", "Host", "Phase")]
+    for pod in pod_status:
+        rows.append((pod["name"], pod["host"], pod["phase"]))
+        if pod["reason"] != "":
+            rows.append(PaastaColors.grey(f"  {pod['reason']}: {pod['message']}"))
+    pods_table = format_table(rows)
+    output.extend([f"      {line}" for line in pods_table])
+
+
 def print_flink_status(
     cluster: str,
     service: str,
@@ -907,6 +918,11 @@ def print_flink_status(
         output.append(f"    State: {status.state}")
 
     if not should_job_info_be_shown(status.state):
+        # In case where the jobmanager of cluster is in crashloopbackoff
+        # The pods for the cluster will be available and we need to show the pods.
+        # So that paasta status -v and kubectl get pods show the same consistent result.
+        if verbose and len(status.pod_status) > 0:
+            append_pod_status(status.pod_status, output)
         output.append(f"    No other information available in non-running state")
         return 0
 
@@ -1005,14 +1021,7 @@ def print_flink_status(
                         f"            {str(exc_ts)} ({humanize.naturaltime(exc_ts)})"
                     )
     if verbose and len(status.pod_status) > 0:
-        output.append(f"    Pods:")
-        rows: List[Union[str, Tuple[str, str, str]]] = [("Pod Name", "Host", "Phase")]
-        for pod in status.pod_status:
-            rows.append((pod["name"], pod["host"], pod["phase"]))
-            if pod["reason"] != "":
-                rows.append(PaastaColors.grey(f"  {pod['reason']}: {pod['message']}"))
-        pods_table = format_table(rows)
-        output.extend([f"      {line}" for line in pods_table])
+        append_pod_status(status.pod_status, output)
     return 0
 
 
