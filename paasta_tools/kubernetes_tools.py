e3f09fb22222d289c7808a10da2d7c2de976c70e
Azret Kenzhaliev <bayke@yelp.com>
2020-01-27 04:01:10 -0800

    don't add annotation if service is not a smartstack service

--- paasta_tools/kubernetes_tools.py
+++ paasta_tools/kubernetes_tools.py
@@ -1111,10 +1111,15 @@ class KubernetesDeploymentConfig(LongRunningServiceConfig):
         docker_volumes = self.get_volumes(
             system_volumes=system_paasta_config.get_volumes()
         )
-        annotations: Dict[str, Any] = {
-            "smartstack_registrations": json.dumps(self.get_registrations()),
-            "iam.amazonaws.com/role": self.get_iam_role(),
-        }
+        if service_namespace_config.is_in_smartstack():
+            annotations: Dict[str, Any] = {
+                "smartstack_registrations": json.dumps(self.get_registrations()),
+                "iam.amazonaws.com/role": self.get_iam_role(),
+            }
+        else:
+            annotations: Dict[str, Any] = {
+                "iam.amazonaws.com/role": self.get_iam_role(),
+            }
         metrics_provider = self.get_autoscaling_params()["metrics_provider"]
         if metrics_provider in {"http", "uwsgi"}:
             annotations["autoscaling"] = metrics_provider
