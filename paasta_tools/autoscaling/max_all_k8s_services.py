c321f5f1244855c09bc08b6d4755b66346174233
Evan Krall <krall@yelp.com>
2020-03-04 14:11:43 -0800

    Script to scale up all HPA k8s deployments to max instances.

---
+++ paasta_tools/autoscaling/max_all_k8s_services.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+from paasta_tools.kubernetes.application.controller_wrappers import (
+    get_application_wrapper,
+)
+from paasta_tools.kubernetes_tools import KubeClient
+from paasta_tools.kubernetes_tools import KubernetesDeploymentConfig
+from paasta_tools.paasta_service_config_loader import PaastaServiceConfigLoader
+from paasta_tools.utils import get_services_for_cluster
+from paasta_tools.utils import load_system_paasta_config
+
+
+def main() -> None:
+    system_paasta_config = load_system_paasta_config()
+
+    kube_client = KubeClient()
+
+    services = {
+        service
+        for service, instance in get_services_for_cluster(
+            cluster=system_paasta_config.get_cluster(), instance_type="kubernetes"
+        )
+    }
+
+    for service in services:
+        pscl = PaastaServiceConfigLoader(service=service, load_deployments=False)
+        for instance_config in pscl.instance_configs(
+            cluster=system_paasta_config.get_cluster(),
+            instance_type_class=KubernetesDeploymentConfig,
+        ):
+            # Force min_instances = max_instances so that we scale up.
+            max_instances = instance_config.get_max_instances()
+            if max_instances is not None:
+                instance_config.config_dict["min_instances"] = max_instances
+                formatted_application = instance_config.format_kubernetes_app()
+                wrapper = get_application_wrapper(formatted_application)
+                wrapper.soa_config = instance_config
+                print(f"Scaling up {service}.{instance_config.instance}")
+                wrapper.update(kube_client)
+
+
+if __name__ == "__main__":
+    main()
