557a7eb74050c70d162120038d0aa230b0045b8e
Ka Wai Wan <kawaiwan@yelp.com>
2020-03-19 12:41:59 -0700

    Add sanitise option to paasta_list_kubernetes_service_instances

--- tests/test_list_kubernetes_service_instances.py
+++ tests/test_list_kubernetes_service_instances.py
@@ -1,5 +1,5 @@
 import mock
-from pytest import raises
+import pytest
 
 from paasta_tools.list_kubernetes_service_instances import main
 from paasta_tools.list_kubernetes_service_instances import parse_args
@@ -13,22 +13,34 @@ def test_parse_args():
         assert parse_args() == mock_parser.return_value.parse_args()
 
 
-def test_main():
-    with mock.patch(
-        "paasta_tools.list_kubernetes_service_instances.parse_args", autospec=True
-    ) as mock_parse_args, mock.patch(
-        "paasta_tools.list_kubernetes_service_instances.get_services_for_cluster",
-        autospec=True,
-        return_value=[("service1", "instance1"), ("service2", "instance1")],
-    ) as mock_get_services_for_cluster, mock.patch(
-        "paasta_tools.list_kubernetes_service_instances.paasta_print", autospec=True
-    ) as mock_print:
-        with raises(SystemExit) as e:
-            main()
-        assert e.value.code == 0
-        mock_get_services_for_cluster.assert_called_with(
+@mock.patch("paasta_tools.list_kubernetes_service_instances.parse_args", autospec=True)
+@mock.patch(
+    "paasta_tools.list_kubernetes_service_instances.get_services_for_cluster",
+    autospec=True,
+    return_value=[("service_1", "instance1"), ("service_2", "instance1")],
+)
+@mock.patch(
+    "paasta_tools.list_kubernetes_service_instances.paasta_print", autospec=True
+)
+@pytest.mark.parametrize(
+    "sanitise,expected",
+    [
+        (False, "service_1.instance1\nservice_2.instance1"),
+        (True, "service--1-instance1\nservice--2-instance1"),
+    ],
+)
+def test_main(mock_print, mock_get_services, mock_parse_args, sanitise, expected):
+    mock_parse_args.return_value = mock.Mock(sanitise=sanitise)
+
+    with pytest.raises(SystemExit) as e:
+        main()
+
+    assert e.value.code == 0
+    assert mock_get_services.call_args_list == [
+        mock.call(
             cluster=mock_parse_args.return_value.cluster,
             instance_type="kubernetes",
             soa_dir=mock_parse_args.return_value.soa_dir,
         )
-        mock_print.assert_called_with("service1.instance1\nservice2.instance1")
+    ]
+    assert mock_print.call_args_list == [mock.call(expected)]
