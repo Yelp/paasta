dd013e8e63dd702fa1e99a6c7fa71d1787f2a00f
Mingqi Zhu <mingqiz@yelp.com>
2020-03-23 22:46:02 -0700

    Add api for paasta autoscaling

--- paasta_tools/instance/hpa_metrics_parser.py
+++ paasta_tools/instance/hpa_metrics_parser.py
@@ -1,4 +1,4 @@
-class HPAMetricsParser():
+class HPAMetricsParser:
     def __init__(self, hpa):
         self.NAME = "name"
         self.TARGET = "target_value"
@@ -9,15 +9,16 @@ class HPAMetricsParser():
         Parse target metrics.
         """
         metric_spec = getattr(metric, metric.type.lower())
+        print(metric_spec)
         status = {}
         switchers = {
-            'Pods': self.parse_pod_metric,
-            'External': self.parse_external_metric,
-            'Resource': self.parse_resource_metric,
+            "Pods": self.parse_pod_metric,
+            "External": self.parse_external_metric,
+            "Resource": self.parse_resource_metric,
         }
         switchers[metric.type](metric_spec, status)
+        status[self.TARGET] = str(status[self.TARGET]) if status[self.TARGET] else "N/A"
         return status
-
 
     def parse_current(self, metric):
         """
@@ -27,21 +28,32 @@ class HPAMetricsParser():
         status = {}
         status[self.NAME] = metric_spec.metric_name
         switchers = {
-            'Pods': self.parse_pod_metric_current,
-            'External': self.parse_external_metric_current,
-            'Resource': self.parse_resource_metric_current,
+            "Pods": self.parse_pod_metric_current,
+            "External": self.parse_external_metric_current,
+            "Resource": self.parse_resource_metric_current,
         }
         switchers[metric.type](metric_spec, status)
+        status[self.CURRENT] = (
+            str(status[self.CURRENT]) if status[self.CURRENT] else "N/A"
+        )
         return status
 
-    #External metrics
+    # External metrics
     def parse_external_metric(self, metric_spec, status):
         status[self.NAME] = metric_spec.metric_name
-        status[self.TARGET] = metric_spec.target_average_value if hasattr(metric_spec, "target_average_value") else metric_spec.target_value
+        status[self.TARGET] = (
+            metric_spec.target_average_value
+            if getattr(metric_spec, "target_average_value")
+            else metric_spec.target_value
+        )
 
     def parse_external_metric_current(self, metric_spec, status):
         status[self.NAME] = metric_spec.metric_name
-        status[self.CURRENT] =metric_spec.current_average_value if hasattr(metric_spec, "current_average_value") else metric_spec.current_value
+        status[self.CURRENT] = (
+            metric_spec.current_average_value
+            if getattr(metric_spec, "current_average_value")
+            else metric_spec.current_value
+        )
 
     # Pod metrics
     def parse_pod_metric(self, metric_spec, status):
@@ -55,8 +67,16 @@ class HPAMetricsParser():
     # Resource metrics
     def parse_resource_metric(self, metric_spec, status):
         status[self.NAME] = metric_spec.name
-        status[self.TARGET] = metric_spec.target_average_value if hasattr(metric_spec, "target_average_value") else metric_spec.target_average_utilization
+        status[self.TARGET] = (
+            metric_spec.target_average_value
+            if getattr(metric_spec, "target_average_value")
+            else metric_spec.target_average_utilization
+        )
 
     def parse_resource_metric_current(self, metric_spec, status):
         status[self.NAME] = metric_spec.name
-        status[self.CURRENT] =metric_spec.current_average_value if hasattr(metric_spec, "current_average_value") else metric_spec.current_average_utilization
+        status[self.CURRENT] = (
+            metric_spec.current_average_value
+            if getattr(metric_spec, "current_average_value")
+            else metric_spec.current_average_utilization
+        )
