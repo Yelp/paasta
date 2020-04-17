68e35ea6b682eb0708bd6c43db1ee4284ce220d7
gabrielv <gabrielv@yelp.com>
2020-04-17 02:57:20 -0700

    CLUSTERMAN-511 Add support for pod.affinity on metrics_collector.
    Previously we only checked for node selectors. But cassandra is using
    pod affinity, so we need to include support for affinity expressions.

--- tests/kubernetes/kubernetes_cluster_connector_test.py
+++ tests/kubernetes/kubernetes_cluster_connector_test.py
@@ -23,7 +23,13 @@ from kubernetes.client import V1PodCondition
 from kubernetes.client import V1PodSpec
 from kubernetes.client import V1PodStatus
 from kubernetes.client import V1ResourceRequirements
+from kubernetes.client.models.v1_affinity import V1Affinity
 from kubernetes.client.models.v1_node import V1Node as KubernetesNode
+from kubernetes.client.models.v1_node_affinity import V1NodeAffinity
+from kubernetes.client.models.v1_node_selector import V1NodeSelector
+from kubernetes.client.models.v1_node_selector_requirement import V1NodeSelectorRequirement
+from kubernetes.client.models.v1_node_selector_term import V1NodeSelectorTerm
+from kubernetes.client.models.v1_preferred_scheduling_term import V1PreferredSchedulingTerm
 
 from clusterman.interfaces.cluster_connector import AgentState
 from clusterman.kubernetes.kubernetes_cluster_connector import KubernetesClusterConnector
@@ -84,7 +90,7 @@ def pod3():
 @pytest.fixture
 def pod5():
     return V1Pod(
-        metadata=V1ObjectMeta(name='pod3', annotations=dict()),
+        metadata=V1ObjectMeta(name='pod5', annotations=dict()),
         status=V1PodStatus(
             phase='Pending',
             conditions=None,
@@ -97,6 +103,81 @@ def pod5():
                 )
             ],
             node_selector={'clusterman.com/pool': 'bar'}
+        )
+    )
+
+
+@pytest.fixture
+def pod6():
+    return V1Pod(
+        spec=V1PodSpec(
+            containers=[
+                V1Container(
+                    name='container',
+                    resources=V1ResourceRequirements(requests={'cpu': '1.5'})
+                )
+            ],
+            affinity=V1Affinity(
+                node_affinity=V1NodeAffinity(
+                    required_during_scheduling_ignored_during_execution=V1NodeSelector(
+                        node_selector_terms=[
+                            V1NodeSelectorTerm(
+                                match_expressions=[
+                                    V1NodeSelectorRequirement(
+                                        key='clusterman.com/pool',
+                                        operator='In',
+                                        values=['bar']
+                                    )
+                                ]
+                            )
+                        ]
+                    )
+                )
+            )
+        )
+    )
+
+
+@pytest.fixture
+def pod7():
+    return V1Pod(
+        spec=V1PodSpec(
+            containers=[
+                V1Container(
+                    name='container',
+                    resources=V1ResourceRequirements(requests={'cpu': '1.5'})
+                )
+            ],
+            affinity=V1Affinity(
+                node_affinity=V1NodeAffinity(
+                    required_during_scheduling_ignored_during_execution=V1NodeSelector(
+                        node_selector_terms=[
+                            V1NodeSelectorTerm(
+                                match_expressions=[
+                                    V1NodeSelectorRequirement(
+                                        key='clusterman.com/scheduler',
+                                        operator='Exists'
+                                    )
+                                ]
+                            )
+                        ]
+                    ),
+                    preferred_during_scheduling_ignored_during_execution=[
+                        V1PreferredSchedulingTerm(
+                            weight=10,
+                            preference=V1NodeSelectorTerm(
+                                match_expressions=[
+                                    V1NodeSelectorRequirement(
+                                        key='clusterman.com/pool',
+                                        operator='In',
+                                        values=['bar']
+                                    )
+                                ]
+                            )
+                        )
+                    ]
+                )
+            )
         )
     )
 
@@ -195,3 +276,40 @@ def test_delete_node(mock_cluster_connector):
         mock_delete_node.return_value = None
         mock_cluster_connector.delete_node(mock_node)
         assert mock_delete_node.called
+
+
+def test_pod_matches_node_selector_or_affinity(mock_cluster_connector, pod2, pod5, pod6, pod7):
+    pod8 = deepcopy(pod6)
+    pod8.spec.node_selector = {'clusterman.com/pool': 'not-bar'}
+    pod9 = deepcopy(pod6)
+    pod9.spec.affinity.node_affinity.required_during_scheduling_ignored_during_execution \
+        .node_selector_terms[0].match_expressions[0] \
+        .values = ['not-bar']
+    assert not mock_cluster_connector._pod_matches_node_selector_or_affinity(pod2)
+    assert mock_cluster_connector._pod_matches_node_selector_or_affinity(pod5)
+    assert mock_cluster_connector._pod_matches_node_selector_or_affinity(pod6)
+    assert mock_cluster_connector._pod_matches_node_selector_or_affinity(pod7)
+    assert not mock_cluster_connector._pod_matches_node_selector_or_affinity(pod8)
+    assert not mock_cluster_connector._pod_matches_node_selector_or_affinity(pod9)
+
+
+def test_selector_term_matches_requirement(mock_cluster_connector):
+    selector_term = V1NodeSelectorTerm(
+        match_expressions=[
+            V1NodeSelectorRequirement(
+                key='clusterman.com/scheduler',
+                operator='Exists'
+            ),
+            V1NodeSelectorRequirement(
+                key='clusterman.com/pool',
+                operator='In',
+                values=['bar']
+            )
+        ]
+    )
+    selector_requirement = V1NodeSelectorRequirement(
+        key='clusterman.com/pool',
+        operator='In',
+        values=['bar']
+    )
+    assert mock_cluster_connector._selector_term_matches_requirement(selector_term, selector_requirement)
