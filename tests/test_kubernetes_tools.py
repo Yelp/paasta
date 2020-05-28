efd7e5b76a1d4907cd7f631b53a23bfaa3fddcfb
Maksym Melnychok <maksym@yelp.com>
2020-05-28 06:20:56 -0700

    Revert "Set dns policy to Default to use node's dns resolver (#2820)"
    This reverts commit 7c9ea4e45e44def9e35e40e0abb7d75760d83efb.

--- tests/test_kubernetes_tools.py
+++ tests/test_kubernetes_tools.py
@@ -1018,7 +1018,6 @@ class TestKubernetesDeploymentConfig:
             node_selector={"yelp.com/pool": "default"},
             restart_policy="Always",
             volumes=[],
-            dns_policy="Default",
         )
         pod_spec_kwargs.update(spec_affinity)
         assert ret == V1PodTemplateSpec(
@@ -2700,7 +2699,7 @@ def test_warning_big_bounce():
             job_config.format_kubernetes_app().spec.template.metadata.labels[
                 "paasta.yelp.com/config_sha"
             ]
-            == "configc21a00a4"
+            == "config3836dd19"
         ), "If this fails, just change the constant in this test, but be aware that deploying this change will cause every service to bounce!"
 
 
