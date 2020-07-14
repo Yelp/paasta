06b8a5233998d9ae689b07ad27a45f787cbd1fc7
David Morrison <drmorr@yelp.com>
2020-07-14 09:28:13 -0700

    bump metrics check TTL to hopefully make this less noisy

--- clusterman/batch/cluster_metrics_collector.py
+++ clusterman/batch/cluster_metrics_collector.py
@@ -147,7 +147,7 @@ class ClusterMetricsCollector(BatchDaemon, BatchLoggingMixin, BatchRunningSentin
                         output='OK: clusterman cluster_metrics was successful',
                         check_every='1m',
                         source=self.options.cluster,
-                        ttl='10m',
+                        ttl='20m',
                         noop=self.options.disable_sensu,
                     )
                     sensu_checkin(**sensu_args)
