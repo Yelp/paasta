ca27c7b138b18a886a8d8910f951d2d752aae318
Thomas Robinson <trobinso@yelp.com>
2020-07-16 17:57:28 -0700

    Use "should" instead of "must", given the limitations of this feature

--- paasta_tools/cli/cmds/mark_for_deployment.py
+++ paasta_tools/cli/cmds/mark_for_deployment.py
@@ -219,7 +219,7 @@ def mark_for_deployment(git_url, deploy_group, service, commit, allowed_groups=N
     """Mark a docker image for deployment"""
     username = get_username()
     if allowed_groups is not None and username not in ldap_user_search(allowed_groups):
-        logline = f"current user is not authorized to perform this action (must be in one of {allowed_groups})"
+        logline = f"current user is not authorized to perform this action (should be in one of {allowed_groups})"
         _log(service=service, line=logline, component="deploy", level="event")
         return 1
 
