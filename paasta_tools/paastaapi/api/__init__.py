7da044035dd40166c0c9779a4dcc6313ec1f96d6
Maksym Melnychok <maksym@yelp.com>
2020-08-10 02:45:34 -0700

    paastaapi from oapi3 spec
    - convert swagger.json into oapi.yaml
    - add makefile target to generate api
    - add generated code

---
+++ paasta_tools/paastaapi/api/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from paasta_tools.paastaapi.api.autoscaler_api import AutoscalerApi
+from paasta_tools.paastaapi.api.default_api import DefaultApi
+from paasta_tools.paastaapi.api.marathon_dashboard_api import MarathonDashboardApi
+from paasta_tools.paastaapi.api.resources_api import ResourcesApi
+from paasta_tools.paastaapi.api.service_api import ServiceApi
