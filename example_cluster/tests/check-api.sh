c58b48934ab8d0b340f728e153f415d1ecee9604
Evan Krall <krall@yelp.com>
2021-04-19 17:35:08 -0700

    Remove marathon-related configs and check for example_cluster

--- example_cluster/tests/check-api.sh
+++ example_cluster/tests/check-api.sh
@@ -9,8 +9,3 @@ test_url (){
 test_url paasta_api:5054/v1/version
 test_url paasta_api:5054/v1/services/hello-world
 test_url paasta_api:5054/v1/services/hello-world
-STATUS=$(test_url paasta_api:5054/v1/services/hello-world/main/status 2>&1 | jq -r '.["marathon"]["deploy_status"]')
-if [ "$STATUS" != "Running" ]; then
-    echo "Status should be Running not $STATUS"
-    exit 1
-fi
