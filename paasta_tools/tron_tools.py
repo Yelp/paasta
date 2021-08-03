cb001c5af6cb9a064a09e10b933db5e459c50f7a
Luis Perez <luisp@yelp.com>
2021-08-03 14:21:44 -0700

    Add logspout feature flag to all tron on k8s containers
    We don't want to turn this on for existing Mesos tronjobs since the logs
    won't be used in that case.
    
    We'll be using logspout to ship tronjob logs to our logging streams and
    then displaying those in Tron rather than doing what we did with
    Tron-on-Mesos and asking Mesos (or, in this case, K8s) to feed us logs
    periodically.

--- paasta_tools/tron_tools.py
+++ paasta_tools/tron_tools.py
@@ -306,8 +306,14 @@ class TronActionConfig(InstanceConfig):
     def get_spark_cluster_manager(self):
         return self.config_dict.get("spark_cluster_manager", "mesos")
 
-    def get_env(self):
+    def get_env(self, use_k8s: bool = False) -> Dict[str, str]:
         env = super().get_env()
+        # for Tron-on-K8s, we want to ship tronjob output through logspout
+        # such that this output eventually makes it into our per-instance
+        # log streams automatically
+        if use_k8s:
+            env["TRON_ENABLE_LOGSPOUT"] = "1"
+
         if self.get_executor() == "spark":
             spark_config_dict = self.get_spark_config_dict()
             env["EXECUTOR_CLUSTER"] = self.get_spark_paasta_cluster()
@@ -718,9 +724,11 @@ def format_tron_action_dict(action_config: TronActionConfig, use_k8s: bool = Fal
     # whatever is in soaconfigs to the k8s equivalent here as well.
     if executor in KUBERNETES_EXECUTOR_NAMES and use_k8s:
         result["executor"] = "kubernetes"
+
         result["secret_env"] = action_config.get_secret_env()
+
+        all_env = action_config.get_env(use_k8s=True)
         # For k8s, we do not want secret envvars to be duplicated in both `env` and `secret_env`
-        all_env = action_config.get_env()
         result["env"] = {k: v for k, v in all_env.items() if not is_secret_ref(v)}
 
     elif executor in MESOS_EXECUTOR_NAMES:
