456810385bf04428b5f460abf741b00baddb6e11
Manvendra Singh <manvendra021997@gmail.com>
2021-10-08 09:10:37 +0100

    Create Istio VirtualServices for service routing (#3221)
    * Added garbage collection for istio mesh services
    
    Fixed return value from cleanup_kube_svc
    
    Added tests for setup_istio_mesh
    
    refactored error logging
    
    renamed setup_kube_services to renamed_kube_services
    
    * blackify
    
    * create virtualservice objects for paasta namespaces
    
    * better name, use const
    
    * cleanup vs objects for removed namespaces
    
    * Added garbage collection for istio mesh services
    
    Fixed return value from cleanup_kube_svc
    
    Added tests for setup_istio_mesh
    
    refactored error logging
    
    renamed setup_kube_services to renamed_kube_services
    
    * create virtualservice objects for paasta namespaces
    
    * cleanup vs objects for removed namespaces
    
    * fix iterating over partials, move the iterating part out of main to make it testable
    
    * removed redundent checks while retriving list
    
    * Fixed api call to create VS objects, removed redundent exception block
    
    Co-authored-by: Manvendra Singh <msingh@yelp.com>
    Co-authored-by: Maksym Melnychok <maksym@yelp.com>

--- tests/test_setup_istio_mesh.py
+++ tests/test_setup_istio_mesh.py
@@ -3,8 +3,9 @@ import mock
 from paasta_tools.kubernetes_tools import registration_label
 from paasta_tools.setup_istio_mesh import cleanup_paasta_namespace_services
 from paasta_tools.setup_istio_mesh import sanitise_kubernetes_service_name
+from paasta_tools.setup_istio_mesh import setup_istio_mesh
 from paasta_tools.setup_istio_mesh import setup_paasta_namespace_services
-from paasta_tools.setup_istio_mesh import setup_unified_service
+from paasta_tools.setup_istio_mesh import setup_paasta_routing
 from paasta_tools.setup_istio_mesh import UNIFIED_K8S_SVC_NAME
 from paasta_tools.setup_istio_mesh import UNIFIED_SVC_PORT
 
@@ -15,45 +16,38 @@ MOCK_PORT_NUMBER = 20508
 def test_setup_kube_service():
     mock_client = mock.Mock()
     service_name = "compute-infra-test-service.main"
-    mock_paasta_namespaces = {service_name: {"port": 20508}}
+    mock_paasta_namespaces = {service_name: {"port": MOCK_PORT_NUMBER}}
     sanitized_service_name = sanitise_kubernetes_service_name(service_name)
 
-    (k8s_fn, (_, k8s_svc)), *rest = setup_paasta_namespace_services(
-        kube_client=mock_client, paasta_namespaces=mock_paasta_namespaces
+    fn, *rest = setup_paasta_namespace_services(
+        kube_client=mock_client,
+        paasta_namespaces=mock_paasta_namespaces,
+        existing_namespace_services={},
+        existing_virtual_services={},
     )
 
-    assert len(rest) == 0
-    assert k8s_fn is mock_client.core.create_namespaced_service
+    k8s_svc = fn.args[1]
+
+    assert len(rest) == 1
+    assert fn.func is mock_client.core.create_namespaced_service
     assert k8s_svc.metadata.name == sanitized_service_name
     assert k8s_svc.spec.selector == {registration_label(service_name): "true"}
 
 
-def test_setup_unified_service():
+def test_setup_paasta_routing():
     mock_client = mock.Mock()
+    mock_namespaces = dict(foo=dict(proxy_port=MOCK_PORT_NUMBER))
 
-    mock_port_list = [MOCK_PORT_NUMBER]
-
-    (k8s_fn, (_, k8s_svc)), *rest = setup_unified_service(
-        kube_client=mock_client, port_list=mock_port_list
+    fn, *rest = setup_paasta_routing(
+        kube_client=mock_client, namespaces=mock_namespaces,
     )
+    k8s_svc = fn.args[1]
 
-    assert len(rest) == 0
-    assert k8s_fn is mock_client.core.create_namespaced_service
+    assert len(rest) == 1
+    assert fn.func is mock_client.core.create_namespaced_service
     assert len(k8s_svc.spec.ports) == 2
     assert k8s_svc.spec.ports[0].port == UNIFIED_SVC_PORT
     assert k8s_svc.spec.ports[1].port == MOCK_PORT_NUMBER
-
-
-def test_cleanup_paasta_namespace_services_garbage_collect_services():
-    mock_client = mock.Mock()
-    mock_paasta_namespaces = {"svc1", "svc2"}
-    mock_existing_namespace_services = {"svc1", "svc2", "svc3"}
-    (k8s_fn, (k8s_svc, _)), *rest = cleanup_paasta_namespace_services(
-        mock_client, mock_paasta_namespaces, mock_existing_namespace_services,
-    )
-    assert len(rest) == 0
-    assert k8s_fn is mock_client.core.delete_namespaced_service
-    assert k8s_svc == "svc3"
 
 
 def test_cleanup_paasta_namespace_services_does_not_remove_unified_svc():
@@ -61,13 +55,22 @@ def test_cleanup_paasta_namespace_services_does_not_remove_unified_svc():
     mock_paasta_namespaces = {"svc1", "svc2"}
 
     mock_existing_namespace_services = {"svc1", "svc2", "svc3", UNIFIED_K8S_SVC_NAME}
-    (k8s_fn, (k8s_svc, _)), *rest = cleanup_paasta_namespace_services(
-        mock_client, mock_paasta_namespaces, mock_existing_namespace_services,
+    calls = list(
+        cleanup_paasta_namespace_services(
+            mock_client,
+            mock_paasta_namespaces,
+            mock_existing_namespace_services,
+            mock_existing_namespace_services,
+        )
     )
+    funcs = {fn.func for fn in calls}
+    services = {calls[0].args[0], calls[1].args[-1]}
 
-    assert len(rest) == 0
-    assert k8s_fn is mock_client.core.delete_namespaced_service
-    assert k8s_svc != UNIFIED_K8S_SVC_NAME
+    assert {
+        mock_client.core.delete_namespaced_service,
+        mock_client.custom.delete_namespaced_custom_object,
+    } == funcs
+    assert {"svc3"} == services
 
 
 def test_cleanup_paasta_namespace_services_does_not_remove_svc_while_running_first_time():
@@ -76,6 +79,18 @@ def test_cleanup_paasta_namespace_services_does_not_remove_svc_while_running_fir
 
     mock_existing_namespace_services = {}
     calls = cleanup_paasta_namespace_services(
-        mock_client, mock_paasta_namespaces, mock_existing_namespace_services
+        mock_client,
+        mock_paasta_namespaces,
+        mock_existing_namespace_services,
+        mock_existing_namespace_services,
     )
     assert len(list(calls)) == 0
+
+
+@mock.patch("time.sleep", autospec=True)
+@mock.patch("paasta_tools.setup_istio_mesh.process_kube_services", autospec=True)
+def test_setup_istio_mesh_iterates(mock_process_kube_services, mock_sleep):
+    mock_yielded = mock.Mock()
+    mock_process_kube_services.return_value = iter([mock_yielded])
+    setup_istio_mesh(mock.Mock())
+    assert len(mock_yielded.mock_calls) == 1
