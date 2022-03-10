53f678d8ddc56c86e7c9ed729c972358735903c7
Luis Perez <luisp@yelp.com>
2022-03-10 12:44:25 -0800

    Only run check_manual_oapi_changes once
    We were running this hook once for every file whenever we ran pre-commit
    run --all-files, which was making things unhappy when the hook was run
    in parallel (since `make openapi-codegen` mucks with files)

--- paasta_tools/contrib/check_manual_oapi_changes.sh
+++ paasta_tools/contrib/check_manual_oapi_changes.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 set -uo pipefail
 
-if ! [[ "${PAASTA_SYSTEM_CONFIG_DIR:-}" =~ ^.*/general_itests/fake_etc_paasta ]] ; then
-    # Do not run during general_itests
-    diff_names=$(git diff origin/master --name-only)
-    schema_touched=$(echo "$diff_names" | grep 'paasta_tools/api/api_docs/oapi.yaml')
-    api_touched=$(echo "$diff_names" | grep 'paasta_tools/paastaapi')
+diff_names=$(git diff origin/master --name-only)
+schema_touched=$(echo "$diff_names" | grep 'paasta_tools/api/api_docs/oapi.yaml')
+api_touched=$(echo "$diff_names" | grep 'paasta_tools/paastaapi')
 
-    if [ ! -z "$api_touched" -a -z "$schema_touched" ]; then
-        echo "paasta_tools/paastaapi must not be modified manually" >&2
-        echo "Please revert your changes:" >&2
-        echo -e "$api_touched" >&2
-        exit 1
-    fi
+if [ ! -z "$api_touched" -a -z "$schema_touched" ]; then
+    echo "paasta_tools/paastaapi must not be modified manually" >&2
+    echo "Please revert your changes:" >&2
+    echo -e "$api_touched" >&2
+    exit 1
 fi
 
 make openapi-codegen &>/dev/null
