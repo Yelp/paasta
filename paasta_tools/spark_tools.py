0dd74503a416a232ec1704b055e343c66cbbf762
Luis Perez <luisp@yelp.com>
2022-03-21 09:57:50 -0700

    Add function for getting default spark configuration
    for the time being, this is really just used for configuring where to
    store event logs in S3 (`default_event_log_dir`)

--- paasta_tools/spark_tools.py
+++ paasta_tools/spark_tools.py
@@ -1,7 +1,31 @@
 import socket
+from functools import lru_cache
+from typing import Dict
+from typing import Optional
+
+import yaml
+from mypy_extensions import TypedDict
 
 KUBERNETES_NAMESPACE = "paasta-spark"
 DEFAULT_SPARK_SERVICE = "spark"
+
+DEFAULT_SPARK_RUN_CONFIG = "/nail/srv/configs/spark.yaml"
+
+
+class SparkConfig(TypedDict):
+    account_id: str
+    default_event_log_dir: str
+    history_server: str
+
+
+class SparkEnvironmentConfig(TypedDict):
+    environments: Dict[str, SparkConfig]
+
+
+@lru_cache(maxsize=1)
+def get_default_spark_configuration() -> Optional[SparkEnvironmentConfig]:
+    with open(DEFAULT_SPARK_RUN_CONFIG, mode="r") as f:
+        return yaml.safe_load(f.read())
 
 
 def get_webui_url(port: str) -> str:
