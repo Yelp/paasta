18ffade03f83d4f113168c7a750e1cc25c60538d
AhmedSoliman7 <asoliman@yelp.com>
2022-07-15 13:44:45 +0100

    Get Flink cluster status via JM API (#3377)
    * Add flink apis to paasta-api opi
    
    * Add routes and sample test
    
    * Add tests for flink routes
    
    * Add route to swagger
    
    * Add route to swagger
    
    * Fix oapi and work status function
    
    * fix flink/flink tools tests
    
    * Adapt print_flink_status to the paasta api
    
    * Add missing job details route to api.py
    
    * Add mocking for paasta config load
    
    * [FLINK-4097] Mypy typing fixes
    
    * [FLINK-4097] Remove unused parameters
    
    * [FLINK-4097] Add swagger spec for new endpoints
    
    * Remove unused model `InstanceSpecKafkaCluster`
    
    * Add makefile target to validate swagger.json
    
    * [FLINK-4097] Add fail-case unit tests
    
    * [FLINK-4097] Small improvements
    
    - Removes a logger set up for debugging
    - Removes empty reraise blocks
    - Changes elifs for ifs
    
    * [FLINK-4097] Fix for the JM url
    
    * [FLINK-4097] Small changes
    
    - Added 'concurrent fetch' of job details
    - Corrected test cases
    
    * [FLINK-4097] Correct type annotations
    
    * [FLINK-4097] Small corrections
    
    * [FLINK-4085] Gracefully handle non-running flink cluster states
    
    Co-authored-by: Marcos Salgueiro Balsa <mbalsa@yelp.com>

--- paasta_tools/api/api.py
+++ paasta_tools/api/api.py
@@ -99,6 +99,22 @@ def make_app(global_config=None):
 
     config.include("pyramid_swagger")
     config.include(request_logger)
+
+    config.add_route(
+        "flink.service.instance.jobs", "/v1/flink/{service}/{instance}/jobs"
+    )
+
+    config.add_route(
+        "flink.service.instance.job_details",
+        "/v1/flink/{service}/{instance}/jobs/{job_id}",
+    )
+
+    config.add_route(
+        "flink.service.instance.overview", "/v1/flink/{service}/{instance}/overview"
+    )
+    config.add_route(
+        "flink.service.instance.config", "/v1/flink/{service}/{instance}/config"
+    )
     config.include(profiling)
 
     config.add_route("resources.utilization", "/v1/resources/utilization")
