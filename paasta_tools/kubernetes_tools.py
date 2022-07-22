dba5e4ee3b41a8e745dd3043a3b44a3bb4c90315
Luis Pérez <luisp@yelp.com>
2022-07-22 13:13:20 -0400

    Wrap readiness check script with a timeout (#3402)
    In PAASTA-17673 we noticed that even though there's a 1s timeout set on
    this V1Probe, that timeout doesn't actually kill the script that the
    probe runs - which can lead to these checks stacking up in our hacheck
    sidecar and overwhelming the CPU request+limit of that sidecar and
    leads to the hacheck sidecar getting heavily throttled (which doesn't
    help :p)

--- paasta_tools/kubernetes_tools.py
+++ paasta_tools/kubernetes_tools.py
@@ -936,14 +936,21 @@ class KubernetesDeploymentConfig(LongRunningServiceConfig):
             # and to not cause rolling updates on everything at once this is a config option for now
             if not system_paasta_config.get_hacheck_match_initial_delay():
                 initial_delay = 10
+
+            period_s = 10  # we probably don't need to make this configurable...
+            timeout_s = (
+                period_s - 1
+            )  # same here, one second less than the period is probably ok
             readiness_probe = V1Probe(
                 _exec=V1ExecAction(
-                    command=self.get_readiness_check_script(system_paasta_config)
+                    command=["timeout", "--signal=KILL", f"{timeout_s}s"]
+                    + self.get_readiness_check_script(system_paasta_config)
                     + [str(self.get_container_port())]
                     + self.get_registrations()
                 ),
                 initial_delay_seconds=initial_delay,
-                period_seconds=10,
+                period_seconds=period_s,
+                timeout_seconds=timeout_s,
             )
         else:
             readiness_probe = None
