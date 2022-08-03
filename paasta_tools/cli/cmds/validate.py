23113c76440986cd36823f054dfc0b15445f3ff5
Jonathan Lee <jonlee@yelp.com>
2022-08-03 13:32:55 -0700

    Change regex logic to accomodate multiline comment

--- paasta_tools/cli/cmds/validate.py
+++ paasta_tools/cli/cmds/validate.py
@@ -100,7 +100,7 @@ SCHEMA_TYPES = {
 # we expect a comment that looks like # override-cpu-setting PROJ-1234
 # but we don't have a $ anchor in case users want to add an additional
 # comment
-OVERRIDE_CPU_AUTOTUNE_ACK_PATTERN = r"^#\s*override-cpu-setting\s+\([A-Z]+-[0-9]+\)"
+OVERRIDE_CPU_AUTOTUNE_ACK_PATTERN = r"#\s*override-cpu-setting\s+\([A-Z]+-[0-9]+\)"
 
 
 class ConditionConfig(TypedDict, total=False):
@@ -507,7 +507,7 @@ def _get_comments_for_key(data: CommentedMap, key: Any) -> Optional[str]:
     # this is a little weird, but ruamel is returning a list that looks like:
     # [None, None, CommentToken(...), None] for some reason instead of just a
     # single string
-    # Sometimes ruamel returns a recursive list of as well that looks like
+    # Sometimes ruamel returns a recursive list of CommentTokens as well that looks like
     # [None, None, [CommentToken(...),CommentToken(...),None], CommentToken(...), None]
     def _flatten_comments(comments):
         for comment in comments:
@@ -602,7 +602,7 @@ def validate_autoscaling_configs(service_path):
                     # a DAR if people aren't being careful.
                     if (
                         cpu_comment is None
-                        or re.match(
+                        or re.search(
                             pattern=OVERRIDE_CPU_AUTOTUNE_ACK_PATTERN,
                             string=cpu_comment,
                         )
