ef6d7d297159c481af0ba95cab00a9416f7499e1
Nurdan Almazbekov <nalma@yelp.com>
2023-04-17 17:41:29 -0700

    Fix mock patch for `get_secret`

--- tests/cli/test_cmds_secret.py
+++ tests/cli/test_cmds_secret.py
@@ -17,6 +17,7 @@ import mock
 from pytest import raises
 
 from paasta_tools.cli.cmds import secret
+from paasta_tools.kubernetes_tools import get_paasta_secret_name
 from paasta_tools.kubernetes_tools import KUBE_CONFIG_USER_PATH
 
 
@@ -147,7 +148,7 @@ def test_paasta_secret():
     ) as mock_log_audit, mock.patch(
         "paasta_tools.cli.cmds.secret.is_secrets_for_teams_enabled", autospec=True
     ) as mock_is_secrets_for_teams_enabled, mock.patch(
-        "paasta_tools.cli.cmds.secret.get_kubernetes_secret", autospec=True
+        "paasta_tools.cli.cmds.secret.get_secret", autospec=True
     ) as mock_get_kubernetes_secret, mock.patch(
         "paasta_tools.cli.cmds.secret.KubeClient", autospec=True
     ) as mock_kube_client, mock.patch(
@@ -252,7 +253,9 @@ def test_paasta_secret():
             config_file=KUBE_CONFIG_USER_PATH, context="mesosstage"
         )
         mock_get_kubernetes_secret.assert_called_with(
-            kube_client, "middleearth", "theonering", "paasta"
+            kube_client,
+            get_paasta_secret_name("paasta", "middleearth", "theonering"),
+            "paasta",
         )
 
         # empty namespace list
@@ -260,7 +263,9 @@ def test_paasta_secret():
         mock_select_k8s_secret_namespace.return_value = None
         secret.paasta_secret(mock_args)
         mock_get_kubernetes_secret.assert_called_with(
-            kube_client, "middleearth", "theonering"
+            kube_client,
+            get_paasta_secret_name("paasta", "middleearth", "theonering"),
+            "paasta",
         )
 
         mock_args = mock.Mock(
