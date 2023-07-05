0fc36f8a963f7df859732e5f0232e46ecbc35136
Wilmer Bandres <wilmerrafael@yelp.com>
2023-07-05 06:17:41 -0700

    Fixing tests

--- tests/test_kubernetes_tools.py
+++ tests/test_kubernetes_tools.py
@@ -3908,7 +3908,7 @@ def test_warning_big_bounce():
             job_config.format_kubernetes_app().spec.template.metadata.labels[
                 "paasta.yelp.com/config_sha"
             ]
-            == "configc22655c7"
+            == "configd6531f39"
         ), "If this fails, just change the constant in this test, but be aware that deploying this change will cause every service to bounce!"
 
 
@@ -3954,7 +3954,7 @@ def test_warning_big_bounce_routable_pod():
             job_config.format_kubernetes_app().spec.template.metadata.labels[
                 "paasta.yelp.com/config_sha"
             ]
-            == "config0107126a"
+            == "configa5abd828"
         ), "If this fails, just change the constant in this test, but be aware that deploying this change will cause every smartstack-registered service to bounce!"
 
 
