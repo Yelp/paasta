92c3988734cf3121c8c322888e374bf82876051a
Chi Chang <chi@yelp.com>
2023-11-21 06:53:58 -0800

    wip

--- paasta_tools/cli/cmds/spark_run.py
+++ paasta_tools/cli/cmds/spark_run.py
@@ -83,6 +83,7 @@ metadata:
   labels:
     spark: {spark_pod_label}
 spec:
+  dnsPolicy: Default
   affinity:
     podAffinity:
       preferredDuringSchedulingIgnoredDuringExecution:
@@ -698,21 +699,20 @@ def _parse_user_spark_args(
     enable_compact_bin_packing: bool = False,
     enable_spark_dra: bool = False,
 ) -> Dict[str, str]:
-    if not spark_args:
-        return {}
-
     user_spark_opts = {}
-    for spark_arg in spark_args.split():
-        fields = spark_arg.split("=", 1)
-        if len(fields) != 2:
-            print(
-                PaastaColors.red(
-                    "Spark option %s is not in format option=value." % spark_arg
-                ),
-                file=sys.stderr,
-            )
-            sys.exit(1)
-        user_spark_opts[fields[0]] = fields[1]
+
+    if spark_args:
+        for spark_arg in spark_args.split():
+            fields = spark_arg.split("=", 1)
+            if len(fields) != 2:
+                print(
+                    PaastaColors.red(
+                        "Spark option %s is not in format option=value." % spark_arg
+                    ),
+                    file=sys.stderr,
+                )
+                sys.exit(1)
+            user_spark_opts[fields[0]] = fields[1]
 
     if enable_compact_bin_packing:
         user_spark_opts["spark.kubernetes.executor.podTemplateFile"] = pod_template_path
@@ -1293,6 +1293,7 @@ def paasta_spark_run(args):
         args.enable_compact_bin_packing,
         args.enable_dra,
     )
+    log.warn("www=" + user_spark_opts["spark.kubernetes.executor.podTemplateFile"])
 
     args.cmd = _auto_add_timeout_for_job(args.cmd, args.timeout_job_runtime)
 
