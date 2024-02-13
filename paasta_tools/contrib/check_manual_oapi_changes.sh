9e24306ce4ed569186837d8a96bf5208237b1405
Jon Lee <jonlee@yelp.com>
2024-02-13 12:50:12 -0800

    Revert "fix test"
    This reverts commit dc02b9044a6dcfee05bd6cc5d264f0f10fcc190b.

--- paasta_tools/contrib/check_manual_oapi_changes.sh
+++ paasta_tools/contrib/check_manual_oapi_changes.sh
@@ -15,7 +15,7 @@ if [ ! -z "$api_touched" -a -z "$schema_touched" ]; then
     exit 1
 fi
 
-make openapi-codegen
+make openapi-codegen &>/dev/null
 diff=$(git diff --name-only | grep paasta_tools/paastaapi)
 if [ ! -z "$diff" ]; then
     echo "paasta_tools/paastaapi codegen has a diff, either commit the changes or fix oapi.yaml:" >&2
