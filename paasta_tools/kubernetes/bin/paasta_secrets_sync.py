bb7b7c48abe42745af4575aaa2ca7a6c23cfb458
Luis Perez <luisp@yelp.com>
2024-02-15 11:46:41 -0800

    Make sync delay configurable

--- paasta_tools/kubernetes/bin/paasta_secrets_sync.py
+++ paasta_tools/kubernetes/bin/paasta_secrets_sync.py
@@ -696,7 +696,7 @@ def create_or_update_k8s_secret(
     :param get_secret_data: is a function to postpone fetching data in order to reduce service load, e.g. Vault API
     """
     # In order to prevent slamming the k8s API, add some artificial delay here
-    time.sleep(0.3)
+    time.sleep(load_system_paasta_config().get_secret_sync_delay_seconds())
 
     kubernetes_signature = get_secret_signature(
         kube_client=kube_client,
