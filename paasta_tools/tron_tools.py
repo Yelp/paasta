22aaf9362b9565eb32331dc97d6275873dc596ff
Sameer Sharma <sameersharma@yelp.com>
2024-05-09 15:55:43 +0100

    MLCOMPUTE-1224 | add aws default region to prevent botocore.exceptions.NoRegionError

--- paasta_tools/tron_tools.py
+++ paasta_tools/tron_tools.py
@@ -442,6 +442,7 @@ class TronActionConfig(InstanceConfig):
         env = super().get_env(system_paasta_config=system_paasta_config)
 
         if self.get_executor() == "spark":
+            env["AWS_DEFAULT_REGION"] = DEFAULT_AWS_REGION
             env["PAASTA_INSTANCE_TYPE"] = "spark"
             # XXX: is this actually necessary? every PR that's added this hasn't really mentioned why,
             # and Chesterton's Fence makes me very wary about removing it
