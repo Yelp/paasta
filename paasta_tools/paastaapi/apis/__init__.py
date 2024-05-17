e923dd03d256e4ef14e7a3f4e094439cb55a5d65
Evan Krall <krall@yelp.com>
2024-05-17 16:43:19 -0700

    Remove resources.utilization API endpoint, since this only ever worked on Mesos

--- paasta_tools/paastaapi/apis/__init__.py
+++ paasta_tools/paastaapi/apis/__init__.py
@@ -17,5 +17,4 @@
 # Import APIs into API package:
 from paasta_tools.paastaapi.api.autoscaler_api import AutoscalerApi
 from paasta_tools.paastaapi.api.default_api import DefaultApi
-from paasta_tools.paastaapi.api.resources_api import ResourcesApi
 from paasta_tools.paastaapi.api.service_api import ServiceApi
