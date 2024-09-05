15e7e69b8f3d1a8e54bd3132ada99bb2474f9e7a
Luis Perez <luisp@yelp.com>
2024-09-05 11:19:31 -0700

    tron: update schema for bulkdata + correctly pass extra volumes
    We collectively missed that get_volumes() gets called on the
    TronActionConfig and not the TronJobConfig. Furthermore, we missed that
    we weren't actually calling get_volumes() for normal Tron jobs -
    instead, we were only calling get_extra_volumes() and relying on the
    Tron `default_volumes` config to add the usual default volumes (which
    differs from how services work: we always call get_volumes() as we don't
    distinguish between user-provided volumes and default ones when
    generating the config there).
    
    I've tested this on tron-infrastage (as well as ensured tests pass for
    this commit), so this should be safe.

--- paasta_tools/tron_tools.py
+++ paasta_tools/tron_tools.py
@@ -1021,7 +1021,13 @@ def format_tron_action_dict(action_config: TronActionConfig):
         # service account token volumes for service authentication
         result["projected_sa_volumes"] = action_config.get_projected_sa_volumes()
 
-        extra_volumes = action_config.get_extra_volumes()
+        # XXX: now that we're actually passing through extra_volumes correctly (e.g., using get_volumes()),
+        # we can get rid of the default_volumes from the Tron master config
+        system_paasta_config = load_system_paasta_config()
+        extra_volumes = action_config.get_volumes(
+            system_paasta_config.get_volumes(),
+            uses_bulkdata_default=system_paasta_config.get_uses_bulkdata_default(),
+        )
         if executor == "spark":
             is_mrjob = action_config.config_dict.get("mrjob", False)
             system_paasta_config = load_system_paasta_config()
