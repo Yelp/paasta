0ff45c7b82724f24834202b121160b704a81337a
Chi Chang <chi@yelp.com>
2025-02-28 04:18:56 -0800

    Skip a part if the config is missing

--- paasta_tools/spark_tools.py
+++ paasta_tools/spark_tools.py
@@ -234,20 +234,23 @@ def get_spark_driver_monitoring_annotations(
     """
     Returns Spark driver pod annotations - currently used for Prometheus metadata.
     """
-    ui_port_str = str(spark_config.get("spark.ui.port", ""))
+    annotations: Dict[str, str] = dict()
+
+    ui_port_str = spark_config.get("spark.ui.port", "")
+    if ui_port_str != "":
+        annotations["prometheus.io/port"] = ui_port_str
+        annotations["prometheus.io/path"] = "/metrics/prometheus"
+
     paasta_service_non_truncated = spark_config.get(
         "spark.kubernetes.executor.annotation.paasta.yelp.com/service", ""
     )
     paasta_instance_non_truncated = spark_config.get(
         "spark.kubernetes.executor.annotation.paasta.yelp.com/instance", ""
     )
+    if paasta_service_non_truncated != "" and paasta_instance_non_truncated != "":
+        annotations["paasta.yelp.com/service"] = paasta_service_non_truncated
+        annotations["paasta.yelp.com/instance"] = paasta_instance_non_truncated
 
-    annotations = {
-        "prometheus.io/port": ui_port_str,
-        "prometheus.io/path": "/metrics/prometheus",
-        "paasta.yelp.com/service": paasta_service_non_truncated,
-        "paasta.yelp.com/instance": paasta_instance_non_truncated,
-    }
     return annotations
 
 
--- tests/test_spark_tools.py
+++ tests/test_spark_tools.py
@@ -70,6 +70,73 @@ def test_get_volumes_from_spark_k8s_configs(mock_sys, spark_conf, expected):
 
 
 @pytest.mark.parametrize(
+    "spark_config,expected",
+    [
+        # Empty config
+        ({}, {}),
+        # Only UI port specified
+        (
+            {"spark.ui.port": "4040"},
+            {
+                "prometheus.io/port": "4040",
+                "prometheus.io/path": "/metrics/prometheus",
+            },
+        ),
+        # Only service and instance specified
+        (
+            {
+                "spark.kubernetes.executor.annotation.paasta.yelp.com/service": "my-service",
+                "spark.kubernetes.executor.annotation.paasta.yelp.com/instance": "my-instance",
+            },
+            {
+                "paasta.yelp.com/service": "my-service",
+                "paasta.yelp.com/instance": "my-instance",
+            },
+        ),
+        # All annotations specified
+        (
+            {
+                "spark.ui.port": "4040",
+                "spark.kubernetes.executor.annotation.paasta.yelp.com/service": "my-service",
+                "spark.kubernetes.executor.annotation.paasta.yelp.com/instance": "my-instance",
+            },
+            {
+                "prometheus.io/port": "4040",
+                "prometheus.io/path": "/metrics/prometheus",
+                "paasta.yelp.com/service": "my-service",
+                "paasta.yelp.com/instance": "my-instance",
+            },
+        ),
+        # Missing service
+        (
+            {
+                "spark.ui.port": "4040",
+                "spark.kubernetes.executor.annotation.paasta.yelp.com/instance": "my-instance",
+            },
+            {
+                "prometheus.io/port": "4040",
+                "prometheus.io/path": "/metrics/prometheus",
+            },
+        ),
+        # Missing instance
+        (
+            {
+                "spark.ui.port": "4040",
+                "spark.kubernetes.executor.annotation.paasta.yelp.com/service": "my-service",
+            },
+            {
+                "prometheus.io/port": "4040",
+                "prometheus.io/path": "/metrics/prometheus",
+            },
+        ),
+    ],
+)
+def test_get_spark_driver_monitoring_annotations(spark_config, expected):
+    result = spark_tools.get_spark_driver_monitoring_annotations(spark_config)
+    assert result == expected
+
+
+@pytest.mark.parametrize(
     "docker_volumes,expected",
     [
         # Empty list
