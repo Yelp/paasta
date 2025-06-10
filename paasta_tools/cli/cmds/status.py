5bf589d8c36e06afe37157d21bd2d08b6a8d0ee5
Nathan Leigh <nleigh11@hotmail.com>
2025-06-10 15:29:19 +0100

    Update paasta_tools/cli/cmds/status.py
    Co-authored-by: Luis Pérez <luisp@yelp.com>

--- paasta_tools/cli/cmds/status.py
+++ paasta_tools/cli/cmds/status.py
@@ -801,21 +801,21 @@ def _print_flink_status_from_job_manager(
 
     if verbose:
         if flink_instance_config:
-            # Print Flink Pool information
-            flink_pool = flink_instance_config.get_pool()
-            output.append(f"    Flink Pool: {flink_pool}")
+        # Print Flink Pool information
+        flink_pool = flink_instance_config.get_pool()
+        output.append(f"    Flink Pool: {flink_pool}")
 
-            # Print ownership information
-            flink_monitoring_team = flink_instance_config.get_team() or get_team(
-                overrides={}, service=service, soa_dir=DEFAULT_SOA_DIR
-            )
-            output.append(f"    Owner: {flink_monitoring_team}")
+        # Print ownership information
+        flink_monitoring_team = flink_instance_config.get_team() or get_team(
+            overrides={}, service=service, soa_dir=DEFAULT_SOA_DIR
+        )
+        output.append(f"    Owner: {flink_monitoring_team}")
 
-            # Print rb information
-            flink_rb_for_instance = flink_instance_config.get_runbook() or get_runbook(
-                overrides={}, service=service, soa_dir=DEFAULT_SOA_DIR
-            )
-            output.append(f"    Flink Runbook: {flink_rb_for_instance}")
+        # Print rb information
+        flink_rb_for_instance = flink_instance_config.get_runbook() or get_runbook(
+            overrides={}, service=service, soa_dir=DEFAULT_SOA_DIR
+        )
+        output.append(f"    Flink Runbook: {flink_rb_for_instance}")
 
         # Print Flink repo links
         output.append(f"    Repo(git): https://github.yelpcorp.com/services/{service}")
