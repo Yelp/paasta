d0a01e9b094b43a24c65316d96c743b00e57cde4
Luis Perez <luisp@yelp.com>
2025-09-25 11:52:55 -0700

    Released 1.30.14 via make release

--- paasta_tools/paastaapi/models/__init__.py
+++ paasta_tools/paastaapi/models/__init__.py
@@ -45,6 +45,7 @@ from paasta_tools.paastaapi.model.instance_status_tron import InstanceStatusTron
 from paasta_tools.paastaapi.model.instance_tasks import InstanceTasks
 from paasta_tools.paastaapi.model.integer_and_error import IntegerAndError
 from paasta_tools.paastaapi.model.kubernetes_container import KubernetesContainer
+from paasta_tools.paastaapi.model.kubernetes_container_env_var_v2 import KubernetesContainerEnvVarV2
 from paasta_tools.paastaapi.model.kubernetes_container_v2 import KubernetesContainerV2
 from paasta_tools.paastaapi.model.kubernetes_healthcheck import KubernetesHealthcheck
 from paasta_tools.paastaapi.model.kubernetes_pod import KubernetesPod
